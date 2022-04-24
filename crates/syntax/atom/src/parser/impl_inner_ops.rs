cd289481d45be6dd95dab35fe8c4eb4ccf22331a
Xiyu Zhai <xiyuzhai@mit.edu>
2022-04-24 14:06:31 -0400

    fix: correct TokenStream range start position

--- crates/syntax/atom/src/parser/impl_inner_ops.rs
+++ crates/syntax/atom/src/parser/impl_inner_ops.rs
@@ -8,11 +8,11 @@ impl<'a> AtomLRParser<'a> {
         self.stack.push(Atom::new(self.stream.pop_range(), kind))
     }
 
-    pub(crate) fn save_stream(&self) -> Stream<'a> {
+    pub(crate) fn save_stream(&self) -> TokenStream<'a> {
         self.stream.clone()
     }
 
-    pub(crate) fn rollback(&mut self, stream: Stream<'a>) {
+    pub(crate) fn rollback(&mut self, stream: TokenStream<'a>) {
         self.stream = stream
     }
 }
