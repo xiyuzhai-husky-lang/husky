c6a49393d90b34404e20694cba669448ebfa9669
Xiyu Zhai <xiyuzhai@mit.edu>
2022-04-28 21:44:21 -0400

    refactor: delete comment

--- crates/syntax/ast/src/transform/impl_parse_atoms.rs
+++ crates/syntax/ast/src/transform/impl_parse_atoms.rs
@@ -9,9 +9,6 @@ impl<'a> AstTransformer<'a> {
         f: impl FnOnce(AtomLRParser) -> S,
     ) -> S {
         let symbol_context = self.symbol_context();
-        // let abs_semantic_tokens_ptr: *const Vec<AbsSemanticToken> = &self.abs_semantic_tokens;
-        // let abs_semantic_tokens_mut_ptr: *mut Vec<AbsSemanticToken> =
-        //     abs_semantic_tokens_ptr as *mut Vec<AbsSemanticToken>;
         f(AtomLRParser::new(
             &symbol_context,
             Some(unsafe { ref_to_mut_ref(&self.abs_semantic_tokens) }),
