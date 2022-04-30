65110ca4eac4cb92f8400db60ab23919f15a0437
Xiyu Zhai <xiyuzhai@mit.edu>
2022-04-30 10:33:04 -0400

    feat: generate primitive literals

---
+++ rust-gen/mnist/src/lib.rs
@@ -0,0 +1,12 @@
+
+pub(crate) fn find_connected_components(binary_image: &datasets::cv::mnist::BinaryImage28) -> Vec<i32 {
+    let mut b = binary_image.clone();
+    b[0i32] = 1u32;
+    Vec<i32::__call__()
+}
+
+pub mod __init__ {
+    pub fn link_entity_with_compiled(compile_time: &mut compile_time_db::HuskyLangCompileTime) {
+        todo!()
+    }
+}
