9f43faabe61f38a8b1727ec4c9d433c2464b16d9
Xiyu Zhai <xiyuzhai@mit.edu>
2022-05-13 10:26:49 -0400

    feat: diagnostics for enum variant definition head

--- crates/syntax/ast/src/transform/impl_enum_item.rs
+++ crates/syntax/ast/src/transform/impl_enum_item.rs
@@ -5,13 +5,21 @@ use token::identify;
 
 impl<'a> AstTransformer<'a> {
     pub(super) fn parse_enum_variant(&mut self, tokens: &[Token]) -> AstResult<AstKind> {
-        Ok(if tokens.len() == 1 {
-            AstKind::EnumVariantDefnHead {
+        if tokens.len() == 1 {
+            Ok(AstKind::EnumVariantDefnHead {
                 ident: identify!(self, tokens[0], SemanticTokenKind::EnumVariant),
                 variant_class: EnumVariantKind::Constant,
-            }
+            })
         } else {
-            todo!()
-        })
+            match tokens[0].kind {
+                TokenKind::Keyword(_) => todo!(),
+                _ => err!(
+                    format!(
+                        "expect keyword to lead multiple tokens in definition head of enum variant"
+                    ),
+                    tokens[0].range
+                ),
+            }
+        }
     }
 }
