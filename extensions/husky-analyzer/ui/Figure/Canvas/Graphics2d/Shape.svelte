53de60308b00a0b51b52d2674b4018bfe2e95d16
Xiyu Zhai <xiyuzhai@mit.edu>
2022-05-20 23:31:49 -0400

    feat: make laser points work

--- extensions/husky-analyzer/ui/Figure/Canvas/Graphics2d/Shape.svelte
+++ extensions/husky-analyzer/ui/Figure/Canvas/Graphics2d/Shape.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
-    import type { Shape2dProps } from "src/figure/graphics2d/shape";
+    import type {
+        Shape2dKind,
+        Shape2dProps,
+    } from "src/figure/graphics2d/shape";
     import Arrow from "./Shape/Arrow.svelte";
+    import Point2d from "./Shape/Point2d.svelte";
 
-    export let shape: Shape2dProps;
+    export let shape2d_props: Shape2dProps;
     export let lineWidth: number;
+    $: console.log(shape2d_props);
 </script>
 
-{#if shape.kind === "Arrow2d"}
-    <Arrow arrow={shape} {lineWidth} />
+{#if shape2d_props.kind === "Arrow2d"}
+    <Arrow arrow={shape2d_props} {lineWidth} />
+{:else if shape2d_props.kind === "Point2d"}
+    <Point2d point={shape2d_props.point} />
 {/if}
