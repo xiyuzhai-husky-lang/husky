42916a88621aa032232bf5b139576c385353c8c9
Xiyu Zhai <xiyuzhai@mit.edu>
2022-05-25 19:15:21 -0400

    fix: bug in LocalValue

--- crates/abstraction/fold/src/local_value.rs
+++ crates/abstraction/fold/src/local_value.rs
@@ -1,7 +1,20 @@
-#[derive(Debug)]
 pub struct LocalValue<T: Copy> {
     values: Vec<(u8, T)>,
     current: u8,
+}
+
+impl<T> std::fmt::Debug for LocalValue<T>
+where
+    T: Copy + std::fmt::Debug,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("\nLocalValue:\n"))?;
+        f.write_fmt(format_args!("    values:\n"))?;
+        for (version, value) in &self.values {
+            f.write_fmt(format_args!("        {} {:?}\n", version, value))?;
+        }
+        Ok(())
+    }
 }
 
 impl<T: Copy> LocalValue<T> {
@@ -18,7 +31,7 @@ impl<T: Copy> LocalValue<T> {
 
     pub fn exit(&mut self) {
         self.current -= 1;
-        if self.values.last().unwrap().0 > self.current {
+        while self.values.last().unwrap().0 > self.current {
             self.values.pop();
         }
     }
@@ -36,6 +49,7 @@ impl<T: Copy> LocalValue<T> {
 fn use_fold_value() {
     let mut v: LocalValue<i64> = LocalValue::new(0);
     v.enter();
+    v.set(22);
     v.set(23);
     v.enter();
     assert_eq!(v.value(), 23);
