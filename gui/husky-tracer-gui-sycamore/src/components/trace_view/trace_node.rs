35a4e435cc1ecf134db15705d15cbea2be2938b5
Xiyu Zhai <xiyuzhai@mit.edu>
2022-06-12 02:57:48 -0400

    save progress

--- gui/husky-tracer-gui-sycamore/src/components/trace_view/trace_node.rs
+++ gui/husky-tracer-gui-sycamore/src/components/trace_view/trace_node.rs
@@ -1,6 +1,15 @@
 use crate::*;
 
 #[derive(Prop)]
-pub struct TraceNodeProps2 {
+pub struct TraceNodeProps {
     trace_id: TraceId,
+}
+
+#[component]
+pub fn TraceNode<'a, G: Html>(scope: Scope<'a>, props: TraceNodeProps) -> View<G> {
+    let tracer_context = use_context::<TracerContext>(scope);
+    let tree_context = &tracer_context.tree_context;
+    let shown = tree_context.shown_signal(props.trace_id);
+    let expansion = tree_context.expanded_signal(props.trace_id);
+    todo!()
 }
