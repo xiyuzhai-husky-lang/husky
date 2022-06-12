452e3185183a182a34a4904d2eddaa8cd5c9479c
Xiyu Zhai <xiyuzhai@mit.edu>
2022-06-12 18:02:32 -0400

    save progress

---
+++ gui/husky-tracer-gui-sycamore/src/context/impl_control.rs
@@ -0,0 +1,47 @@
+use super::*;
+
+impl TracerContext {
+    pub fn toggle_expansion_handler(&self, trace_id: TraceId) -> Rc<dyn Fn()> {
+        let this = self.clone();
+        Rc::new(move || this.toggle_expansion(trace_id))
+    }
+
+    fn toggle_expansion(&self, trace_id: TraceId) {
+        let expansion = self.tree_context.expanded_signal(trace_id);
+        if expansion.get_cloned() {
+            expansion.set(false)
+        } else {
+            let focus = self.focus_context.focus_signal.get();
+            let trace_kind = self.tree_context.trace_kind(trace_id);
+            let key = SubtracesKey::new(&focus, trace_kind, trace_id);
+            if self.tree_context.subtraces_map.contains_key(&key) {
+                self.ws.send_message(
+                    HuskyTracerGuiMessageVariant::ToggleExpansion {
+                        trace_id,
+                        request_subtraces: false,
+                    },
+                    None,
+                );
+                expansion.set(true)
+            } else {
+                let this = self.clone();
+                self.ws.send_message(
+                    HuskyTracerGuiMessageVariant::ToggleExpansion {
+                        trace_id,
+                        request_subtraces: true,
+                    },
+                    Some(Box::new(move |message| match message.variant {
+                        HuskyTracerServerMessageVariant::ToggleExpansion {
+                            subtraces,
+                            associated_traces,
+                        } => {
+                            this.tree_context.receive_subtraces(key, subtraces);
+                            todo!()
+                        }
+                        _ => panic!(),
+                    })),
+                )
+            }
+        }
+    }
+}
