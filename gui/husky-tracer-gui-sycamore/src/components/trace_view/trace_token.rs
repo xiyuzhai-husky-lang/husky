51ba3f84440f263034985abe48b795431b11a3a9
Xiyu Zhai <xiyuzhai@mit.edu>
2022-06-13 10:53:24 -0400

    feat: impl spaces before and after

--- gui/husky-tracer-gui-sycamore/src/components/trace_view/trace_token.rs
+++ gui/husky-tracer-gui-sycamore/src/components/trace_view/trace_token.rs
@@ -7,12 +7,38 @@ pub struct TraceTokenProps {
 
 #[component]
 pub fn TraceToken<'a, G: Html>(scope: Scope<'a>, props: TraceTokenProps) -> View<G> {
+    let text = &props.data.value;
+    let spaces_before_style = spaces_style(count_spaces_before(text));
+    let spaces_after_style = spaces_style(count_spaces_after(text));
     view! {
         scope,
+        span (style=spaces_before_style)
         code(class=format!("TraceToken {}", props.data.kind)) {
             (props.data.value)
         }
+        span (style=spaces_after_style)
     }
+}
+
+fn count_spaces_before(text: &str) -> usize {
+    for (i, c) in text.chars().enumerate() {
+        if (c != ' ') {
+            return i;
+        }
+    }
+    text.chars().count()
+}
+fn count_spaces_after(text: &str) -> usize {
+    for (i, c) in text.chars().rev().enumerate() {
+        if c != ' ' {
+            return i;
+        }
+    }
+    return 0;
+}
+fn spaces_style(n: usize) -> String {
+    let width = n as f64 * 9.5;
+    format!("width: {width}px")
 }
 
 // <span style={spacesBeforeStyles} />
