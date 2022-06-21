b569ab69064fa4498d88e577cd1fd16d19002b79
Xiyu Zhai <xiyuzhai@mit.edu>
2022-06-21 00:29:32 -0400

    fix: css

--- forks/sycamore/examples/todomvc/src/main.rs
+++ forks/sycamore/examples/todomvc/src/main.rs
@@ -250,7 +250,7 @@ pub fn Item<G: Html>(cx: Scope, todo: RcSignal<Todo>) -> View<G> {
         input_ref
             .get::<DomNode>()
             .unchecked_into::<HtmlInputElement>()
-            .attention()
+            .focus()
             .unwrap();
         value.set(title());
     };
