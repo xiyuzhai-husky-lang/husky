9387ad5e1277c87358ce898a7eb93ed2686fd4d1
Xiyu Zhai <xiyuzhai@mit.edu>
2022-07-01 12:57:47 -0400

    save

--- .compiled/crates/mnist-classifier/src/bin/main.rs
+++ .compiled/crates/mnist-classifier/src/bin/main.rs
@@ -1,16 +1,18 @@
+use husky_compile_time::*;
 use husky_debugger::*;
 use mnist_classifier::__init__::link_entity_with_compiled;
-use husky_compile_time::*;
 
 #[tokio::main]
 async fn main() {
-    Debugger::new(|compile_time| init_compile_time(compile_time))
+    HuskyDebugger::new(|compile_time| init_compile_time(compile_time))
         .serve("localhost:51617")
         .await
         .expect("")
 }
 
 fn init_compile_time(compile_time: &mut HuskyCompileTime) {
-    compile_time.load_package("/home/xiyuzhai/Documents/husky/.compiled/crates/mnist-classifier/snapshot".into());
+    let husky_dir: std::path::PathBuf = std::env::var("HUSKY_DIR").unwrap().into();
+    let code_snapshot_dir = husky_dir.join(".compiled/crates/mnist_classifier/snapshot");
+    compile_time.load_package(&code_snapshot_dir);
     link_entity_with_compiled(compile_time)
 }
