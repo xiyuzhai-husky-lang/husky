bd85db3658f9f58cbcb92455b0f7a994da07ba3a
Xiyu Zhai <xiyuzhai@mit.edu>
2022-07-04 17:32:31 -0400

    clean

--- core/crates/compile/husky-rust-code-gen/src/lib_rs_content.rs
+++ core/crates/compile/husky-rust-code-gen/src/lib_rs_content.rs
@@ -5,7 +5,6 @@ pub(crate) fn rust_lib_rs_content(
     db: &dyn RustCodeGenQueryGroup,
     package_main: FilePtr,
 ) -> Arc<String> {
-    emsg_once!("deal with submodules");
     let mut generator = RustCodeGenerator::new_lib(db, package_main);
     generator.gen_lib_rs_content();
     Arc::new(generator.finish())
