a8fa358a99f1e276c92e51020a805bde79d34312
Xiyu Zhai <xiyuzhai@mit.edu>
2022-07-03 20:44:03 -0400

    feat: profile mobilenet on alienware

--- benchmarks/ml/cv/imagenet/mobilenet/main.py
+++ benchmarks/ml/cv/imagenet/mobilenet/main.py
@@ -1,6 +1,7 @@
 #!/home/xiyuzhai/anaconda3/bin/python
 import numpy as np
 import tensorflow as tf
+import time
 from tensorflow import keras
 from tensorflow.keras.layers import Dense, Activation
 from tensorflow.keras.optimizers import Adam
@@ -12,6 +13,7 @@ from tensorflow.keras.applications import imagenet_utils
 from sklearn.metrics import confusion_matrix
 import itertools
 import os
+from pathlib import Path
 import shutil
 import random
 import matplotlib.pyplot as plt
@@ -22,12 +24,22 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 mobile = tf.keras.applications.mobilenet.MobileNet()
 
-file_path = os.path.realpath(__file__)
+file_path = Path(__file__)
+mobilenet_dir = file_path.parent.absolute()
+data_dir = mobilenet_dir.joinpath("data")
 
 
 def prepare_image(file):
-    img_path = "data/MobileNet-samples/"
-    img = image.load_img(img_path + file, target_size=(224, 224))
+    img_path = str(data_dir)
+    img = image.load_img(img_path + "/" + file, target_size=(224, 224))
     img_array = image.img_to_array(img)
     img_array_expanded_dims = np.expand_dims(img_array, axis=0)
     return tf.keras.applications.mobilenet.preprocess_input(img_array_expanded_dims)
+
+
+preprocessed_image = prepare_image("1.jpg")
+start = time.time()
+for _ in range(0, 100):
+    predictions = mobile.predict(preprocessed_image)
+end = time.time()
+print("time elapsed for 100 predictions: %f seconds", end - start)
