ec9ffdba3a41ce21579f13df26a0527abc3ff95a
Xiyu Zhai <xiyuzhai@mit.edu>
2022-07-04 13:28:38 -0400

    save

--- core/crates/eval/vm/src/linkage/fp/field.rs
+++ core/crates/eval/vm/src/linkage/fp/field.rs
@@ -10,3 +10,52 @@ macro_rules! field_copy_fp {
         __SpecificRoutineFp(__wrapper)
     }};
 }
+
+#[macro_export]
+macro_rules! field_eval_ref_fp {
+    ($Type: ty, $field: ident) => {{
+        fn __wrapper<'temp, 'eval>(
+            values: &mut [__TempValue<'temp, 'eval>],
+        ) -> __TempValue<'temp, 'eval> {
+            let value: &'eval $Type = values[0].downcast_eval_ref();
+            __EvalRef(value.$field.upcast_any()).into()
+        }
+        __SpecificRoutineFp(__wrapper)
+    }};
+}
+#[macro_export]
+macro_rules! field_temp_ref_fp {
+    ($Type: ty, $field: ident) => {{
+        fn __wrapper<'temp, 'eval>(
+            values: &mut [__TempValue<'temp, 'eval>],
+        ) -> __TempValue<'temp, 'eval> {
+            let value: &$Type = values[0].downcast_temp_ref();
+            __TempValue::TempRefEval(unsafe { value.$field.upcast_arb_any() })
+        }
+        __SpecificRoutineFp(__wrapper)
+    }};
+}
+
+#[macro_export]
+macro_rules! field_temp_mut_fp {
+    ($Type: ty, $field: ident) => {{
+        fn __wrapper<'temp, 'eval>(
+            values: &mut [__TempValue<'temp, 'eval>],
+        ) -> __TempValue<'temp, 'eval> {
+            todo!("field_temp_mut_fp")
+        }
+        __SpecificRoutineFp(__wrapper)
+    }};
+}
+
+#[macro_export]
+macro_rules! field_move_fp {
+    ($Type: ty, $field: ident) => {{
+        fn __wrapper<'temp, 'eval>(
+            values: &mut [__TempValue<'temp, 'eval>],
+        ) -> __TempValue<'temp, 'eval> {
+            todo!("field_move_fp")
+        }
+        __SpecificRoutineFp(__wrapper)
+    }};
+}
--- core/crates/static/__husky_root/src/lib.rs
+++ core/crates/static/__husky_root/src/lib.rs
@@ -29,14 +29,16 @@ pub mod __init_utils {
     pub use dev_utils::__static_dev_src;
     pub use vm::{
         __EvalResult, __Linkage, __MemberLinkage, __OwnedValue, __SpecificRoutineFp,
-        __SpecificRoutineLinkage, __TempValue, field_copy_fp, field_linkage, index_copy_fp,
-        index_eval_ref_fp, index_linkage, index_move_fp, index_temp_mut_fp, index_temp_ref_fp,
-        method_elem_copy_fp, method_elem_eval_ref_fp, method_elem_linkage, method_elem_move_fp,
-        method_elem_temp_mut_fp, method_elem_temp_ref_fp, specific_transfer_linkage,
+        __SpecificRoutineLinkage, __TempValue, field_copy_fp, field_eval_ref_fp, field_linkage,
+        field_move_fp, field_temp_mut_fp, field_temp_ref_fp, index_copy_fp, index_eval_ref_fp,
+        index_linkage, index_move_fp, index_temp_mut_fp, index_temp_ref_fp, method_elem_copy_fp,
+        method_elem_eval_ref_fp, method_elem_linkage, method_elem_move_fp, method_elem_temp_mut_fp,
+        method_elem_temp_ref_fp, specific_transfer_linkage,
     };
     pub use wild_utils::arb_ref as __arb_ref;
 }
 
+use __init_utils::*;
 use dev_utils::__StaticDevSource;
 use dev_utils::{__static_dev_src, dev_src};
 use entity_kind::{EntityKind, FieldKind, MemberKind, RoutineKind, TyKind};
