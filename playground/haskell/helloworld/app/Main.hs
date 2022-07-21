5acc30b0d397d1ebc77fd3e4c63473971d6e1e5f
Xiyu Zhai <xiyuzhai@mit.edu>
2022-07-21 01:18:13 -0400

    try: haskell

--- playground/haskell/helloworld/app/Main.hs
+++ playground/haskell/helloworld/app/Main.hs
@@ -21,3 +21,21 @@ body_ = el "body"
 four = (\num1 num2 -> num1 + num2 + 1) 1 2
 
 newtype Html = Html String
+
+data Person = Person String Int -- where the first is the name and the second is
+                                -- the age
+
+data Person2 = Person2 { name :: String, age :: Int }
+
+a = Person2 "Gil" 32
+
+data Person3 = Person3 { pName :: String, pAge :: Int }
+
+data Tuple a b = Tuple a b
+
+t1 = Tuple "Clicked" True
+
+t2 = Tuple 'a' 'z'
+
+data Either a b = Left a
+                | Right b
