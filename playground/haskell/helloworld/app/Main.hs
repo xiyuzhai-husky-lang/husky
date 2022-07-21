0c4db74a3971211210705f2857c2e4c110f3535b
Xiyu Zhai <xiyuzhai@mit.edu>
2022-07-21 11:38:58 -0400

    try: haskell

--- playground/haskell/helloworld/app/Main.hs
+++ playground/haskell/helloworld/app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import           Lib
+import           GHC.Natural
+import           Data.Word
 
 main :: IO ()
 main = putStrLn myhtml
@@ -39,3 +41,86 @@ t2 = Tuple 'a' 'z'
 
 data Either a b = Left a
                 | Right b
+
+increment n = n + 1
+
+decrement m = m - 1
+
+add n m = if m /= 0
+          then add (increment n) (decrement m)
+          else n
+
+type Document = [Structure]
+
+data Structure = Heading Natural String
+               | Paragraph String
+               | UnorderedList [String]
+               | OrderedList [String]
+               | CodeBlock [String]
+
+parseLines :: [String] -> [String] -> Document
+parseLines currentParagraph txts =
+  let paragraph = Paragraph (unlines (reverse currentParagraph)) -- (2), (3)
+  in case txts    -- (4)
+      of
+       [] -> [paragraph]
+       currentLine:rest -> if trim currentLine == ""
+                           then paragraph:parseLines [] rest -- (5)
+                           else parseLines (currentLine:currentParagraph) rest -- (6)
+
+trim :: String -> String
+trim = unwords . words
+
+data Color = RGB Word8 Word8 Word8
+
+getBluePart :: Color -> Word8
+getBluePart color = case color of
+  RGB _ _ blue -> blue
+
+data Brightness = Dark
+                | Bright
+
+data EightColor =
+    Black
+  | Red
+  | Green
+  | Yellow
+  | Blue
+  | Magenta
+  | Cyan
+  | White
+
+data AnsiColor = AnsiColor Brightness EightColor
+
+ansiColorToVGA :: AnsiColor -> Color
+ansiColorToVGA ansicolor = case ansicolor of
+  AnsiColor Dark Black -> RGB 0 0 0
+  AnsiColor Bright Black -> RGB 85 85 85
+  AnsiColor Dark Red -> RGB 170 0 0
+  AnsiColor Bright Red -> RGB 255 85 85
+  _ -> RGB 0 0 0
+
+safeHead :: [a] -> Maybe a
+safeHead list = case list of
+  -- Empty list
+  []  -> Nothing
+  -- Cons cell pattern, will match any list with at least one element
+  x:_ -> Just x
+
+exactlyTwo :: [a] -> Maybe (a, a)
+exactlyTwo list = case list of
+  -- Will match a list with exactly two elements
+  [x, y] -> Just (x, y)
+  -- Will match any other pattern
+  _      -> Nothing
+
+-- This will also work
+exactlyTwoVersion2 :: [a] -> Maybe (a, a)
+exactlyTwoVersion2 list = case list of
+  -- Will match a list with exactly two elements
+  [x, y] -> Just (x, y)
+  -- Will match any other pattern
+  _      -> Nothing
+
+parse :: String -> Document
+parse = parseLines [] . lines
