700ca2a533fde397726940eee50940f1db58f724
Xiyu Zhai <xiyuzhai@mit.edu>
2022-07-21 12:39:58 -0400

    forks mmaclone

--- forks/jyh1/mmaclone/src/Data/Environment/Environment.hs
+++ forks/jyh1/mmaclone/src/Data/Environment/Environment.hs
@@ -1,21 +1,19 @@
 module Data.Environment.Environment where
 
-import Data.DataType
-import Eval.Patt.Pattern
-import Eval.Patt.PatternPrimi
-import Data.Environment.EnvironmentType
-
-
-import Control.Monad.Except
+import           Data.DataType
+import           Eval.Patt.Pattern
+import           Eval.Patt.PatternPrimi
+import           Data.Environment.EnvironmentType
+import           Control.Monad.Except
 import qualified Data.Map.Strict as M
-import Control.Monad.Trans.Except
-import Control.Lens hiding (Context,List)
-import Data.Maybe
+import           Control.Monad.Trans.Except
+import           Control.Lens hiding (Context, List)
+import           Data.Maybe
 import qualified Data.Text as T
-
 
 emptyOwnValue :: OwnValue
 emptyOwnValue = M.fromList [("$IterationLimit", Number 50000)]
+
 emptyDownValue :: DownValue
 emptyDownValue = M.empty
 
@@ -27,64 +25,56 @@ nullContext = Context emptyOwnValue emptyDownValue
 
 -- readCont :: Env -> IOThrowsError Context
 -- readCont = liftIO . readIORef
-
 mergePatt :: PatternRule -> PatternRule -> PatternRule
 mergePatt = (++)
+
 addPatt = (:)
 
 insertPattern :: [LispVal] -> LispVal -> Down -> Down
-insertPattern lhs rhs downV =
-  pattern %~ addPatt (List lhs,rhs) $ downV
+insertPattern lhs rhs = pattern %~ addPatt (List lhs, rhs)
 
 insertValue :: [LispVal] -> LispVal -> Down -> Down
-insertValue lhs rhs downV =
-  value %~ M.insert (List lhs) rhs $ downV
+insertValue lhs rhs = value %~ M.insert (List lhs) rhs
 
-updateDown :: [LispVal] -> LispVal ->Down -> Down
+updateDown :: [LispVal] -> LispVal -> Down -> Down
 updateDown lhs
   | isPattern (List lhs) = insertPattern lhs
   | otherwise = insertValue lhs
 
 updateDownValue :: LispVal -> LispVal -> DownValue -> DownValue
 updateDownValue val rhs =
-  let
-    name = getSetName val
-    List lhs = getLhs val
-    initial = updateDown lhs rhs emptyDown
-    update = const (updateDown lhs rhs)
-  in
-    M.insertWith update name initial
+  let name = getSetName val
+      List lhs = getLhs val
+      initial = updateDown lhs rhs emptyDown
+      update = const (updateDown lhs rhs)
+  in M.insertWith update name initial
 
 updateContext :: LispVal -> LispVal -> Context -> Context
-updateContext (Atom name) rhs =
-  own %~ M.insert name rhs
-updateContext val@(List _) rhs =
-  down %~ updateDownValue val rhs
-
+updateContext (Atom name) rhs = own %~ M.insert name rhs
+updateContext val@(List _) rhs = down %~ updateDownValue val rhs
 
 validSet :: LispVal -> Bool
-validSet (List [(Atom "Condition"), p, _]) = validSet p
+validSet (List [Atom "Condition", p, _]) = validSet p
 validSet (List ((Atom "Condition"):_)) = False
-validSet (List (Atom _ : _)) = True
+validSet (List (Atom _:_)) = True
 validSet (Atom _) = True
 validSet _ = False
 
 replaceDown :: Down -> LispVal -> ReplaceResult
 replaceDown downV lhs =
-  let patt = downV ^. pattern.to (replaceRuleList lhs)-- (msum . map (replace lhs))
-      val = downV ^. value.to (M.lookup lhs) in
-    case val of
-      Nothing -> patt
-      just -> return just
+  let patt = downV ^. pattern . to (replaceRuleList lhs)-- (msum . map (replace lhs))
+      val = downV ^. value . to (M.lookup lhs)
+  in case val of
+       Nothing -> patt
+       just    -> return just
 
 getSetName :: LispVal -> T.Text
 getSetName (List (Atom "Condition":p:_)) = getSetName p
 getSetName (List (Atom name:_)) = name
 
 getLhs :: LispVal -> LispVal
-getLhs (List [Atom "Condition", p, t]) =
-  List [Atom "Condition", getLhs p, t]
-getLhs (List (Atom _: lhs)) = List lhs
+getLhs (List [Atom "Condition", p, t]) = List [Atom "Condition", getLhs p, t]
+getLhs (List (Atom _:lhs)) = List lhs
 
 unpackLhs :: LispVal -> (T.Text, LispVal)
 unpackLhs val = (getSetName val, getLhs val)
@@ -93,19 +83,18 @@ replaceDownValue :: LispVal -> DownValue -> Primi
 -- replaceDownValue val@(List (Atom name : lhs)) downVal =
 replaceDownValue val downVal =
   let name = getSetName val
-      lhs = getLhs val in
-    liftM (fromMaybe val) $ do
-      let downV = M.lookup name downVal
-      case downV of
-        Nothing -> return Nothing
-        Just downV' -> replaceDown downV' lhs
+      lhs = getLhs val
+  in fmap (fromMaybe val)
+     $ do
+       let downV = M.lookup name downVal
+       case downV of
+         Nothing     -> return Nothing
+         Just downV' -> replaceDown downV' lhs
 
 replaceOwnValue :: LispVal -> OwnValue -> LispVal
-replaceOwnValue val@(Atom name) ownVal =
-  fromMaybe val $ M.lookup name ownVal
+replaceOwnValue val@(Atom name) ownVal = fromMaybe val $ M.lookup name ownVal
 
 replaceContext :: LispVal -> Context -> Primi
-replaceContext val@(Atom _) con =
-  return $ con ^.own.to (replaceOwnValue val)
-replaceContext val@(List _) con =
-  con ^. down.to (replaceDownValue val)
+replaceContext val@(Atom _) con = return
+  $ con ^. own . to (replaceOwnValue val)
+replaceContext val@(List _) con = con ^. down . to (replaceDownValue val)
--- forks/jyh1/mmaclone/src/Data/Environment/EnvironmentType.hs
+++ forks/jyh1/mmaclone/src/Data/Environment/EnvironmentType.hs
@@ -1,31 +1,32 @@
-{-#LANGUAGE TemplateHaskell#-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Data.Environment.EnvironmentType where
 
-import Data.DataType
-
-
+import           Data.DataType
 import qualified Data.Map.Strict as M
-import Control.Lens hiding (Context,List)
+import           Control.Lens hiding (Context, List)
 import qualified Data.Text as T
-import Control.Monad.Trans.State
-
-
+import           Control.Monad.Trans.State
 
 type ValueRule = M.Map LispVal LispVal
+
 type PatternRule = [(LispVal, LispVal)]
+
 type OwnValue = M.Map T.Text LispVal
+
 type DownValue = M.Map T.Text Down
-data Down = Down {_value :: ValueRule,_pattern :: PatternRule}
-data Context = Context {_own :: OwnValue, _down :: DownValue}
+
+data Down = Down { _value :: ValueRule, _pattern :: PatternRule }
+
+data Context = Context { _own :: OwnValue, _down :: DownValue }
 
 makeLenses ''Down
+
 makeLenses ''Context
 
-
-
 -- * Types and common functions for defining primitive functions.
-
 type Result = ThrowsError (Maybe LispVal)
+
 type IOResult = IOThrowsError (Maybe LispVal)
 
 type EvalResult = IOThrowsError LispVal
@@ -41,30 +42,32 @@ type Primitives = M.Map T.Text Primi
 
 type EvalArguments = [LispVal] -> Primi
 
-
 -- | Envrionment for primitive function
-data PrimiEnv = PrimiEnv
-  { _eval :: Eval
-  , _con :: Context
-  , _args :: [LispVal]
-  -- , _modified :: Bool
-  , _dep :: Int
-  , _line :: Int
-  }
+data PrimiEnv = PrimiEnv { _eval :: Eval
+                         , _con :: Context
+                         , _args :: [LispVal]
+                           -- , _modified :: Bool
+                         , _dep :: Int
+                         , _line :: Int
+                         }
 
 makeLenses ''PrimiEnv
 
-
 -- Pattern matching types
 type Pattern = LispVal
+
 type Matched = (T.Text, LispVal)
+
 type MatchRes = M.Map T.Text LispVal
+
 initialMatch = M.empty
 
-
 type Rule = (Pattern, LispVal)
+
 type Rules = [Rule]
 
 type MaybeMatch = Maybe MatchRes
+
 type MatchResult = StateResult MaybeMatch
+
 type ReplaceResult = StateResult (Maybe LispVal)
