25422ac9b070a29b697032773fe60421da9d14b7
Xiyu Zhai <xiyuzhai@mit.edu>
2022-07-23 13:49:37 -0400

    profile cargo build timings

--- __rust_gen__/crates/tests/test-enum/src/bin/main.rs
+++ __rust_gen__/crates/tests/test-enum/src/bin/main.rs
@@ -3,39 +3,39 @@ use test_enum::__init__::LINKAGES;
 
 #[tokio::main]
 async fn main() {
-    let code_snapshot_dir = "crates/tests/test-enum/snapshot/test-enum".into();
-    HuskyDebugger::new(
-        HuskyDebuggerConfig {
-            package_dir: code_snapshot_dir,
-            opt_sample_id: Some(__SampleId(23)),
-            verbose: false,
-            warn_missing_linkage: true,
-        },
-        LINKAGES,
-    )
-    .serve("localhost:51617")
-    .await
-    .expect("")
+    // let code_snapshot_dir = "crates/tests/test-enum/snapshot/test-enum".into();
+    // HuskyDebugger::new(
+    //     HuskyDebuggerConfig {
+    //         package_dir: code_snapshot_dir,
+    //         opt_sample_id: Some(__SampleId(23)),
+    //         verbose: false,
+    //         warn_missing_linkage: true,
+    //     },
+    //     LINKAGES,
+    // )
+    // .serve("localhost:51617")
+    // .await
+    // .expect("")
 }
 
 #[test]
 fn serve_on_error() {
-    __serve_on_error_init();
-    let code_snapshot_dir = "snapshot/test-enum".into();
-    let sample_id = __SampleId(23);
-    match tokio_test::block_on(
-        HuskyDebugger::new(
-            HuskyDebuggerConfig {
-                package_dir: code_snapshot_dir,
-                opt_sample_id: Some(sample_id),
-                verbose: false,
-                warn_missing_linkage: true,
-            },
-            LINKAGES,
-        )
-        .serve_on_error("localhost:51617", sample_id),
-    ) {
-        __TestResult::Success => (),
-        __TestResult::Failure => panic!(),
-    }
+    // __serve_on_error_init();
+    // let code_snapshot_dir = "snapshot/test-enum".into();
+    // let sample_id = __SampleId(23);
+    // match tokio_test::block_on(
+    //     HuskyDebugger::new(
+    //         HuskyDebuggerConfig {
+    //             package_dir: code_snapshot_dir,
+    //             opt_sample_id: Some(sample_id),
+    //             verbose: false,
+    //             warn_missing_linkage: true,
+    //         },
+    //         LINKAGES,
+    //     )
+    //     .serve_on_error("localhost:51617", sample_id),
+    // ) {
+    //     __TestResult::Success => (),
+    //     __TestResult::Failure => panic!(),
+    // }
 }
