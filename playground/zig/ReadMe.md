56576e614fde24e7b1fb8909f8c261932375a24a
Xiyu Zhai <xiyuzhai@mit.edu>
2022-07-25 16:07:05 -0400

    try more

---
+++ playground/zig/ReadMe.md
@@ -0,0 +1,3 @@
+# source
+
+<https://ziglearn.org/>
--- playground/zig/main.zig
+++ playground/zig/main.zig
@@ -33,3 +33,23 @@ test "while with continue expression" {
     }
     try expect(sum == 55);
 }
+
+test "for" {
+    //character literals are equivalent to integer literals
+    const string = [_]u8{ 'a', 'b', 'c' };
+
+    for (string) |character, index| {
+        _ = character;
+        _ = index;
+    }
+
+    for (string) |character| {
+        _ = character;
+    }
+
+    for (string) |_, index| {
+        _ = index;
+    }
+
+    for (string) |_| {}
+}
