eb404166f1143535eec685467f5ab6a3098df5f8
Xiyu Zhai <xiyuzhai@mit.edu>
2022-07-25 16:04:17 -0400

    try: zig

---
+++ playground/zig/main.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+const expect = @import("std").testing.expect;
+
+pub fn main() void {
+    std.debug.print("Hello, {s}!\n", .{"World"});
+}
+
+
+test "if statement" {
+    const a = true;
+    var x: u16 = 0;
+    if (a) {
+        x += 1;
+    } else {
+        x += 2;
+    }
+    try expect(x == 1);
+}
+
+test "while" {
+    var i: u8 = 2;
+    while (i < 100) {
+        i *= 2;
+    }
+    try expect(i == 128);
+}
+
+test "while with continue expression" {
+    var sum: u8 = 0;
+    var i: u8 = 1;
+    while (i <= 10) : (i += 1) {
+        sum += i;
+    }
+    try expect(sum == 55);
+}
