69f989c4ae0dc3a0c40932edcfe78430d50fe2d1
Xiyu Zhai <xiyuzhai@mit.edu>
2022-07-26 05:27:40 -0400

    feat: seems to work well

--- interfaces/husky-vm-interface/csrc/husky_vm.c
+++ interfaces/husky-vm-interface/csrc/husky_vm.c
@@ -1,1 +1,10 @@
 #include "husky_vm.h"
+
+const __RegisterPrototype __VOID_REGISTER_PROTOTYPE = {type_name : "void"};
+const __RegisterPrototype __BOOL_REGISTER_PROTOTYPE = {type_name : "bool"};
+const __RegisterPrototype __I32_REGISTER_PROTOTYPE = {type_name : "i32"};
+const __RegisterPrototype __I64_REGISTER_PROTOTYPE = {type_name : "i64"};
+const __RegisterPrototype __B32_REGISTER_PROTOTYPE = {type_name : "b32"};
+const __RegisterPrototype __B64_REGISTER_PROTOTYPE = {type_name : "b64"};
+const __RegisterPrototype __F32_REGISTER_PROTOTYPE = {type_name : "f32"};
+const __RegisterPrototype __F64_REGISTER_PROTOTYPE = {type_name : "f64"};
