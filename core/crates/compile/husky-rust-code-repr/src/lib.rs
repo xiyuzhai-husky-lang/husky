633bda1e2f21f081b3115a4c0d186ff290afb50b
Xiyu Zhai <xiyuzhai@mit.edu>
2022-07-27 16:32:51 -0400

    feat: factor into repr and gen

--- core/crates/compile/husky-rust-code-repr/src/lib.rs
+++ core/crates/compile/husky-rust-code-repr/src/lib.rs
@@ -1,14 +1,70 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::fmt::Display;
+
+use convert_case::{Case, Casing};
+
+pub struct PrimitiveTypeRegistration<'a> {
+    pub ty: &'a str,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<'a> Display for PrimitiveTypeRegistration<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let ty = self.ty;
+        let uppercase_ty = ty.to_uppercase();
+        let result = match ty {
+            "void" => "false",
+            "bool" => "data",
+            "i32" | "i64" | "b32" | "b64" => "data != 0",
+            "f32" | "f64" => "data != 0.0",
+            _ => panic!(),
+        };
+        write!(
+            f,
+            r#"
+// {ty}
+#[no_mangle]
+pub unsafe extern "C" fn __{ty}_primitive_value_to_bool(data: __RegisterData) -> bool {{
+    let data = data.as_{ty};
+    {result}
+}}
+#[no_mangle]
+pub unsafe extern "C" fn __{ty}_primitive_value_to_box(data: __RegisterData) -> *mut () {{
+    let data = data.as_{ty};
+    let ptr: *mut {ty} = Box::<{ty}>::into_raw(Box::new(data));
+    ptr as *mut ()
+}}
+#[no_mangle]
+pub unsafe extern "C" fn __{ty}_drop(data: *mut ()) {{
+    Box::from_raw(data as *mut {ty});
+}}
+extern "C" {{
+    pub static __{uppercase_ty}_VTABLE: __RegisterVTable;
+}}
+"#
+        )
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub struct NonPrimitiveTypeRegistration<'a> {
+    pub ty: &'a str,
+}
+
+impl<'a> Display for NonPrimitiveTypeRegistration<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let ty = self.ty;
+        let snake_ty = ty.to_case(Case::Snake);
+        let upper_snake_ty = ty.to_case(Case::UpperSnake);
+        write!(
+            f,
+            r#"
+// {ty}
+#[no_mangle]
+pub unsafe extern "C" fn __{snake_ty}_drop(data: *mut ()) {{
+    Box::from_raw(data as *mut {ty});
+}}
+extern "C" {{
+    pub static __{upper_snake_ty}_VTABLE: __RegisterVTable;
+}}
+"#
+        )
     }
 }
--- core/crates/vm/husky-vm-interface/husky-vm-interface-code-gen/src/rust_code.rs
+++ core/crates/vm/husky-vm-interface/husky-vm-interface-code-gen/src/rust_code.rs
@@ -1,7 +1,7 @@
 use super::PRIMITIVE_TYPES;
 use crate::NONPRIMITIVE_BUILTIN_TYPES;
-use convert_case::{Case, Casing};
 use husky_print_utils::p;
+use husky_rust_code_repr::{NonPrimitiveTypeRegistration, PrimitiveTypeRegistration};
 use std;
 use std::fs::File;
 use std::io::prelude::*;
@@ -23,56 +23,11 @@ type b64 = u64;
     );
 
     for ty in PRIMITIVE_TYPES {
-        let uppercase_ty = ty.to_uppercase();
-        let result = match *ty {
-            "void" => "false",
-            "bool" => "data",
-            "i32" | "i64" | "b32" | "b64" => "data != 0",
-            "f32" | "f64" => "data != 0.0",
-            _ => panic!(),
-        };
-        write!(
-            buffer,
-            r#"
-// {ty}
-#[no_mangle]
-pub unsafe extern "C" fn __{ty}_primitive_value_to_bool(data: __RegisterData) -> bool {{
-    let data = data.as_{ty};
-    {result}
-}}
-#[no_mangle]
-pub unsafe extern "C" fn __{ty}_primitive_value_to_box(data: __RegisterData) -> *mut () {{
-    let data = data.as_{ty};
-    let ptr: *mut {ty} = Box::<{ty}>::into_raw(Box::new(data));
-    ptr as *mut ()
-}}
-#[no_mangle]
-pub unsafe extern "C" fn __{ty}_drop(data: *mut ()) {{
-    Box::from_raw(data as *mut {ty});
-}}
-extern "C" {{
-    pub static __{uppercase_ty}_VTABLE: __RegisterVTable;
-}}
-"#
-        )?
+        write!(buffer, "{}", PrimitiveTypeRegistration { ty })?
     }
 
     for ty in NONPRIMITIVE_BUILTIN_TYPES {
-        let snake_ty = ty.to_case(Case::Snake);
-        let upper_snake_ty = ty.to_case(Case::UpperSnake);
-        write!(
-            buffer,
-            r#"
-// {ty}
-#[no_mangle]
-pub unsafe extern "C" fn __{snake_ty}_drop(data: *mut ()) {{
-    Box::from_raw(data as *mut {ty});
-}}
-extern "C" {{
-    pub static __{upper_snake_ty}_VTABLE: __RegisterVTable;
-}}
-"#
-        )?
+        write!(buffer, "{}", NonPrimitiveTypeRegistration { ty })?
     }
     Ok(())
 }
