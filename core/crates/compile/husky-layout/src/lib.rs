f024cff2b0d5ad935452d30340bb52986afe6ed7
Xiyu Zhai <xiyuzhai@mit.edu>
2022-08-02 23:55:45 -0400

    new lib husky-layout

---
+++ core/crates/compile/husky-layout/src/lib.rs
@@ -0,0 +1,13 @@
+use std::alloc::{Layout, LayoutError};
+
+pub fn repr_c(fields: &[Layout]) -> Result<(Layout, Vec<usize>), LayoutError> {
+    let mut offsets = Vec::new();
+    let mut layout = Layout::from_size_align(0, 1)?;
+    for &field in fields {
+        let (new_layout, offset) = layout.extend(field)?;
+        layout = new_layout;
+        offsets.push(offset);
+    }
+    // Remember to finalize with `pad_to_align`!
+    Ok((layout.pad_to_align(), offsets))
+}
