411f4f9ec2493c64c086fea636e314e1b345173a
Xiyu Zhai <xiyuzhai@mit.edu>
2022-08-03 00:15:41 -0400

    impl Signalable for SampleId

--- core/crates/static/domains/ml/husky-datasets-protocol/src/lib.rs
+++ core/crates/static/domains/ml/husky-datasets-protocol/src/lib.rs
@@ -1,3 +1,5 @@
+#[cfg(feature = "signal_support")]
+use husky_signal::Signalable;
 #[cfg(feature = "serde_support")]
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
@@ -5,6 +7,9 @@ use std::sync::Arc;
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "serde_support", derive(Serialize, Deserialize))]
 pub struct SampleId(pub usize);
+
+#[cfg(feature = "signal_support")]
+impl Signalable for SampleId {}
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
 #[cfg_attr(feature = "serde_support", derive(Serialize, Deserialize))]
