df2e017ae54ecc465c06465755441182c1cf44fc
Xiyu Zhai <xiyuzhai@mit.edu>
2022-08-07 10:39:38 -0400

    new app husky

---
+++ core/crates/apps/husky/src/main.rs
@@ -0,0 +1,37 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(name = "husky-debugger")]
+#[clap(author = "Xiyu Zhai <dirac12345@gmail.com>")]
+pub struct HuskyCli {
+    #[clap(short, long, value_parser)]
+    verbose: bool,
+    #[clap(subcommand)]
+    command: HuskyCommands,
+}
+
+#[derive(Subcommand)]
+enum HuskyCommands {
+    /// compile and serve traces on given package
+    Debug {
+        #[clap(value_parser)]
+        package_dir: PathBuf,
+    },
+    /// serve traces on first package with error
+    Test {
+        #[clap(value_parser)]
+        packages_dir: PathBuf,
+    },
+    /// format
+    Fmt {
+        #[clap(value_parser)]
+        packages_dir: PathBuf,
+    },
+}
+
+fn main() {
+    let cli = HuskyCli::parse();
+    todo!()
+}
