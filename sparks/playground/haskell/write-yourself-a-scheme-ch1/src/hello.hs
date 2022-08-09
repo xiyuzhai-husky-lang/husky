cef9ebcb11f3cda095f90d80d504f9f6e81ef2bd
Xiyu Zhai <xiyuzhai@mit.edu>
2022-08-09 19:06:52 -0400

    try more haskell

--- sparks/playground/haskell/write-yourself-a-scheme-ch1/src/hello.hs
+++ sparks/playground/haskell/write-yourself-a-scheme-ch1/src/hello.hs
@@ -1,9 +1,18 @@
 module Main where
 
-import           System.Environment
+import           System.Environment (getArgs)
+import           Text.ParserCombinators.Parsec hiding (spaces)
 
 main :: IO ()
 main = do
   args <- getArgs
   putStrLn ("Hello, " ++ head args)
-  putStrLn ("Hello, " ++ head args)
+  putStrLn ("Hello, " ++ args !! 1)
+
+symbol :: Parser Char
+symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
+
+readExpr :: String -> String
+readExpr input = case parse symbol "lisp" input of
+  Left err  -> "No match: " ++ show err
+  Right val -> "Found value"
