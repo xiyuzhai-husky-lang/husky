2aa13a13f662bf916fa69d87819a1a87eba292ac
Xiyu Zhai <xiyuzhai@mit.edu>
2022-08-16 08:39:35 -0400

    make it explicit the source of stats

--- crates/trace/husky-trace-protocol/src/trace/stats.rs
+++ crates/trace/husky-trace-protocol/src/trace/stats.rs
@@ -5,11 +5,11 @@ use super::*;
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub enum TraceStats {
     Classification {
-        samples: usize,
-        arrivals: usize,
-        nulls: usize,
-        trues: usize,
-        falses: usize,
+        dev_samples: usize,
+        dev_arrivals: usize,
+        dev_nulls: usize,
+        dev_trues: usize,
+        dev_falses: usize,
     },
 }
 
