6148cea395b77076b1c819acd0c1ce5cfaf9f2d5
Xiyu Zhai <xiyuzhai@mit.edu>
2022-08-17 18:53:09 -0400

    impl: Display TraceId

--- crates/trace/husky-trace-protocol/src/trace/id.rs
+++ crates/trace/husky-trace-protocol/src/trace/id.rs
@@ -4,4 +4,10 @@ use husky_signal::Signalable;
 #[derive(Debug, Clone, Serialize, Deserialize, Copy, PartialEq, Eq, Hash)]
 pub struct TraceId(pub usize);
 
+impl std::fmt::Display for TraceId {
+    fn fmt(&self, f: &mut __private::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "tr#{}", self.0)
+    }
+}
+
 impl Signalable for TraceId {}
