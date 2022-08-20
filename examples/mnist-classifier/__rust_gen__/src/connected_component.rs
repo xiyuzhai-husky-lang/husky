b1b3659319bdff11565ab37606284979892d418b
Xiyu Zhai <xiyuzhai@mit.edu>
2022-08-20 14:22:46 -0400

    feat: reorganise mnist classifier code

--- examples/mnist-classifier/__rust_gen__/src/connected_component.rs
+++ examples/mnist-classifier/__rust_gen__/src/connected_component.rs
@@ -51,53 +51,6 @@ impl<'eval> __Registrable<'eval> for ConnectedComponent {
         todo!()
     }
 }
-pub(crate) fn connected_components<'eval>(
-    __ctx: &dyn __EvalContext<'eval>,
-) -> &'eval Vec<ConnectedComponent> {
-    let __feature = feature_ptr!(
-        __ctx,
-        "mnist_classifier::connected_component::connected_components"
-    );
-    if let Some(__result) = __ctx.opt_cached_feature(__feature) {
-        return __result
-            .unwrap()
-            .downcast_eval_ref(&__registration__::__VEC_CONNECTED_COMPONENT_VTABLE);
-    }
-    return __ctx
-        .cache_feature(
-            __feature,
-            Ok(__Register::new_box::<Vec<ConnectedComponent>>(
-                find_connected_components(&__input(__ctx)),
-                &__registration__::__VEC_CONNECTED_COMPONENT_VTABLE,
-            )),
-        )
-        .unwrap()
-        .downcast_eval_ref(&__registration__::__VEC_CONNECTED_COMPONENT_VTABLE);
-}
-pub(crate) fn major_connected_component<'eval>(
-    __ctx: &dyn __EvalContext<'eval>,
-) -> &'eval ConnectedComponent {
-    let __feature = feature_ptr!(
-        __ctx,
-        "mnist_classifier::connected_component::major_connected_component"
-    );
-    if let Some(__result) = __ctx.opt_cached_feature(__feature) {
-        return __result
-            .unwrap()
-            .downcast_eval_ref(&__registration__::__CONNECTED_COMPONENT_VTABLE);
-    }
-    return __ctx
-        .cache_feature(
-            __feature,
-            Ok(__Register::new_eval_ref::<ConnectedComponent>(
-                &(connected_components(__ctx)[(0) as usize]),
-                &__registration__::__CONNECTED_COMPONENT_VTABLE,
-            )
-            .into()),
-        )
-        .unwrap()
-        .downcast_eval_ref(&__registration__::__CONNECTED_COMPONENT_VTABLE);
-}
 
 pub(crate) fn horizontal_extend(a: u32, x: u32) -> u32 {
     let mut y = a & (x | (x << 1) | (x >> 1));
