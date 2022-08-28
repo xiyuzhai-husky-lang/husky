378285df7d1bc1cb232d9cf5372cbc21537b667a
Xiyu Zhai <xiyuzhai@mit.edu>
2022-08-27 20:33:48 -0400

    feat: impl `GenericF32`

--- gui/husky-tracer-gui-sycamore/src/components/figure_view/figure_content/generic_f32.rs
+++ gui/husky-tracer-gui-sycamore/src/components/figure_view/figure_content/generic_f32.rs
@@ -1,4 +1,7 @@
+mod scale;
+
 use super::*;
+use scale::*;
 
 #[derive(Prop)]
 pub struct GenericF32Props<'a> {
@@ -8,5 +11,55 @@ pub struct GenericF32Props<'a> {
 
 #[component]
 pub fn GenericF32<'a, G: Html>(scope: Scope<'a>, props: GenericF32Props<'a>) -> View<G> {
-    todo!()
+    let scale = GenericF32Scale::new(props.partitioned_samples);
+    let focus = create_signal(scope, None);
+    let mut points = vec![];
+    for (i, (_partition, samples)) in props.partitioned_samples.iter().enumerate() {
+        for (sample_id, value) in samples.iter() {
+            let circle = scale.circle(i, *value);
+            points.push(view! {scope, GenericF32Point {
+                focus,
+                sample_id: *sample_id,
+                circle
+            }})
+        }
+    }
+    let points = View::new_fragment(points);
+    view! {
+        scope,
+        div (
+            class="GenericF32",
+            style=props.dimension.cget().to_style(),
+        ) {
+            div (class="GenericF32PlotRegion") {
+                svg (
+                    class="GenericF32PlotRegion",
+                    viewBox=scale.svg_view_box()
+                ) {
+                    (points)
+                }
+            }
+            div (class="GenericF32VisualRegion") {}
+        }
+    }
+}
+
+#[derive(Prop)]
+pub struct GenericF32PointProps<'a> {
+    focus: &'a Signal<Option<SampleId>>,
+    sample_id: SampleId,
+    circle: CircleProps,
+}
+
+#[component]
+fn GenericF32Point<'a, G: Html>(scope: Scope<'a>, props: GenericF32PointProps<'a>) -> View<G> {
+    view! {
+        scope,
+        circle (
+            class = format!("ClassIndex{}", props.circle.class_index),
+            cx = {props.circle.cx},
+            cy = {props.circle.cy},
+            r = {props.circle.r}
+        )
+    }
 }
---
+++ gui/husky-tracer-gui-sycamore/src/components/figure_view/figure_content/generic_f32/scale.rs
@@ -0,0 +1,70 @@
+use super::*;
+use std::ops::Range;
+
+#[derive(Debug)]
+pub(super) struct GenericF32Scale {
+    partitions_len: usize,
+    value_min: f32,
+    value_max: f32,
+    value_padding: f32,
+    r: f32,
+}
+
+impl GenericF32Scale {
+    pub fn new(partitioned_samples: &[(PartitionDefnData, Vec<(SampleId, f32)>)]) -> Self {
+        let mut value_min = f32::MIN;
+        let mut value_max = f32::MAX;
+        for (_, samples) in partitioned_samples {
+            for (sample, value) in samples {
+                if *value < value_min {
+                    value_min = *value
+                } else if *value > value_max {
+                    value_max = *value
+                }
+            }
+        }
+        let value_padding = if value_min == value_max {
+            1.0
+        } else {
+            (value_max - value_min) * 0.05
+        };
+        Self {
+            partitions_len: partitioned_samples.len(),
+            value_min,
+            value_max,
+            value_padding,
+            r: 5.0,
+        }
+    }
+
+    pub fn normalize_class_index(&self, class_index: usize) -> f32 {
+        let middle = ((self.partitions_len - 1) as f32) / 2.0;
+        ((class_index as f32) - middle) / (self.partitions_len as f32)
+    }
+
+    pub fn normalize_value(&self, value: f32) -> f32 {
+        let middle = (self.value_max + self.value_min) / 2.0;
+        (value - middle) / (self.value_max - self.value_min + self.value_padding)
+    }
+
+    pub fn circle(&self, class_index: usize, value: f32) -> CircleProps {
+        let cx = 150. + 300. * self.normalize_class_index(class_index);
+        let cy = 500. + 1000. * self.normalize_value(value);
+        CircleProps {
+            class_index,
+            cx,
+            cy,
+            r: self.r,
+        }
+    }
+
+    pub fn svg_view_box(&self) -> &'static str {
+        "0 0 300 1000"
+    }
+}
+pub struct CircleProps {
+    pub(super) class_index: usize,
+    pub(super) cx: f32,
+    pub(super) cy: f32,
+    pub(super) r: f32,
+}
