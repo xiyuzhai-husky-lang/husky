fc6f9cdde6247b1d8e2370196f144c3c59a5b42e
Xiyu Zhai <xiyuzhai@mit.edu>
2022-09-01 21:06:43 -0400

    use lean to write specs and prove impl correctness

---
+++ specs/contract.lean
@@ -0,0 +1,26 @@
+inductive Qualifier where
+  | EvalRef
+  | Copyable
+
+inductive Contract where
+  | EvalRef
+  | Copyable
+  | Any
+
+def does_contract_contains_qualifer (contract: Contract)(qualifier: Qualifier) :Bool :=
+  match contract with
+  | Contract.EvalRef =>
+    match qualifier with
+    | Qualifier.EvalRef => true
+    | _ => false
+  | Contract.Copyable =>
+    match qualifier with
+    | Qualifier.Copyable => true
+    | _ => false
+  | Contract.Any => true
+
+example: ∀ qualifier : Qualifier, does_contract_contains_qualifer Contract.Any qualifier = true :=
+  fun qualifier: Qualifier =>
+  match qualifier  with
+  | Qualifier.EvalRef =>  rfl
+  | Qualifier.Copyable => rfl
