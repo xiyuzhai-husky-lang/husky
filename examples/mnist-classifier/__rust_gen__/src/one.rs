1f77a9f3bfe6f493a9ab2ede9bf3d12b9ad6a429
Xiyu Zhai <xiyuzhai@mit.edu>
2022-09-04 16:07:24 -0400

    save

--- examples/mnist-classifier/__rust_gen__/src/one.rs
+++ examples/mnist-classifier/__rust_gen__/src/one.rs
@@ -35,7 +35,7 @@ pub(crate) fn one_fermi_match<'eval>(
     return __ctx
         .cache_feature(
             __feature,
-            Ok(__Register::new_box::<crate::fermi::FermiMatchResult<'eval>>(crate::fermi::fermi_match(&crate::major::major_concave_components(__ctx), &vec![ThickFp::__new_base(downmost as fn(&'static crate::line_segment_sketch::concave_component::ConcaveComponent<'static>)->Option<f32>), ThickFp::__new_base(upmost as fn(&'static crate::line_segment_sketch::concave_component::ConcaveComponent<'static>)->Option<f32>), ThickFp::__new_base(downmost as fn(&'static crate::line_segment_sketch::concave_component::ConcaveComponent<'static>)->Option<f32>)], __ctx), &__registration__::__FERMI_MATCH_RESULT_VTABLE)),
+            Ok(__Register::new_box::<crate::fermi::FermiMatchResult<'eval>>(crate::fermi::fermi_match(&crate::major::major_concave_components(__ctx), &vec![ThickFp::__new_base(downmost as fn(&'static crate::line_segment_sketch::concave_component::ConcaveComponent<'static>)->Option<f32>), ThickFp::__new_base(upmost as fn(&'static crate::line_segment_sketch::concave_component::ConcaveComponent<'static>)->Option<f32>), ThickFp::__new_base(hat as fn(&'static crate::line_segment_sketch::concave_component::ConcaveComponent<'static>)->Option<f32>)], __ctx), &__registration__::__FERMI_MATCH_RESULT_VTABLE)),
         )
         .unwrap()
         .downcast_eval_ref(&__registration__::__FERMI_MATCH_RESULT_VTABLE);
@@ -52,5 +52,13 @@ pub(crate) fn downmost<'eval>(
 ) -> Option<f32> {
     let dp = cc.displacement();
     normal_require!(dp.y <= 0f32);
-    return Some(-dp.y);
+    return Some(-cc.end().y);
+}
+pub(crate) fn hat<'eval>(
+    cc: &'eval crate::line_segment_sketch::concave_component::ConcaveComponent<'eval>,
+) -> Option<f32> {
+    let dp = cc.displacement();
+    normal_require!(dp.y < 0f32);
+    normal_require!(dp.x < 0f32);
+    return Some(-dp.y - dp.x);
 }
