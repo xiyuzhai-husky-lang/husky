edbf980b36e075c86f36e0ceb57dc6168fbb7550
Xiyu Zhai <xiyuzhai@mit.edu>
2022-09-06 17:18:17 -0400

    write specs for ast

--- specs/ast.lean
+++ specs/ast.lean
@@ -6,6 +6,49 @@ structure RawStmt
 
 inductive UseVariant
 
+inductive RawExprVariant
+  | Variable
+  | FrameVariable
+  | ThisValue
+  | ThisField
+  | Unrecognized
+  | Entity
+  | PrimitiveLiteral
+  | Bracketed
+  | Opn
+  | Lambda
+
+namespace RawExprVariant
+def subexprs (variant : RawExprVariant) : List RawExpr :=
+  match variant with
+  | Variable => []
+  | FrameVariable => []
+  | ThisValue => []
+  | ThisField => []
+  | Unrecognized => []
+  | Entity => sorry
+  | PrimitiveLiteral => []
+  | Bracketed => sorry
+  | Opn => sorry
+  | Lambda => sorry
+end RawExprVariant
+
+namespace RawExpr
+def valid (range : Range) (paradigm : Paradigm) (variant: RawExprVariant) : Prop := sorry
+end RawExpr
+
+inductive Paradigm
+  | LazyFunctional
+  | EagerFunctional
+  | EagerProcedural
+
+structure RawExpr where
+  range : Range
+  paradigm : Paradigm
+  variant : RawExprVariant
+  hvalid: RawExpr.valid range paradigm variant
+
+
 inductive AstVariant where
   | TypeDefnHead
   | MainDefnHead
@@ -19,6 +62,5 @@ inductive AstVariant where
   | Visual
 
 structure Ast where
-  file : File
   range : Range
   variant : AstVariant
