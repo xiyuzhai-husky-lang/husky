7f9afa342f8ee5a07346582c91114697f3473d39
Xiyu Zhai <xiyuzhai@mit.edu>
2022-09-08 16:50:28 -0400

    try lean4

---
+++ specs/HuskyLeanSpecs/qualifier.lean
@@ -0,0 +1,29 @@
+inductive RefQualifier
+  | None
+  | EvalRef
+  | TempRef
+  | TempRefMut
+
+namespace RefQualifier
+instance : BEq RefQualifier where
+  beq
+    | None, None => true
+    | EvalRef, EvalRef => true
+    | TempRef, TempRef => true
+    | TempRefMut, TempRefMut => true
+    | _, _ => false
+end RefQualifier
+
+def all_ref_qualifiers : List RefQualifier :=
+  [RefQualifier.None, RefQualifier.EvalRef, RefQualifier.TempRef, RefQualifier.TempRefMut]
+
+def no_dups[BEq α](list : List α) : Bool := list.eraseDups == list
+
+def is_enumeration[BEq α](list: List α) : Prop :=
+  no_dups list ∧ (∀ a : α, list.contains a)
+
+theorem haha : is_enumeration all_ref_qualifiers := by
+  apply And.intro
+  apply rfl
+  simp
+  sorry
