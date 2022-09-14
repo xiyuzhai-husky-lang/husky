0b0988da28e0cb574daa25090b5c63ab4f35298c
Xiyu Zhai <xiyuzhai@mit.edu>
2022-09-14 17:02:59 -0400

    feat: prove SpecialToken is enumerable in Lean4

--- Specs/abstraction/Enumerable.lean
+++ Specs/abstraction/Enumerable.lean
@@ -1,4 +1,4 @@
-def no_dups[DecidableEq α](list : List α) : Bool := list.eraseDups == list
+def no_dups[DecidableEq α](list : List α) : Bool := list.eraseDups = list
 
 def valid_enumeration[DecidableEq  α](list: List α) : Prop :=
   no_dups list ∧ (∀ a : α, list.contains a)
