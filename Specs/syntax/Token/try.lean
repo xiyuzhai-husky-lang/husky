dcd9acf7a6524da23c142059c11478c1e64063f2
Xiyu Zhai <xiyuzhai@mit.edu>
2022-09-14 23:32:28 -0400

    save

---
+++ Specs/syntax/Token/try.lean
@@ -0,0 +1,30 @@
+inductive SimpleToken where
+  | A
+  | BB
+  | Custom : Char -> SimpleToken
+
+namespace SimpleToken
+def code : SimpleToken -> List Char
+  | A => "A".toList
+  | BB => "BB".toList
+  | Custom c => [c]
+
+def writeCode : List SimpleToken -> List Char
+  | [] => []
+  | a::as => a.code.append (writeCode as)
+
+def parseCode : List Char -> List SimpleToken
+  | [] => []
+  | 'A'::as => [A].append (parseCode as)
+  | 'B'::'B'::as => [BB].append (parseCode as)
+  | c::as => [Custom c].append (parseCode as)
+
+theorem t : ∀ tokens : List SimpleToken, tokens = parseCode (writeCode tokens)
+  | [] => rfl
+  | A::as => by
+    simp[writeCode]
+    simp[code]
+    simp[parseCode]
+    assumption
+  | _ => sorry
+end SimpleToken
