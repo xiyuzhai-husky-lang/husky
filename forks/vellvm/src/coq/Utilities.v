bbbd5ea1eb08f8078ae89cd6e2e6b9bf6e10f576
Xiyu Zhai <xiyuzhai@mit.edu>
2022-09-15 08:31:34 -0400

    fork vellvm

---
+++ forks/vellvm/src/coq/Utilities.v
@@ -0,0 +1,15 @@
+(* Re-export of the main utilities used accross the development.
+   Use `From Vellvm Require Import Utils.` to get most utilities in scope.
+
+   Note: We avoid as much as possible to import notations. You can therefore import 
+   additionally the following modules:
+   - `Import AlistNotations.` for notations related to `alist` used to lookup blocks.
+ *)
+
+From Vellvm Require Export
+     Utils.Tactics
+     Utils.Util
+     Utils.AListFacts
+     Utils.ListUtil
+     Utils.Error
+     Utils.PropT.
