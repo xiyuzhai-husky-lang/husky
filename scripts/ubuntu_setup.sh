267802f8d12b4fa65ae85d6aa81f948faf83b39c
xiyu <dirac12345@gmail.com>
2022-09-22 10:42:30 -0400

    save

--- scripts/ubuntu_setup.sh
+++ scripts/ubuntu_setup.sh
@@ -4,6 +4,8 @@ sudo apt install curl
 if ["$(which cargo)" = ""]; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
+cargo install trunk
+rustup target add wasm32-unknown-unknown
 if ["$(which npm)" = ""]; then
     curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
     sudo apt-get install -y nodejs
