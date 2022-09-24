ff5cd2ba590ad1a460998f3cc06737f9d2cfdb67
xiyu <dirac12345@gmail.com>
2022-09-24 18:40:20 -0400

    write specs

--- Specs/vm.lean
+++ Specs/vm.lean
@@ -1,3 +1,7 @@
+import Specs.vm.Instruction
+import Specs.vm.Register
+import Specs.vm.VMStack
+
 import Specs.abstraction.Book
 
 def ch_vm : Chapter := { title := "Virtual Machine", sections := [] }
---
+++ Specs/vm/Register.lean
@@ -0,0 +1,1 @@
+structure __Register
