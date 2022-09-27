7ab740cc9e6162ba6352ce2a33e2e00e837c38db
xiyu <dirac12345@gmail.com>
2022-09-27 00:21:07 -0400

    feat: finally make coq work

--- Specs/concepts/Ty.lean
+++ Specs/concepts/Ty.lean
@@ -28,3 +28,32 @@ structure OrderedF32
   deriving DecidableEq
 structure OrderedF64
   deriving DecidableEq
+
+structure TyUniverse where
+  raw : Nat
+
+structure NonEmptyList (α : Type) where
+  a : α
+  as : List α
+
+theorem haha : sizeOf α < sizeOf (NonEmptyList α) := sorry
+
+namespace NonEmptyList
+def maxf [LT β] [DecidableRel (@LT.lt β _)] (list : NonEmptyList α) (f : α -> β) : β :=
+  match (list.as.map f).maximum? with
+  | some v => max v (f list.a)
+  | none => f list.a
+end NonEmptyList
+
+inductive Ty
+  | PrimitiveTy (ty : PrimitiveTy)
+  | Struct (fields : NonEmptyList (String × Ty))
+  | TypeTy (u : TyUniverse)
+
+def Ty.universe : Ty -> TyUniverse
+  | PrimitiveTy _ => { raw := 0 }
+  | Struct fields => { raw := (fields.maxf fun field => field.2.universe.raw) }
+  | TypeTy u => { raw := u.raw + 1 }
+  termination_by Ty.universe ty => ty
+
+def Ty.type_ty (ty : Ty) : Ty := Ty.TypeTy ty.universe
--- sparks/playground/Coq/adam-chlipala/hw0.v
+++
@@ -1,5 +0,0 @@
-Check (1 + 1 = 2)
-
-Inductive list : Set :=
-  | nil : list
-  | cons : nat -> list -> list.
