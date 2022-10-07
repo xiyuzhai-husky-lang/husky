53dbc54944a578db283f7e25a36203e50ad8c5ef
xiyu <dirac12345@gmail.com>
2022-10-07 12:35:10 -0400

    play around with hover

--- crates/ide/husky-hover-contents/src/query.rs
+++ crates/ide/husky-hover-contents/src/query.rs
@@ -1,7 +1,17 @@
-use lsp_types::HoverContents;
+use lsp_types::{HoverContents, MarkupContent, MarkupKind};
 
 pub trait HoverContentsQuery {
-    fn hover_content(&self) -> HoverContents {
-        todo!()
+    fn hover_contents(&self) -> Option<HoverContents> {
+        Some(HoverContents::Markup(MarkupContent {
+            kind: MarkupKind::Markdown,
+            value: [
+                "# Deep learning is Dead",
+                "Michael Jordan is Goat",
+                "```typescript",
+                "someCode();",
+                "```",
+            ]
+            .join("\n"),
+        }))
     }
 }
