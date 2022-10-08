1a9a67c70d2d5f2133f937ce62cd02d94ea84839
xiyu <dirac12345@gmail.com>
2022-10-08 16:38:21 -0400

    save

--- crates/concept/husky-term/src/entity/root.rs
+++ crates/concept/husky-term/src/entity/root.rs
@@ -6,26 +6,26 @@ use RootBuiltinIdentifier::*;
 
 impl TermEntity {
     pub(crate) fn i32(db: &dyn TermQuery) -> TermPtr {
-        Self::root_entity(db, I32)
+        Self::root_builtin_entity(db, I32)
     }
 
     pub(crate) fn i64(db: &dyn TermQuery) -> TermPtr {
-        Self::root_entity(db, I64)
+        Self::root_builtin_entity(db, I64)
     }
 
     pub(crate) fn module(db: &dyn TermQuery) -> TermPtr {
-        Self::root_entity(db, ModuleType)
+        Self::root_builtin_entity(db, ModuleType)
     }
 
     pub(crate) fn trai(db: &dyn TermQuery) -> TermPtr {
-        Self::root_entity(db, TraitType)
+        Self::root_builtin_entity(db, TraitType)
     }
 
     pub(crate) fn std(db: &dyn TermQuery) -> TermPtr {
-        Self::root_entity(db, Std)
+        Self::root_builtin_entity(db, Std)
     }
 
-    fn root_entity(db: &dyn TermQuery, ident: RootBuiltinIdentifier) -> TermPtr {
+    fn root_builtin_entity(db: &dyn TermQuery, ident: RootBuiltinIdentifier) -> TermPtr {
         db.it_term(
             Self {
                 path: db.it_root_entity_path(ident.into()),
