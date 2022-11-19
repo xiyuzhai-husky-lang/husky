53f21286d84328b1afdc772d9358dac38ee3019d
xiyu <dirac12345@gmail.com>
2022-11-19 09:57:47 -0500

    update `husky-vfs`

--- crates/fs/husky-vfs/src/lib.rs
+++ crates/fs/husky-vfs/src/lib.rs
@@ -1,38 +1,16 @@
 // use timed_salsa::jar::Jar;
 
-use std::path::PathBuf;
+mod file;
 
-#[derive(Debug)]
-pub struct HuskyFile {
-    path: PathBufItd,
-    content: String,
-}
+use file::*;
+use std::path::PathBuf;
 
 #[timed_salsa::interned]
 pub struct PathBufItd {
     raw: PathBuf,
 }
 
-// todo: different Durabilities
-#[timed_salsa::input]
-pub struct LibraryFile {
-    #[return_ref]
-    inner: HuskyFile,
-}
-
-#[timed_salsa::input]
-pub struct PublishedFile {
-    #[return_ref]
-    inner: HuskyFile,
-}
-
-#[timed_salsa::input]
-pub struct UserFile {
-    #[return_ref]
-    inner: HuskyFile,
-}
-
 #[timed_salsa::jar(db = Db)]
-pub struct Jar(PathBufItd, LibraryFile, PublishedFile, UserFile);
+pub struct Jar(PathBufItd, HuskyFile);
 
 pub trait Db: timed_salsa::DbWithJar<Jar> {}
