6690985c9f9fb269fbadbf90cbb10b873d466322
xiyu <dirac12345@gmail.com>
2022-11-19 09:29:38 -0500

    try new salsa

--- crates/fs/husky-vfs/src/lib.rs
+++ crates/fs/husky-vfs/src/lib.rs
@@ -1,14 +1,38 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// use timed_salsa::jar::Jar;
+
+use std::path::PathBuf;
+
+#[derive(Debug)]
+pub struct HuskyFile {
+    path: PathBufItd,
+    content: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[timed_salsa::interned]
+pub struct PathBufItd {
+    raw: PathBuf,
 }
+
+// todo: different Durabilities
+#[timed_salsa::input]
+pub struct LibraryFile {
+    #[return_ref]
+    inner: HuskyFile,
+}
+
+#[timed_salsa::input]
+pub struct PublishedFile {
+    #[return_ref]
+    inner: HuskyFile,
+}
+
+#[timed_salsa::input]
+pub struct UserFile {
+    #[return_ref]
+    inner: HuskyFile,
+}
+
+#[timed_salsa::jar(db = Db)]
+pub struct Jar(PathBufItd, LibraryFile, PublishedFile, UserFile);
+
+pub trait Db: timed_salsa::DbWithJar<Jar> {}
