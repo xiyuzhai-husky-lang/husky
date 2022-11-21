5a3820bd1daedf409e2efdccb2a4616219583ebe
xiyu <dirac12345@gmail.com>
2022-11-20 20:21:50 -0500

    impl form

--- crates/syntax/husky-defn/src/form.rs
+++ crates/syntax/husky-defn/src/form.rs
@@ -2,5 +2,27 @@ use crate::*;
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct FormDefn {
+    kind: FormDefnKind,
     body: ExprIdx,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum FormDefnKind {
+    Function(EagerParadigm),
+    Method(EagerParadigm),
+    Feature(Paradigm),
+    TrackedFunction(Paradigm),
+    Morphism,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Paradigm {
+    Eager(EagerParadigm),
+    Def,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum EagerParadigm {
+    Func,
+    Proc,
 }
