7c02cf353faabe239c23ea653e45d667174b1989
xiyu <dirac12345@gmail.com>
2022-11-23 14:52:56 -0500

    save

--- crates/kernel/husky-term/src/curry.rs
+++ crates/kernel/husky-term/src/curry.rs
@@ -77,41 +77,41 @@ impl<'a> TermContext<'a> {
     }
 }
 
-#[test]
-fn test_curry() {
-    todo!()
-    // let db = TermTestsDb::new();
-    // let menu = db.term_menu();
-    // let ctx = TermContext::new(&db, &menu);
-    // let i32_to_i32 = ctx
-    //     .curry(TermCurryVariant::Conceptual, menu.i32(), menu.i32())
-    //     .unwrap();
-    // assert_eq!(i32_to_i32.to_string(), "i32 -> i32");
-    // let i64_to_i64 = ctx
-    //     .curry(TermCurryVariant::Conceptual, menu.i64(), menu.i64())
-    //     .unwrap();
-    // assert_eq!(i64_to_i64.to_string(), "i64 -> i64");
-    // let f32_to_f32 = ctx
-    //     .curry(TermCurryVariant::Conceptual, menu.f32(), menu.f32())
-    //     .unwrap();
-    // assert_eq!(f32_to_f32.to_string(), "f32 -> f32");
-    // let f64_to_f64 = ctx
-    //     .curry(TermCurryVariant::Conceptual, menu.f64(), menu.f64())
-    //     .unwrap();
-    // assert_eq!(f64_to_f64.to_string(), "f64 -> f64");
-    // let b32_to_b32 = ctx
-    //     .curry(TermCurryVariant::Conceptual, menu.b32(), menu.b32())
-    //     .unwrap();
-    // assert_eq!(b32_to_b32.to_string(), "b32 -> b32");
-    // let b64_to_b64 = ctx
-    //     .curry(TermCurryVariant::Conceptual, menu.b64(), menu.b64())
-    //     .unwrap();
-    // assert_eq!(b64_to_b64.to_string(), "b64 -> b64");
-    // let bool_to_bool = ctx
-    //     .curry(TermCurryVariant::Conceptual, menu.bool(), menu.bool())
-    //     .unwrap();
-    // assert_eq!(bool_to_bool.to_string(), "bool -> bool");
-}
+// #[test]
+// fn test_curry() {
+//     todo!()
+//     // let db = TermTestsDb::new();
+//     // let menu = db.term_menu();
+//     // let ctx = TermContext::new(&db, &menu);
+//     // let i32_to_i32 = ctx
+//     //     .curry(TermCurryVariant::Conceptual, menu.i32(), menu.i32())
+//     //     .unwrap();
+//     // assert_eq!(i32_to_i32.to_string(), "i32 -> i32");
+//     // let i64_to_i64 = ctx
+//     //     .curry(TermCurryVariant::Conceptual, menu.i64(), menu.i64())
+//     //     .unwrap();
+//     // assert_eq!(i64_to_i64.to_string(), "i64 -> i64");
+//     // let f32_to_f32 = ctx
+//     //     .curry(TermCurryVariant::Conceptual, menu.f32(), menu.f32())
+//     //     .unwrap();
+//     // assert_eq!(f32_to_f32.to_string(), "f32 -> f32");
+//     // let f64_to_f64 = ctx
+//     //     .curry(TermCurryVariant::Conceptual, menu.f64(), menu.f64())
+//     //     .unwrap();
+//     // assert_eq!(f64_to_f64.to_string(), "f64 -> f64");
+//     // let b32_to_b32 = ctx
+//     //     .curry(TermCurryVariant::Conceptual, menu.b32(), menu.b32())
+//     //     .unwrap();
+//     // assert_eq!(b32_to_b32.to_string(), "b32 -> b32");
+//     // let b64_to_b64 = ctx
+//     //     .curry(TermCurryVariant::Conceptual, menu.b64(), menu.b64())
+//     //     .unwrap();
+//     // assert_eq!(b64_to_b64.to_string(), "b64 -> b64");
+//     // let bool_to_bool = ctx
+//     //     .curry(TermCurryVariant::Conceptual, menu.bool(), menu.bool())
+//     //     .unwrap();
+//     // assert_eq!(bool_to_bool.to_string(), "bool -> bool");
+// }
 
 impl Into<TermData> for TermCurry {
     fn into(self) -> TermData {
