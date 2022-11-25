4325112fdb6ca55ac559a235c02e165003348c17
xiyu <dirac12345@gmail.com>
2022-11-24 23:34:08 -0500

    fix lean error; lean sucks at incremental computation btw

--- Specs/syntax/RawLiteralData.lean
+++ Specs/syntax/RawLiteralData.lean
@@ -2,15 +2,15 @@ import Specs.concepts
 
 inductive RawLiteralData 
    | Void
-   | Integer(value : i64)
-   | I32(value : i32)
-   | I64(value : i64)
-   | Float(value : OrderedF64)
-   | F32(value : OrderedF32)
-   | F64(value : OrderedF64)
-   | Bits(value : b64)
-   | B32(value : b32)
-   | B64(value : b64)
+  --  | Integer(value : i64)
+  --  | I32(value : i32)
+  --  | I64(value : i64)
+  --  | Float(value : OrderedF64)
+  --  | F32(value : OrderedF32)
+  --  | F64(value : OrderedF64)
+  --  | Bits(value : b64)
+  --  | B32(value : b32)
+  --  | B64(value : b64)
    | Bool(value : Bool)
 
 
