87a791be50132db99a1fac04a69cd298adde31b2
xiyu <dirac12345@gmail.com>
2022-11-25 18:28:49 -0500

    save

--- crates/lex/husky-token/src/storage.rs
+++ crates/lex/husky-token/src/storage.rs
@@ -2,17 +2,17 @@ use crate::Token;
 pub struct TokenStorage(Vec<Token>);
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct TokenIdxRange(pub std::ops::Range<usize>);
+pub struct TokenGroup(pub std::ops::Range<usize>);
 
-impl std::ops::Index<TokenIdxRange> for TokenStorage {
+impl std::ops::Index<TokenGroup> for TokenStorage {
     type Output = [Token];
 
-    fn index(&self, index: TokenIdxRange) -> &Self::Output {
+    fn index(&self, index: TokenGroup) -> &Self::Output {
         &self.0[index.0]
     }
 }
 
-impl std::ops::Deref for TokenIdxRange {
+impl std::ops::Deref for TokenGroup {
     type Target = std::ops::Range<usize>;
 
     fn deref(&self) -> &Self::Target {
