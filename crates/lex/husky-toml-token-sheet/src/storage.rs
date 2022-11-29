ec42d04b02b5b7fa4ff2aad6cdcbe84566ef7a79
xiyu <dirac12345@gmail.com>
2022-11-29 12:49:46 -0500

    save

---
+++ crates/lex/husky-toml-token-sheet/src/storage.rs
@@ -0,0 +1,33 @@
+use crate::*;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TomlTokens(Vec<TomlToken>);
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct TomlTokenGroup(std::ops::Range<usize>);
+
+impl TomlTokenGroup {
+    pub fn new(range: std::ops::Range<usize>) -> Self {
+        Self(range)
+    }
+
+    pub fn first(&self, sheet: &TomlTokenSheet) -> &TomlToken {
+        todo!()
+    }
+}
+
+impl std::ops::Index<TomlTokenGroup> for TomlTokens {
+    type Output = [TomlToken];
+
+    fn index(&self, index: TomlTokenGroup) -> &Self::Output {
+        &self.0[index.0]
+    }
+}
+
+impl std::ops::Deref for TomlTokenGroup {
+    type Target = std::ops::Range<usize>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
