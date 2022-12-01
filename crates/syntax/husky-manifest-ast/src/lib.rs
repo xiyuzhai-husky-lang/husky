5dbfed077a1a93ee7e4efb022fb4e023b1ac3fdf
xiyu <dirac12345@gmail.com>
2022-11-30 23:04:43 -0500

    save

--- crates/syntax/husky-manifest-ast/src/lib.rs
+++ crates/syntax/husky-manifest-ast/src/lib.rs
@@ -1,6 +1,14 @@
+use husky_text::TextRange;
 use salsa::DbWithJar;
 
 #[salsa::jar(db = ManifestAstDb)]
 pub struct ManifestAstJar();
 
 pub trait ManifestAstDb: DbWithJar<ManifestAstJar> {}
+
+pub struct ManifestAst {
+    variant: ManifestAstVariant,
+    range: TextRange,
+}
+
+pub enum ManifestAstVariant {}
