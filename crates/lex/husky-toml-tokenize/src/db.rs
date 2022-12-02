517bd4407d95d0664444f463dcf50836940c4355
xiyu <dirac12345@gmail.com>
2022-12-01 23:55:03 -0500

    save

--- crates/lex/husky-toml-tokenize/src/db.rs
+++ crates/lex/husky-toml-tokenize/src/db.rs
@@ -1,12 +1,13 @@
 use crate::*;
 use husky_package_path::{PackagePath, PackagePathDb};
+use husky_toml_token_text::TomlTokenText;
 use husky_vfs::{VfsDb, VfsResult};
 use salsa::DbWithJar;
 
 pub trait TomlTokenizeDb: DbWithJar<TomlTokenizeJar> + PackagePathDb + VfsDb {
     fn toml_tokenize(&self, input: &str) -> Vec<TomlToken>;
 
-    fn toml_token_sheet(&self, package: PackagePath) -> &VfsResult<Vec<TomlToken>>;
+    fn package_manifest_token_text(&self, package: PackagePath) -> &VfsResult<TomlTokenText>;
 }
 
 impl<T> TomlTokenizeDb for T
@@ -17,7 +18,7 @@ where
         TokenIter::new(self, input).collect()
     }
 
-    fn toml_token_sheet(&self, package: PackagePath) -> &VfsResult<Vec<TomlToken>> {
+    fn package_manifest_token_text(&self, package: PackagePath) -> &VfsResult<TomlTokenText> {
         package_manifest_toml_tokens(self, package)
     }
 }
--- crates/lex/husky-toml-tokenize/src/manifest.rs
+++ crates/lex/husky-toml-tokenize/src/manifest.rs
@@ -1,13 +1,14 @@
 use crate::*;
 use husky_package_path::PackagePath;
+use husky_toml_token_text::TomlTokenText;
 use husky_vfs::VfsResult;
 
 #[salsa::tracked(jar = TomlTokenizeJar, return_ref)]
 pub(crate) fn package_manifest_toml_tokens(
     db: &dyn TomlTokenizeDb,
     package: PackagePath,
-) -> VfsResult<Vec<TomlToken>> {
+) -> VfsResult<TomlTokenText> {
     let file = db.package_manifest_toml_file(package).unwrap();
     let file_content = db.file_content(file);
-    Ok(db.toml_tokenize(file_content))
+    Ok(TomlTokenText::new(db.toml_tokenize(file_content)))
 }
