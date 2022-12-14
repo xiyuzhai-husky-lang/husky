cae5c0f248fd8458988ea568e456338825695757
xiyu <dirac12345@gmail.com>
2022-12-14 12:11:23 -0500

    fix

--- crates/static/husky-root-static-defn/src/vec/pop_with_largest_opt_f32.rs
+++ crates/static/husky-root-static-defn/src/vec/pop_with_largest_opt_f32.rs
@@ -13,8 +13,18 @@ pub static VEC_POP_WITH_LARGEST_OPT_F32: EntityStaticDefn = EntityStaticDefn {
         output_ty: "?E",
         spatial_parameters: &[],
         method_static_defn_kind: MethodStaticDefnKind::TypeMethod,
-        opt_linkage: None,
+        opt_linkage: Some(transfer_linkage!(
+            virtual_vec_pop_with_largest_opt_f32,
+            none
+        )),
         output_liason: OutputModifier::Transfer,
     },
     dev_src: static_dev_src!(),
 };
+
+unsafe fn virtual_vec_pop_with_largest_opt_f32<'temp, 'eval>(
+    values: &mut [__Register<'eval>],
+    opt_ctx: Option<&dyn __EvalContext<'eval>>,
+) -> __Register<'eval> {
+    todo!()
+}
