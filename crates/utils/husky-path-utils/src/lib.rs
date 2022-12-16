06cc1450620112671565c52293049e6582cedf6d
xiyu <dirac12345@gmail.com>
2022-12-15 23:10:26 -0500

    save

--- crates/utils/husky-path-utils/src/lib.rs
+++ crates/utils/husky-path-utils/src/lib.rs
@@ -5,6 +5,7 @@ use husky_check_utils::should_satisfy;
 use husky_print_utils::p;
 pub use module_tree::*;
 pub use rel::*;
+use relative_path::{RelativePath, RelativePathBuf};
 pub use std::path::{Path, PathBuf};
 
 pub fn path_has_file_name(path: &Path, name: &str) -> bool {
@@ -45,7 +46,7 @@ pub fn collect_package_dirs_deprecated(dir: &Path) -> Vec<PathBuf> {
     }
 }
 
-pub fn collect_package_dirs(dir: PathBuf) -> Vec<PathBuf> {
+pub fn collect_package_dirs(dir: &Path) -> Vec<PathBuf> {
     should_satisfy!(&dir, |dir: &Path| dir.is_dir());
     let mut pack_paths = vec![];
     collect_package_dirs_aux(dir, &mut pack_paths);
@@ -53,17 +54,48 @@ pub fn collect_package_dirs(dir: PathBuf) -> Vec<PathBuf> {
     pack_paths
 }
 
-fn collect_package_dirs_aux(dir: PathBuf, pack_paths: &mut Vec<PathBuf>) {
+fn collect_package_dirs_aux(dir: &Path, pack_paths: &mut Vec<PathBuf>) {
     let manifest_path = dir.join("Corgi.toml");
     for entry in std::fs::read_dir(&dir).unwrap() {
         let entry = entry.unwrap();
         let subpath = entry.path();
         if subpath.is_dir() {
-            collect_package_dirs_aux(subpath, pack_paths)
+            collect_package_dirs_aux(&subpath, pack_paths)
         }
     }
     if manifest_path.exists() {
-        pack_paths.push(dir)
+        pack_paths.push(dir.to_owned())
+    }
+}
+
+pub fn collect_package_relative_dirs(base: &Path) -> Vec<RelativePathBuf> {
+    should_satisfy!(&base, |dir: &Path| dir.is_dir());
+    let mut pack_paths = vec![];
+    let dir = RelativePathBuf::from(".");
+    collect_package_relative_dirs_aux(base, &dir, &mut pack_paths);
+    pack_paths.sort();
+    pack_paths
+}
+
+fn collect_package_relative_dirs_aux(
+    base: &Path,
+    dir: &RelativePath,
+    pack_paths: &mut Vec<RelativePathBuf>,
+) {
+    let manifest_path = dir.join("Corgi.toml");
+    for entry in std::fs::read_dir(&dir.to_logical_path(base)).unwrap() {
+        let entry = entry.unwrap();
+        let subpath = entry.path();
+        if subpath.is_dir() {
+            collect_package_relative_dirs_aux(
+                base,
+                &dir.join(subpath.file_name().unwrap().to_str().unwrap()),
+                pack_paths,
+            )
+        }
+    }
+    if manifest_path.to_logical_path(base).exists() {
+        pack_paths.push(dir.to_owned())
     }
 }
 
@@ -82,7 +114,7 @@ fn collect_package_dirs_works() {
             "/home/xiyuzhai/repos/husky/library/std",
         ]
     "#]]
-    .assert_debug_eq(&collect_package_dirs(library_dir))
+    .assert_debug_eq(&collect_package_dirs(&library_dir))
 }
 
 pub fn collect_all_source_files(dir: PathBuf) -> Vec<PathBuf> {
@@ -104,4 +136,36 @@ pub fn collect_all_source_files(dir: PathBuf) -> Vec<PathBuf> {
 
 pub fn cargo_manifest_dir() -> Result<PathBuf, std::env::VarError> {
     std::env::var("CARGO_MANIFEST_DIR").map(|s| s.into())
+}
+
+pub fn derive_library_dir_from_cargo_manifest_dir() -> PathBuf {
+    let cargo_manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let mut library_parent_dir: &Path = cargo_manifest_dir.as_ref();
+    loop {
+        let library_dir = library_parent_dir.join("library");
+        if library_dir.exists() {
+            break library_dir;
+        }
+        if let Some(new_library_parent_dir) = library_parent_dir.parent() {
+            library_parent_dir = new_library_parent_dir
+        } else {
+            todo!()
+        }
+    }
+}
+
+pub fn derive_examples_dir_from_cargo_manifest_dir() -> PathBuf {
+    let cargo_manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let mut parent_dir: &Path = cargo_manifest_dir.as_ref();
+    loop {
+        let library_dir = parent_dir.join("library");
+        if library_dir.exists() {
+            break parent_dir.join("examples");
+        }
+        if let Some(new_parent_dir) = parent_dir.parent() {
+            parent_dir = new_parent_dir
+        } else {
+            todo!()
+        }
+    }
 }
--- crates/utils/husky-vfs-test-utils/src/lib.rs
+++ crates/utils/husky-vfs-test-utils/src/lib.rs
@@ -9,45 +9,50 @@ use husky_vfs::*;
 use std::path::PathBuf;
 
 pub trait VfsTestSupport: VfsDb {
-    fn mimic_dir(&self, module: EntityPath) -> PathBuf;
-    fn mimic_path(&self, module: EntityPath) -> PathBuf;
-    fn run_module_expect_tests<'a, R>(f: impl Fn(&Self, EntityPath) -> &R)
+    fn run_module_expect_tests<'a, R>(name: &str, f: impl Fn(&Self, EntityPath) -> &R)
     where
         Self: Default + 'a,
         R: std::fmt::Debug;
+}
+
+struct TestPathResolver<'a> {
+    db: &'a dyn VfsDb,
+    name: &'a str,
+    package_expects_dir: PathBuf,
+}
+
+impl<'a> TestPathResolver<'a> {
+    fn resolve_dir(&self, module: EntityPath) -> PathBuf {
+        match self.db.dt_entity_path(module) {
+            EntityPathData::Crate { package, kind } => self.package_expects_dir.join(self.name),
+            EntityPathData::Childpath { parent, ident } => {
+                self.resolve_dir(parent).join(self.db.dt_ident(ident))
+            }
+        }
+    }
+
+    fn resolve_path(&self, module: EntityPath) -> PathBuf {
+        let dir = self.resolve_dir(module);
+        dir.join(format!(
+            "{}.{}.txt",
+            match self.db.dt_entity_path(module) {
+                EntityPathData::Crate { package, kind } => match kind {
+                    CratePathKind::Library => "lib",
+                    CratePathKind::Main => "main",
+                    CratePathKind::Binary(_) => todo!(),
+                },
+                EntityPathData::Childpath { ident, .. } => self.db.dt_ident(ident),
+            },
+            self.name
+        ))
+    }
 }
 
 impl<T> VfsTestSupport for T
 where
     T: VfsDb,
 {
-    fn mimic_dir(&self, module: EntityPath) -> PathBuf {
-        match self.dt_entity_path(module) {
-            EntityPathData::Crate { package, kind } => {
-                self.package_dir(package).as_ref().unwrap().join("ast")
-            }
-            EntityPathData::Childpath { parent, ident } => {
-                self.mimic_dir(parent).join(self.dt_ident(ident))
-            }
-        }
-    }
-
-    fn mimic_path(&self, module: EntityPath) -> PathBuf {
-        let dir = self.mimic_dir(module);
-        dir.join(format!(
-            "{}.ast.txt",
-            match self.dt_entity_path(module) {
-                EntityPathData::Crate { package, kind } => match kind {
-                    CratePathKind::Library => "lib",
-                    CratePathKind::Main => "main",
-                    CratePathKind::Binary(_) => todo!(),
-                },
-                EntityPathData::Childpath { ident, .. } => self.dt_ident(ident),
-            }
-        ))
-    }
-
-    fn run_module_expect_tests<'a, R>(f: impl Fn(&Self, EntityPath) -> &R)
+    fn run_module_expect_tests<'a, R>(name: &str, f: impl Fn(&Self, EntityPath) -> &R)
     where
         Self: Default + 'a,
         R: std::fmt::Debug,
@@ -56,15 +61,22 @@ where
         let cargo_manifest_dir = cargo_manifest_dir().unwrap();
         let examples_dir = cargo_manifest_dir.join("tests/examples");
         std::fs::create_dir_all(&examples_dir);
-        collect_package_dirs(examples_dir)
+
+        let base = &db.vfs_config().examples_dir();
+        collect_package_relative_dirs(base)
             .into_iter()
             .for_each(|path| {
-                let package =
-                    db.it_package_path(PackagePathData::Local(AbsolutePath::new(&path).unwrap()));
+                let package = db.it_package_path(PackagePathData::Local(
+                    AbsolutePath::new(&path.to_logical_path(base)).unwrap(),
+                ));
+                let resolver = TestPathResolver {
+                    db: &db,
+                    name,
+                    package_expects_dir: path.to_logical_path(&examples_dir),
+                };
                 for module in db.all_possible_modules(package).unwrap() {
-                    let path = db.mimic_path(module);
+                    let path = resolver.resolve_path(module);
                     std::fs::create_dir_all(path.parent().unwrap()).unwrap();
-                    p!(path);
                     expect_test::expect_file![path].assert_debug_eq(&f(&db, module))
                 }
             });
