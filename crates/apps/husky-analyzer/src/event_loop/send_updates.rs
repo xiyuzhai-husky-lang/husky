1f8a7524bbaa7428b7a1127b05c12d2f66d57a47
xiyu <dirac12345@gmail.com>
2022-12-20 16:39:54 -0500

    save

--- crates/apps/husky-analyzer/src/event_loop/send_updates.rs
+++ crates/apps/husky-analyzer/src/event_loop/send_updates.rs
@@ -1,18 +1,12 @@
 use crate::server::client_comm::ClientCommunicator;
 use crate::*;
 
-macro_rules! batch_into {
-    ($e:expr) => {{
-        $e.iter().map(|d| d.clone().into()).collect()
-    }};
-}
-
 pub(crate) fn send_updates(_db: &AnalyzerDB, _comm: &ClientCommunicator) {
     eprintln!("todo: send updates")
     // db.module_iter().for_each(|module| {
     //     db.diagnostics_reserve(module).release(|diagnostics| {
     //         if let Some(file) = db.module_file(module).ok() {
-    //             comm.send_diagnostics(db.url(file), batch_into!(diagnostics), None);
+    //             comm.send_diagnostics(db.url(file), diagnostics.iter().map(|d| d.clone().into()).collect(), None);
     //         }
     //     })
     // });
