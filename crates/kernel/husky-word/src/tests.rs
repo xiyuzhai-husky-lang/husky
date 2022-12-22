96b27df06f7e58bf2d78e4aec4b20a6b7b8c58af
xiyu <dirac12345@gmail.com>
2022-12-22 18:17:28 -0500

    simplify identifier debug

--- crates/kernel/husky-word/src/tests.rs
+++ crates/kernel/husky-word/src/tests.rs
@@ -35,9 +35,7 @@ fn ident_debug_works() {
     let db = DB::default();
     let haha = db.it_ident_borrowed("haha").unwrap();
     expect_test::expect![[r#"
-        Identifier(
-            "haha",
-        )
+        "haha"
     "#]]
     .assert_debug_eq(&haha.debug(&db));
 }
