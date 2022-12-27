77d8444de0864664cd4691618f76d98ff3921873
xiyu <dirac12345@gmail.com>
2022-12-27 02:29:12 -0500

    save

--- crates/syntax/husky-expr/src/parser.rs
+++ crates/syntax/husky-expr/src/parser.rs
@@ -43,17 +43,27 @@ impl<'a, 'b, 'c> ExprParser<'a, 'b, 'c> {
     }
 
     fn parse_all(mut self) -> ExprIdx {
+        let mut stop_at = None;
         while !self.tokens().is_empty() {
             let (token_idx, token) = self.token_iter.next_indexed().unwrap();
-            match self.accept_token(self.resolve_token(token_idx, token)) {
-                Ok(()) => (),
-                Err(e) => {
-                    p!(self.report_position());
-                    todo!("error = {e}")
+            if let Some(resolved_token) = self.resolve_token(token_idx, token) {
+                match self.accept_token(resolved_token) {
+                    Ok(()) => (),
+                    Err(e) => {
+                        p!(self.report_position());
+                        todo!("error = {e}")
+                    }
                 }
+            } else {
+                stop_at = Some(token_idx);
+                break;
             }
         }
         self.synthesize_all_above(Precedence::None).expect("todo");
+        if self.number_of_exprs() != 1 {
+            p!(self.stack);
+            todo!()
+        }
         should!(self.number_of_exprs() == 1);
         let last_expr = self.pop_expr().unwrap();
         self.arena.alloc_one(last_expr)
