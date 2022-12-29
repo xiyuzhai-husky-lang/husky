97c3937dec824335713fa81cb86f1889c99b232c
xiyu <dirac12345@gmail.com>
2022-12-29 18:58:44 -0500

    connect

--- crates/ide/husky-hover/src/db.rs
+++ crates/ide/husky-hover/src/db.rs
@@ -31,35 +31,9 @@ where
         pos: TextPosition,
     ) -> EntityTreeResult<Option<HoverResult>> {
         let token_sheet = self.token_sheet(module_path)?;
-        let token_idx = token_sheet.search_token_by_position(pos);
-        todo!()
-        // let entity_route_sheet = self.term_sheet(frange.file()).expect("todo");
-        // let (idx, expr) = entity_route_sheet
-        //     .ast_text()
-        //     .find_first_expr_with_end_after(frange.text_start())?;
-        // Some(HoverResult {
-        //     hover: lsp_types::Hover {
-        //         contents: HoverContents::Markup(MarkupContent {
-        //             kind: MarkupKind::Markdown,
-        //             value: format!(r#"{expr:?}"#),
-        //         }),
-        //         range: Some(frange.text_range().into()),
-        //     },
-        //     actions: vec![
-        //         CommandLinkGroup {
-        //             title: Some(format!("FirstCommandLinkeGroup")),
-        //             commands: vec![CommandLink {
-        //                 command: lsp_types::Command {
-        //                     command: format!("FirstCommandLinkeGroupFirstCommand"),
-        //                     title: format!("FirstCommand"),
-        //                     arguments: None,
-        //                 },
-        //                 tooltip: Some(format!("tooltip")),
-        //             }],
-        //         },
-        //         CommandLinkGroup::new_goto_types(),
-        //     ],
-        // })
+        let Some(token_idx) = token_sheet.search_token_by_position(pos)
+                else { return Ok(None) };
+        calc_hover_result(self, module_path, token_idx)
     }
 
     fn hover_config(&self) -> &HoverConfig {
