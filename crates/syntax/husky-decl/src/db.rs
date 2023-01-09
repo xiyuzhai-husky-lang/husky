cfd5668d6728f92126df919703da88d498d1a6e6
xinjiang <xinjiang@csu.edu.cn>
2023-01-09 20:46:09 +0800

    update paerser

--- crates/syntax/husky-decl/src/db.rs
+++ crates/syntax/husky-decl/src/db.rs
@@ -4,7 +4,7 @@ use husky_vfs::{ModulePath, VfsResult};
 use salsa::DbWithJar;
 
 pub trait DeclDb: DbWithJar<DeclJar> + ExprDb {
-    fn module_item_decl(&self, path: ModuleItemPath) -> EntityTreeResult<Decl>;
+    fn module_item_decl(&self, path: ModuleItemPath) -> DeclResult<Decl>;
     fn module_decl_sheet(&self, path: ModulePath) -> EntityTreeResult<&DeclSheet>;
 }
 
@@ -12,7 +12,7 @@ impl<Db> DeclDb for Db
 where
     Db: DbWithJar<DeclJar> + ExprDb,
 {
-    fn module_item_decl(&self, path: ModuleItemPath) -> EntityTreeResult<Decl> {
+    fn module_item_decl(&self, path: ModuleItemPath) -> DeclResult<Decl> {
         module_item_decl(self, path)
     }
     fn module_decl_sheet(&self, path: ModulePath) -> EntityTreeResult<&DeclSheet> {
