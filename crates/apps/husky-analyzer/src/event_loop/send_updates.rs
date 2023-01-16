74b0d46ee878c4999b1c0736e507f6c43b0e8fda
xiyu <dirac12345@gmail.com>
2023-01-15 23:15:15 -0500

    save

--- crates/apps/husky-analyzer/src/event_loop/send_updates.rs
+++ crates/apps/husky-analyzer/src/event_loop/send_updates.rs
@@ -12,12 +12,6 @@ pub(crate) fn send_updates(db: &AnalyzerDB, comm: &ClientCommunicator) {
     );
     for package_path in live_packages.iter() {
         let collect_probable_modules = db.collect_probable_modules(*package_path);
-        // ad hoc
-        assert!(collect_probable_modules.len() > 0);
-        eprintln!(
-            "send updates for probable modules {:?}",
-            &(collect_probable_modules.debug(db))
-        );
         for module_path in collect_probable_modules {
             comm.send_diagnostics(db, module_path)
         }
