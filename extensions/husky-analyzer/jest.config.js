f45595e3d669f039a064fd42d98276b215bd86ec
xiyu <dirac12345@gmail.com>
2023-02-20 09:33:57 -0500

    save

--- extensions/husky-analyzer/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    transform: {
-        "^.+\\.svelte$": "svelte-jester",
-        "^.+\\.js$": "babel-jest",
-    },
-    moduleFileExtensions: ["js", "svelte"],
-    testEnvironment: "jest-environment-jsdom"
-};
