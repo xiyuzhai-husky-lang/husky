ffcdf18132ed10315c83f8a0f04fec38af015ad3
xiyu <dirac12345@gmail.com>
2023-03-02 00:13:25 -0500

    remove `husky-*-expr`

--- crates/syntax/husky-runtime-expr/src/error.rs
+++
@@ -1,9 +0,0 @@
-use crate::*;
-
-#[derive(Debug, PartialEq, Eq)]
-#[salsa::derive_debug_with_db(db = EntityTreeDb)]
-pub enum RuntimeExprError {
-    Hellow,
-}
-
-pub type RuntimeExprResult<T> = Result<T, RuntimeExprError>;
--- crates/syntax/husky-runtime-expr/src/stmt.rs
+++
@@ -1,7 +0,0 @@
-use crate::*;
-
-pub enum RuntimeStmt {}
-
-pub type RuntimeStmtIdx = ArenaIdx<RuntimeStmt>;
-pub type RuntimeStmtArena = Arena<RuntimeStmt>;
-pub type RuntimeStmtIdxRange = ArenaIdxRange<RuntimeStmt>;
