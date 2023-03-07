1cc84864608ff2229b2c7e011f0a966a160f9f1f
xiyu <dirac12345@gmail.com>
2023-03-06 19:03:28 -0500

    clean up

--- crates/kernel/husky-valid-ty/src/db.rs
+++
@@ -1,6 +0,0 @@
-use crate::*;
-use husky_valid_term::RawTermDb;
-
-pub trait ValidTermDb: salsa::DbWithJar<ValidTypeJar> + RawTermDb {}
-
-impl<Db> ValidTermDb for Db where Db: salsa::DbWithJar<ValidTypeJar> + RawTermDb {}
