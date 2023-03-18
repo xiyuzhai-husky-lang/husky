5e52358f6bfed769c06ccc9c0f365bf1d3d219ad
xiyu <dirac12345@gmail.com>
2023-03-18 17:36:15 -0400

    save

--- crates/infer/husky-corgi-config/src/lib.rs
+++ crates/infer/husky-corgi-config/src/lib.rs
@@ -5,22 +5,31 @@ pub use db::*;
 use husky_vfs::*;
 
 #[salsa::jar(db = CorgiConfigDb)]
-pub struct CorgiConfigJar(package_registry_path);
+pub struct CorgiConfigJar(package_registry_path, CorgiConfig);
+
+#[salsa::tracked(db = CorgiConfigDb, jar = CorgiConfigJar)]
+pub struct CorgiConfig {}
 
 #[salsa::tracked(jar = CorgiConfigJar)]
 pub(crate) fn package_registry_path(
     db: &dyn CorgiConfigDb,
     package: PackagePath,
 ) -> VfsResult<RegistryPath> {
-    let corgi_config = todo!();
+    let corgi_config = package.corgi_config(db);
     todo!()
 }
 
 pub trait HasCorgiConfig: Copy {
+    fn corgi_config(self, db: &dyn CorgiConfigDb) -> VfsResult<CorgiConfig>;
+
     fn registry_path(self, db: &dyn CorgiConfigDb) -> VfsResult<RegistryPath>;
 }
 
 impl HasCorgiConfig for PackagePath {
+    fn corgi_config(self, db: &dyn CorgiConfigDb) -> VfsResult<CorgiConfig> {
+        todo!()
+    }
+
     fn registry_path(self, db: &dyn CorgiConfigDb) -> VfsResult<RegistryPath> {
         package_registry_path(db, self)
     }
