9226862b225f6fb336510617da219ae687d0b097
xiyu <dirac12345@gmail.com>
2023-03-23 06:56:30 -0400

    move entity path expr logic closer

--- crates/syntax/husky-expr/src/entity_path.rs
+++ crates/syntax/husky-expr/src/entity_path.rs
@@ -4,7 +4,7 @@ pub use error::*;
 
 use crate::*;
 use husky_entity_tree::EntityTreeError;
-use parsec::OriginalError;
+use parsec::{OriginalError, ParseContext};
 use thiserror::Error;
 
 #[derive(Debug, PartialEq, Eq)]
@@ -25,3 +25,69 @@ pub enum EntityPathExpr {
 
 pub type EntityPathExprArena = Arena<EntityPathExpr>;
 pub type EntityPathExprIdx = ArenaIdx<EntityPathExpr>;
+
+// todo: change this to trait impl
+impl<'a, 'b> ExprParseContext<'a, 'b> {
+    pub(crate) fn parse_entity_path_expr(
+        &mut self,
+        token_idx: TokenIdx,
+        ident: Ident,
+        entity_path: EntityPath,
+    ) -> (EntityPathExprIdx, Option<EntityPath>) {
+        let root = self.alloc_entity_path_expr(EntityPathExpr::Root {
+            token_idx,
+            ident,
+            entity_path,
+        });
+        match self.try_parse::<ScopeResolutionToken>() {
+            Some(scope_resolution_token) => {
+                self.parse_subentity_path_expr(root, Some(entity_path), scope_resolution_token)
+            }
+            None => (root, Some(entity_path)),
+        }
+    }
+
+    fn parse_subentity_path_expr(
+        &mut self,
+        parent: EntityPathExprIdx,
+        parent_path: Option<EntityPath>,
+        scope_resolution_token: ScopeResolutionToken,
+    ) -> (EntityPathExprIdx, Option<EntityPath>) {
+        let ident_token: EntityPathExprResult<IdentToken> =
+            self.parse_expected(OriginalEntityPathExprError::ExpectIdentAfterScopeResolution);
+        let path: EntityPathExprResult<EntityPath> = match parent_path {
+            Some(parent_path) => match ident_token {
+                Ok(ident_token) => {
+                    let ident = ident_token.ident();
+                    match self.db().subentity_path(parent_path, ident) {
+                        Ok(path) => Ok(path),
+                        Err(error) => Err(OriginalEntityPathExprError::EntityTree {
+                            token_idx: ident_token.token_idx(),
+                            error,
+                        }
+                        .into()),
+                    }
+                }
+                Err(_) => todo!(),
+            },
+            None => todo!(),
+        };
+        let parent_path = match path {
+            Ok(path) => Some(path),
+            Err(_) => None,
+        };
+        let expr = EntityPathExpr::Subentity {
+            parent,
+            scope_resolution_token,
+            ident_token,
+            path,
+        };
+        let expr = self.alloc_entity_path_expr(expr);
+        match self.try_parse::<ScopeResolutionToken>() {
+            Some(scope_resolution_token) => {
+                self.parse_subentity_path_expr(expr, parent_path, scope_resolution_token)
+            }
+            None => (expr, parent_path),
+        }
+    }
+}
