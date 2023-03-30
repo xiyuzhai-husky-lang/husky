266aca999c415aa98225b5dd0ce655261ab8b1f0
xiyu <dirac12345@gmail.com>
2023-03-30 11:22:51 -0400

    save

--- crates/apps/husky-notebook/src/panels.rs
+++ crates/apps/husky-notebook/src/panels.rs
@@ -10,20 +10,3 @@ impl HuskyNotebookApp {
         }
     }
 }
-
-// ui.heading("Husky Notebook");
-// ui.horizontal(|ui| {
-//     let name_label = ui.label("Your name: ");
-//     // ui.text_edit_singleline(&mut self.name)
-//     //     .labelled_by(name_label.id);
-//     CodeEdit::singleline(&mut self.name).ui(ui);
-// });
-// ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-// if ui.button("Click each year").clicked() {
-//     self.age += 1;
-// }
-// ui.label(format!("Hello '{}', age {}", self.name, self.age));
-// ui.label(format!(
-//     "Formula: {}xdx",
-//     husky_unicode_symbols::opr::OPR_INTEGRAL
-// ))
