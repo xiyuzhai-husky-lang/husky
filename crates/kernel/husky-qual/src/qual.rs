4d7d71ecc4a626de19c6c6edcef604f93a1102d7
xiyu <dirac12345@gmail.com>
2023-04-04 14:47:34 -0400

    impl `husky-qual`

--- crates/kernel/husky-qual/src/qual.rs
+++ crates/kernel/husky-qual/src/qual.rs
@@ -4,26 +4,15 @@ pub use self::part_ident::*;
 
 use crate::*;
 
-#[salsa::interned(db = QualDb, jar = QualJar)]
-pub struct Qual {
-    base: BaseQual,
-    part_overrides: VecPairMap<PartIdent, Qual>,
-}
-
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct BaseQual {
     mutability: Mutability,
-    validity: Validity,
     ownership: Ownership,
 }
 
 impl BaseQual {
     pub fn mutability(&self) -> Mutability {
         self.mutability
-    }
-
-    pub fn validity(&self) -> Validity {
-        self.validity
     }
 
     pub fn ownership(&self) -> Ownership {
