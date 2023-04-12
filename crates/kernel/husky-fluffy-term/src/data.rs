c713b514fe4aa298299073ecb68f8a4acfafc4e2
xiyu <dirac12345@gmail.com>
2023-04-11 23:52:02 -0400

    save

--- crates/kernel/husky-fluffy-term/src/data.rs
+++ crates/kernel/husky-fluffy-term/src/data.rs
@@ -12,7 +12,7 @@ pub enum FluffyTermData<'a> {
     TypeOntology {
         path: TypePath,
         refined_path: Either<CustomTypePath, PreludeTypePath>,
-        argument_tys: SmallVec<[FluffyTerm; 2]>,
+        argument_tys: &'a [FluffyTerm],
     },
     Curry {
         curry_kind: CurryKind,
@@ -26,30 +26,6 @@ pub enum FluffyTermData<'a> {
     Ritchie {
         ritchie_kind: TermRitchieKind,
         parameter_contracted_tys: &'a [FluffyTermRitchieParameterContractedType],
-        return_ty: FluffyTerm,
-    },
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub enum FluffyTermDataBuf {
-    Literal(TermLiteral),
-    TypeOntology {
-        path: TypePath,
-        refined_path: Either<CustomTypePath, PreludeTypePath>,
-        argument_tys: SmallVec<[FluffyTerm; 2]>,
-    },
-    Curry {
-        curry_kind: CurryKind,
-        variance: Variance,
-        parameter_variable: Option<FluffyTerm>,
-        parameter_ty: FluffyTerm,
-        return_ty: FluffyTerm,
-    },
-    Hole(HoleKind, HollowTerm),
-    Category(TermCategory),
-    Ritchie {
-        ritchie_kind: TermRitchieKind,
-        parameter_contracted_tys: Vec<FluffyTermRitchieParameterContractedType>,
         return_ty: FluffyTerm,
     },
 }
--- crates/kernel/husky-fluffy-term/src/data/hollow.rs
+++ crates/kernel/husky-fluffy-term/src/data/hollow.rs
@@ -1,4 +1,23 @@
 use super::*;
 
 #[derive(Debug, PartialEq, Eq)]
-pub enum HollowTermData {}
+pub enum HollowTermData {
+    TypeOntology {
+        path: TypePath,
+        refined_path: Either<CustomTypePath, PreludeTypePath>,
+        argument_tys: SmallVec<[FluffyTerm; 2]>,
+    },
+    Curry {
+        curry_kind: CurryKind,
+        variance: Variance,
+        parameter_variable: Option<FluffyTerm>,
+        parameter_ty: FluffyTerm,
+        return_ty: FluffyTerm,
+    },
+    Hole(HoleKind),
+    Ritchie {
+        ritchie_kind: TermRitchieKind,
+        parameter_contracted_tys: Vec<FluffyTermRitchieParameterContractedType>,
+        return_ty: FluffyTerm,
+    },
+}
