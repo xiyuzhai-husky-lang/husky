bc0d76a08444a3db52ca5eddb4feb87ec1336875
xiyu <dirac12345@gmail.com>
2023-04-15 09:36:14 -0400

    save

--- crates/kernel/husky-fluffy-term/src/ty/method.rs
+++ crates/kernel/husky-fluffy-term/src/ty/method.rs
@@ -6,17 +6,36 @@ mod ty;
 pub use self::error::*;
 
 use super::*;
+use husky_word::Ident;
 
 #[derive(Debug, PartialEq, Eq)]
-pub struct MethodType {
-    indirections: SmallVec<[MethodIndirection; 2]>,
+pub struct FluffyMethodType {
+    indirections: SmallVec<[FluffyMethodIndirection; 2]>,
+    disambiguation: FluffyMethodDisambiguation,
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub enum MethodIndirection {}
+pub enum FluffyMethodIndirection {}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum FluffyMethodDisambiguation {
+    Type,
+    Trait { trai: FluffyTerm },
+}
 
 impl FluffyTerm {
-    pub fn method_ty(self) -> FluffyMethodTypeResult<MethodType> {
+    pub fn method_ty(
+        self,
+        engine: &mut impl FluffyTermEngine,
+        ident: Ident,
+        available_traits: &[TraitPath],
+    ) -> FluffyMethodTypeResult<FluffyMethodType> {
+        if let Some(ty_method_ty) = self.ty_method_ty(ident)? {
+            return Ok(ty_method_ty);
+        }
+        if let Some(trai_for_ty_method_ty) = self.trai_for_ty_method_ty(ident, available_traits)? {
+            return Ok(trai_for_ty_method_ty);
+        }
         todo!()
     }
 }
