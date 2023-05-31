a1e4a9fdbebf46d61012074e15fbf5de38319a38
xiyu <dirac12345@gmail.com>
2023-05-31 00:59:58 -0400

    save

--- crates/semantics/husky-expr-ty/src/engine/expr_ty.rs
+++ crates/semantics/husky-expr-ty/src/engine/expr_ty.rs
@@ -387,7 +387,28 @@ impl<'a> ExprTypeEngine<'a> {
                     Ok(expectation_ok.return_ty()),
                 ))
             }
-            ExpectEqsFunctionTypeOutcomeVariant::Curry { .. } => todo!(),
+            ExpectEqsFunctionTypeOutcomeVariant::Curry {
+                parameter_symbol,
+                parameter_ty,
+                return_ty,
+            } => {
+                match items.len() {
+                    0 => unreachable!(),
+                    1 => self.infer_new_expr_ty_discarded(
+                        items.start(),
+                        ExpectImplicitlyConvertible::new_const(*parameter_ty),
+                    ),
+                    // parameter_ty must be a tuple
+                    // distribute the types for a tuple
+                    _ => todo!(),
+                }
+                Ok((
+                    ExprDisambiguation::ExplicitApplicationOrRitchieCall(
+                        ApplicationOrRitchieCallExprDisambiguation::Application,
+                    ),
+                    Ok(*return_ty),
+                ))
+            }
         }
     }
 
