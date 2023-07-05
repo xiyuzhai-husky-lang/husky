691a1416ea844fe427fd4b9d5b286716e9fb5f74
Xiyu Zhai <46001555+xiyuzhai@users.noreply.github.com>
2023-07-05 11:32:28 -0400

    Update ci_cargo_test.sh

--- scripts/ci_cargo_test.sh
+++ scripts/ci_cargo_test.sh
@@ -1,3 +1,10 @@
 . ./scripts/ci_env.sh
 cargo test --verbose
+# check available disk spaces
 df -h 
+# print current path
+echo "print current path"
+pwd
+# check file sizes on current path
+echo "check file sizes on current path"
+tree --du -h ./
