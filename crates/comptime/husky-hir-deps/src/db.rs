ce47c0163674fb7d01d9e0c5dd4f84fa881b11d8
Xiyu Zhai <xiyuzhai95@gmail.com>
2023-07-24 02:59:12 +0000

    save

---
+++ crates/comptime/husky-hir-deps/src/db.rs
@@ -0,0 +1,6 @@
+use crate::*;
+
+pub trait HirDepsDb: salsa::DbWithJar<HirDepsJar> {}
+
+#[salsa::jar(db = HirDepsDb)]
+pub struct HirDepsJar(Deps, DepPaths0, DepPaths1, DepPaths2);
---
+++ crates/comptime/husky-hir-deps/src/lib.rs
@@ -0,0 +1,5 @@
+mod db;
+mod deps;
+
+pub use self::db::*;
+pub use self::deps::*;
