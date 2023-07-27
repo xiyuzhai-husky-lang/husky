e5895a39f8ba185b41a5d9c144fefcc546e70420
xiyu <dirac12345@gmail.com>
2023-07-27 19:18:20 -0400

    save

--- crates/devtools/cargo-husky-prebuild/src/main.rs
+++ crates/devtools/cargo-husky-prebuild/src/main.rs
@@ -1,15 +1,16 @@
-mod datasets_interface_codegen;
-mod vm_interface_codegen;
+// mod datasets_interface_codegen;
+// mod vm_interface_codegen;
 
-use datasets_interface_codegen::write_datasets_interface_codegen;
-use husky_io_utils::diff_write;
-use husky_rust_code_repr::registration::*;
-use husky_write_utils::w;
-use std::fmt::Write;
-use std::path::PathBuf;
-use vm_interface_codegen::write_vm_interface_codegen;
+// use datasets_interface_codegen::write_datasets_interface_codegen;
+// use husky_io_utils::diff_write;
+// use husky_rust_code_repr::registration::*;
+// use husky_write_utils::w;
+// use std::fmt::Write;
+// use std::path::PathBuf;
+// use vm_interface_codegen::write_vm_interface_codegen;
 
 fn main() {
-    write_vm_interface_codegen();
-    write_datasets_interface_codegen();
+    todo!()
+    // write_vm_interface_codegen();
+    // write_datasets_interface_codegen();
 }
