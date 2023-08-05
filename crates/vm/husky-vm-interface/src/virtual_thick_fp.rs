08ae98424f7610c8cc69208ae96592904da48588
xiyu <dirac12345@gmail.com>
2023-08-05 02:37:34 -0400

    fix const fn

--- crates/vm/husky-vm-interface/src/virtual_thick_fp.rs
+++ crates/vm/husky-vm-interface/src/virtual_thick_fp.rs
@@ -1,44 +1,44 @@
-use crate::*;
+// use crate::*;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct VirtualThickFp {
-    needs_eval_context: bool,
-    fp: *const c_void,
-}
+// #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// pub struct VirtualThickFp {
+//     needs_eval_context: bool,
+//     fp: *const c_void,
+// }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct __OptVirtualThickFp {
-    needs_eval_context: bool,
-    fp: *const c_void,
-}
+// #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// pub struct __OptVirtualThickFp {
+//     needs_eval_context: bool,
+//     fp: *const c_void,
+// }
 
-impl __OptVirtualThickFp {
-    pub const fn none() -> Self {
-        Self {
-            needs_eval_context: false,
-            fp: std::ptr::null(),
-        }
-    }
+// impl __OptVirtualThickFp {
+//     pub const fn none() -> Self {
+//         Self {
+//             needs_eval_context: false,
+//             fp: std::ptr::null(),
+//         }
+//     }
 
-    #[cfg(feature = "thin_fp")]
-    pub const fn some_base<F: ~const __BaseThinFp>(f: F) -> Self {
-        Self {
-            needs_eval_context: false,
-            fp: f.__to_void_pointer(),
-        }
-    }
+//     #[cfg(feature = "thin_fp")]
+//     pub const fn some_base<F: ~const __BaseThinFp>(f: F) -> Self {
+//         Self {
+//             needs_eval_context: false,
+//             fp: f.__to_void_pointer(),
+//         }
+//     }
 
-    #[cfg(feature = "thin_fp")]
-    pub const fn some_ctx<F: ~const __CtxThinFp>(f: F) -> Self {
-        Self {
-            needs_eval_context: true,
-            fp: f.__to_void_pointer(),
-        }
-    }
+//     #[cfg(feature = "thin_fp")]
+//     pub const fn some_ctx<F: ~const __CtxThinFp>(f: F) -> Self {
+//         Self {
+//             needs_eval_context: true,
+//             fp: f.__to_void_pointer(),
+//         }
+//     }
 
-    #[cfg(feature = "thick_fp")]
-    #[inline(always)]
-    pub const unsafe fn downcast_thick_fp<F: __BaseThinFp>(self) -> ThickFp<F> {
-        ThickFp::new(self.needs_eval_context, self.fp)
-    }
-}
+//     #[cfg(feature = "thick_fp")]
+//     #[inline(always)]
+//     pub const unsafe fn downcast_thick_fp<F: __BaseThinFp>(self) -> ThickFp<F> {
+//         ThickFp::new(self.needs_eval_context, self.fp)
+//     }
+// }
