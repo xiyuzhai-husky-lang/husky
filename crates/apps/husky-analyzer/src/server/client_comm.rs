86ed4b853e974ebf3829b2a4418e4c9177f32e5f
Xiyu Zhai <xiyuzhai95@gmail.com>
2023-08-11 04:47:44 +0000

    save

--- crates/apps/husky-analyzer/src/server/client_comm.rs
+++ crates/apps/husky-analyzer/src/server/client_comm.rs
@@ -109,7 +109,6 @@ impl ClientCommunicator {
     pub(crate) fn respond(&mut self, response: lsp_server::Response) {
         if let Some((method, start)) = self.req_queue.incoming.complete(response.id.clone()) {
             if let Some(err) = &response.error {
-                todo!("err = {:?}", err);
                 if err.message.starts_with("server panicked") {
                     self.poke_husky_developer(format!("{}, check the log", err.message))
                 }
