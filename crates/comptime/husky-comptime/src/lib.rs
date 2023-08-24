6dc6544c19a5091c92d54cc9c6774a7ce4565f50
Xiyu Zhai <xiyuzhai@mit.edu>
2023-08-24 15:29:23 -0400

    save

--- crates/comptime/husky-comptime/src/lib.rs
+++ crates/comptime/husky-comptime/src/lib.rs
@@ -2,12 +2,14 @@ mod db;
 
 use self::db::*;
 
-pub struct Comptime {
+pub struct DevComptime<Task: husky_task::IsTask> {
     db: ComptimeDb,
+    storage: DevComptimeStorage<Task>,
+    linkage_table: dashmap::DashMap<DevLinkageKey<Task>, DevLinkage<Task>>,
     // linkage_table: LinkageTable,
 }
 
-impl Comptime {
+impl DevComptime {
     pub fn db(&self) -> &ComptimeDb {
         &self.db
     }
