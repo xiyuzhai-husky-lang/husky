4dd787ac9c589ae9db234d1c8f8635be1660f6ea
xiyu <dirac12345@gmail.com>
2023-08-30 11:23:32 -0400

    init husky-mono-linkage-table

---
+++ crates/comptime/husky-mono-linkage-table/src/internal/libgen.rs
@@ -0,0 +1,5 @@
+use super::*;
+
+pub(super) fn generate_library(target_crate: CratePath, db: &dyn HirDepsDb) -> MonoLibraryStorage {
+    MonoLibraryStorage {}
+}
--- crates/comptime/husky-mono-linkage-table/src/linkage.rs
+++
@@ -1,6 +0,0 @@
-use crate::*;
-
-#[derive(Debug, Clone, Copy)]
-pub struct MinimalDevLinkage(
-    extern "C" fn(smallvec::SmallVec<[__RegularValue; 4]>) -> __RegularValue,
-);
