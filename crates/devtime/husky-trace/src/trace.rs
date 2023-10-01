43b037fd9b5a9ad2a894b455940e260db9db38f6
Xiyu Zhai <xiyuzhai95@gmail.com>
2023-10-01 16:26:31 +0000

    save

--- crates/devtime/husky-trace/src/trace.rs
+++ crates/devtime/husky-trace/src/trace.rs
@@ -17,14 +17,46 @@ pub use self::loop_group::*;
 pub use self::val_item::*;
 
 use crate::*;
+use husky_sema_expr::SemaExprIdx;
+use vec_like::VecPairMap;
 
-#[derive(Debug, Clone)]
-pub struct TraceView {
-    tokens: Vec<TraceViewToken>,
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[salsa::debug_with_db(db = TraceDb)]
+pub enum Trace {
+    LazyCall(LazyCallTrace),
+    LazyExpr(LazyExprTrace),
+    LazyStmt(LazyStmtTrace),
+    EagerCall(EagerCallTrace),
+    EagerExpr(EagerExprTrace),
+    EagerStmt(EagerStmtTrace),
 }
 
-#[derive(Debug, Clone)]
-pub struct TraceViewToken {
-    text: String,
-    ranged_semantic_token: husky_semantic_token::token::SemanticToken,
+pub enum AssociatedExprTraces<'a> {
+    Eager(&'a [(SemaExprIdx, EagerExprTrace)]),
+    Lazy(&'a [(SemaExprIdx, LazyExprTrace)]),
+}
+
+#[test]
+fn associated_expr_traces_size() {
+    assert_eq!(
+        std::mem::size_of::<Option<AssociatedExprTraces>>(),
+        std::mem::size_of::<AssociatedExprTraces>()
+    )
+}
+
+impl Trace {
+    pub fn associated_expr_traces(self, db: &dyn TraceDb) -> Option<AssociatedExprTraces> {
+        match self {
+            Trace::LazyCall(_) => None,
+            Trace::LazyExpr(_) => None,
+            Trace::LazyStmt(trace) => {
+                Some(AssociatedExprTraces::Lazy(trace.associated_expr_traces(db)))
+            }
+            Trace::EagerCall(_) => None,
+            Trace::EagerExpr(_) => None,
+            Trace::EagerStmt(trace) => Some(AssociatedExprTraces::Eager(
+                trace.associated_expr_traces(db),
+            )),
+        }
+    }
 }
