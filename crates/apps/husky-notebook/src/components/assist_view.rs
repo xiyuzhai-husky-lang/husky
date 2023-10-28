dcdd9eb83a5d655eaf03244d521411dd65d7a144
xiyu <dirac12345@gmail.com>
2023-10-28 10:46:34 -0400

    save

--- crates/apps/husky-notebook/src/components/assist_view.rs
+++ crates/apps/husky-notebook/src/components/assist_view.rs
@@ -3,5 +3,6 @@ use super::*;
 impl NotebookApp {
     pub(crate) fn render_assist_view(&mut self, _ctx: &egui::Context, ui: &mut egui::Ui) {
         ui.label("todo: assist view");
+        ui.allocate_space(ui.available_size());
     }
 }
