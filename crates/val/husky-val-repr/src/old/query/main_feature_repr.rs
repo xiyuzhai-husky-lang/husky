fd7af5679b240315583605c6ba91c141f1a147a7
Xiyu Zhai <xiyuzhai95@gmail.com>
2023-11-08 07:01:33 +0000

    clean up

--- crates/val/husky-val-repr/src/old/query/main_feature_repr.rs
+++
@@ -1,7 +0,0 @@
-use super::*;
-
-pub(super) fn main_feature_repr(db: &dyn ValReprDb, target_entrance: EntityPath) -> ValRepr {
-    let package = db.package(target_entrance).unwrap();
-    let main = &*package.main_defn;
-    ValRepr::from_defn(db, None, &main.defn_repr, db.feature_interner())
-}
