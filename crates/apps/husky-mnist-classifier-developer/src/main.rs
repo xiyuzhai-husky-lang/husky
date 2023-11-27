497b56d9808580bbcfc65ce5c97da2795ea4b1f5
xiyu <dirac12345@gmail.com>
2023-11-27 09:49:36 -0500

    developer works again

--- crates/apps/husky-mnist-classifier-developer/src/main.rs
+++ crates/apps/husky-mnist-classifier-developer/src/main.rs
@@ -1,17 +1,14 @@
-// use husky_ad_hoc_protocol::AD_HOC_WEBSOCKET_ADDRESS;
-// use husky_devtime::*;
-// use husky_graphics2d_visual_protocol::Graphics2dVisualProtocol;
-// use husky_ml_task::MlTask;
-// use husky_standard_dev_comptime_db::StandardDevComptimeDb;
-// use std::path::PathBuf;
+use husky_ad_hoc_protocol::AD_HOC_WEBSOCKET_ADDRESS;
+use husky_devtime::*;
+use husky_graphics2d_visual_protocol::Graphics2dVisualProtocol;
+use husky_ml_task::MlTask;
+use std::path::PathBuf;
 
-// type Task = MlTask<StandardDevComptimeDb, Graphics2dVisualProtocol>;
+type Task = MlTask<Graphics2dVisualProtocol>;
 
-// fn main() {
-//     let task = Task::new();
-//     let path: PathBuf = "examples/mnist-classifier".into();
-//     let devtime = Devtime::new(task, &path, None).expect("valid");
-//     devtime.serve_traces(AD_HOC_WEBSOCKET_ADDRESS)
-// }
-
-fn main() {}
+fn main() {
+    let task = Task::new();
+    let path: PathBuf = "examples/mnist-classifier".into();
+    let devtime = Devtime::new(task, &path, None).expect("valid");
+    devtime.serve_traces(AD_HOC_WEBSOCKET_ADDRESS)
+}
