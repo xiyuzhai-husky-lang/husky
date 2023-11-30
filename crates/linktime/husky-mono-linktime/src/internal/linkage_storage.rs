152bcac111de5f3deab5bba1cb0b03e8dd283eba
xiyu <dirac12345@gmail.com>
2023-11-30 18:56:15 -0500

    save

--- crates/linktime/husky-mono-linktime/src/internal/linkage_storage.rs
+++ crates/linktime/husky-mono-linktime/src/internal/linkage_storage.rs
@@ -13,15 +13,15 @@ impl MonoLinkageStorage {
 
 #[test]
 fn generate_linkage_storage_works() {
-    // use husky_print_utils::*;
-    // use husky_standard_dev_comptime_db::StandardDevComptimeDb;
-    // use salsa::DebugWithDb;
-    // let mut db = StandardDevComptimeDb::default();
-    // db.vfs_plain_test(
-    //     |db, package_path: PackagePath| {
-    //         MonoLinkageStorage::generate(LinktimeTargetPath::new_package(package_path, db), db);
-    //     },
-    //     &VfsTestConfig::new("generate_linkage_storage")
-    //         .with_vfs_test_domains_config(VfsTestDomainsConfig::ExcludeLibrary),
-    // );
+    use husky_dev_comptime::db::DevComptimeDb;
+    use husky_print_utils::*;
+    use salsa::DebugWithDb;
+    let mut db = DevComptimeDb::default();
+    db.vfs_plain_test(
+        |db, package_path: PackagePath| {
+            MonoLinkageStorage::generate(LinktimeTargetPath::new_package(package_path, db), db);
+        },
+        &VfsTestConfig::new("generate_linkage_storage")
+            .with_vfs_test_domains_config(VfsTestDomainsConfig::ExcludeLibrary),
+    );
 }
