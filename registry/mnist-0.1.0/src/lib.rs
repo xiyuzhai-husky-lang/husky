7297999358e19f18302bc01f4b5c91f85f91065a
Xiyu Zhai <xiyuzhai@mit.edu>
2023-12-07 13:16:29 -0500

    save

---
+++ registry/mnist-0.1.0/src/lib.rs
@@ -0,0 +1,54 @@
+pub enum MnistLabel {
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+}
+
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
+pub struct BinaryImage28([u32; 30]);
+
+impl BinaryImage28 {
+    pub fn new_zeros() -> Self {
+        Self::default()
+    }
+}
+
+impl std::ops::Index<usize> for BinaryImage28 {
+    type Output = u32;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.0[index]
+    }
+}
+
+impl std::ops::IndexMut<usize> for BinaryImage28 {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.0[index]
+    }
+}
+
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
+pub struct BinaryGrid28([u32; 31]);
+
+impl std::ops::Index<usize> for BinaryGrid28 {
+    type Output = u32;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.0[index]
+    }
+}
+
+impl std::ops::IndexMut<usize> for BinaryGrid28 {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.0[index]
+    }
+}
+
+impl BinaryGrid28 {}
