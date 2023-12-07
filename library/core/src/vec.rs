e69bea3c5c91afc0f00f37980903c47a0c04a9af
xiyu <dirac12345@gmail.com>
2023-12-07 10:42:35 -0500

    save

--- library/core/src/vec.rs
+++ library/core/src/vec.rs
@@ -1,9 +1,25 @@
 pub trait __VecX {
+    type Element;
+
     fn ilen(&self) -> i32;
+
+    fn collect_leashes(&'static self) -> Vec<&'static Self::Element>;
+
+    fn pop_with_largest_opt_f32(&mut self, f: fn(Self::Element) -> Option<f32>);
 }
 
 impl<T> __VecX for Vec<T> {
+    type Element = T;
+
     fn ilen(&self) -> i32 {
         self.len() as i32
+    }
+
+    fn collect_leashes(&'static self) -> Vec<&'static Self::Element> {
+        self.iter().collect()
+    }
+
+    fn pop_with_largest_opt_f32(&mut self, f: fn(Self::Element) -> Option<f32>) {
+        todo!()
     }
 }
