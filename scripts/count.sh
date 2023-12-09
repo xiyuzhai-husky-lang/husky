925df9ac0edce38ba806148e423a959be3d36806
xiyu <dirac12345@gmail.com>
2023-12-09 17:53:33 -0500

    avoid target-rs when counting

--- scripts/count.sh
+++ scripts/count.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # cloc ./ --exclude-dir=__rust_gen__,__rust_gen_cache__,node_modules,out,target,dist,forks,package-lock.json,package.json --exclude-ext=html,css,json,toml
-scc ./ --exclude-dir=expect-files,tests,__rust_gen__,__rust_gen_cache__,node_modules,out,target,dist,forks,package-lock.json,package.json,__rust_code_gen__.rs,sparks,static
+scc ./ --exclude-dir=expect-files,tests,target-rs,__rust_gen__,__rust_gen_cache__,node_modules,out,target,dist,forks,package-lock.json,package.json,__rust_code_gen__.rs,sparks,static
