66d22ce1d863c39735e3247818c8ae34095767ee
xiyu <dirac12345@gmail.com>
2023-12-15 15:56:47 -0500

    save

--- crates/tasks/husky-task/src/helpers.rs
+++ crates/tasks/husky-task/src/helpers.rs
@@ -1,12 +1,13 @@
 use crate::*;
 
 pub type TaskDevLinkTime<Task> = <<Task as IsTask>::DevAscension as IsDevAscension>::Linktime;
+pub type TaskDevLinkageImpl<Task> =
+    <<<Task as IsTask>::DevAscension as IsDevAscension>::Linktime as IsLinktime>::LinkageImpl;
 pub type TaskDevRuntimeSpecificConfig<Task> =
     <<Task as IsTask>::DevAscension as IsDevAscension>::RuntimeSpecificConfig;
 pub type DevRuntimeStorage<Task> =
     <<Task as IsTask>::DevAscension as IsDevAscension>::RuntimeStorage;
-pub type TaskDevAscensionBasePoint<Task> =
-    <<Task as IsTask>::DevAscension as IsDevAscension>::BasePoint;
+pub type TaskDevBasePoint<Task> = <<Task as IsTask>::DevAscension as IsDevAscension>::BasePoint;
 pub type TaskDevAscension<Task> = <Task as IsTask>::DevAscension;
 pub type TaskValue<Task> = <TaskDevAscension<Task> as IsDevAscension>::Value;
 pub type TaskTraceProtocol<Task> = <TaskDevAscension<Task> as IsDevAscension>::TraceProtocol;
--- crates/tasks/husky-task/src/linktime.rs
+++ crates/tasks/husky-task/src/linktime.rs
@@ -6,6 +6,6 @@ use husky_vfs::linktime_target_path::LinktimeTargetPath;
 pub trait IsLinktime: Sized + Send {
     type LinkageImpl: IsLinkageImpl;
     // linktime has the responsibility to guarantee that the linkage provided is up to date.
-    fn get_linkage(&self, path: Linkage, db: &::salsa::Db) -> Self::LinkageImpl;
+    fn linkage_impl(&self, linkage: Linkage, db: &::salsa::Db) -> Self::LinkageImpl;
     fn new_linktime(target_path: LinktimeTargetPath, db: &::salsa::Db) -> Self;
 }
