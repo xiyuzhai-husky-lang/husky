c36dc366852e1064aa3c0e46a9f24e745f8a1aa3
xiyu <dirac12345@gmail.com>
2023-12-16 19:54:04 -0500

    save

--- registry/ad-hoc-task-dependency/src/ugly.rs
+++ registry/ad-hoc-task-dependency/src/ugly.rs
@@ -2,3 +2,4 @@ pub use crate::{
     FromValue as __FromValue, IntoValue as __IntoValue, JarIndex as __JarIndex,
     JarIndexOnceCell as __JarIndexOnceCell, Value as __Value,
 };
+pub use husky_ml_task_prelude::DEV_EVAL_CONTEXT as __DEV_EVAL_CONTEXT;
