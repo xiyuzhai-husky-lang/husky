ef0c200eab2aee6107d653924d059541ede96eb0
xiyu <dirac12345@gmail.com>
2024-01-18 01:13:28 -0500

    figure canvas and control

--- crates/gui/husky-trace-doc/src/view/forest.rs
+++ crates/gui/husky-trace-doc/src/view/forest.rs
@@ -272,73 +272,73 @@ where
         self.render_space_chars(1, ui);
         match value {
             ValuePresentation::Unit(()) => {
-                ui.label("()");
+                ui.label(format!("()"));
             }
             ValuePresentation::Bool(b) => {
-                ui.label("{b}");
+                ui.label(format!("{}", b));
             }
             ValuePresentation::Char(c) => {
-                ui.label("'{c}'");
+                ui.label(format!("'{}'", c));
             }
             ValuePresentation::I8(i) => {
-                ui.label("{i}i8");
+                ui.label(format!("{}i8", i));
             }
             ValuePresentation::I16(i) => {
-                ui.label("{i}i16");
+                ui.label(format!("{}i16", i));
             }
             ValuePresentation::I32(i) => {
-                ui.label("{i}i32");
+                ui.label(format!("{}i32", i));
             }
             ValuePresentation::I64(i) => {
-                ui.label("{i}i64");
+                ui.label(format!("{}i64", i));
             }
             ValuePresentation::I128(i) => {
-                ui.label("{i}i128");
+                ui.label(format!("{}i128", i));
             }
             ValuePresentation::ISize(i) => {
-                ui.label("{i}isize");
+                ui.label(format!("{}isize", i));
             }
             ValuePresentation::U8(u) => {
-                ui.label("{u}u8");
+                ui.label(format!("{}u8", u));
             }
             ValuePresentation::U16(u) => {
-                ui.label("{u}u16");
+                ui.label(format!("{}u16", u));
             }
             ValuePresentation::U32(u) => {
-                ui.label("{u}u32");
+                ui.label(format!("{}u32", u));
             }
             ValuePresentation::U64(u) => {
-                ui.label("{u}u64");
+                ui.label(format!("{}u64", u));
             }
             ValuePresentation::U128(u) => {
-                ui.label("{u}u128");
+                ui.label(format!("{}u128", u));
             }
             ValuePresentation::USize(u) => {
-                ui.label("{u}usize");
+                ui.label(format!("{}usize", u));
             }
             ValuePresentation::R8(r) => {
-                ui.label("{r}r8");
+                ui.label(format!("{}r8", r));
             }
             ValuePresentation::R16(r) => {
-                ui.label("{r}r16");
+                ui.label(format!("{}r16", r));
             }
             ValuePresentation::R32(r) => {
-                ui.label("{r}r32");
+                ui.label(format!("{}r32", r));
             }
             ValuePresentation::R64(r) => {
-                ui.label("{r}r64");
+                ui.label(format!("{}r64", r));
             }
             ValuePresentation::R128(r) => {
-                ui.label("{r}r128");
+                ui.label(format!("{}r128", r));
             }
             ValuePresentation::RSize(r) => {
-                ui.label("{r}rsize");
+                ui.label(format!("{}rsize", r));
             }
             ValuePresentation::F32(f) => {
-                ui.label("{f}f32");
+                ui.label(format!("{}f32", f));
             }
             ValuePresentation::F64(f) => {
-                ui.label("{f}f64");
+                ui.label(format!("{}f64", f));
             }
             ValuePresentation::Enum => todo!(),
             ValuePresentation::Struct => todo!(),
