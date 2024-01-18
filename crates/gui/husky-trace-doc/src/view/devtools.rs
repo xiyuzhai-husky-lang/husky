1fed291f849f4e7c57342361e95b066ba141b435
xiyu <dirac12345@gmail.com>
2024-01-18 15:31:20 -0500

    add trace doc devtools

---
+++ crates/gui/husky-trace-doc/src/view/devtools.rs
@@ -0,0 +1,33 @@
+// DevTools
+// Chrome DevTools is a set of web developer tools built directly into the Google Chrome browser. DevTools can help you edit pages on-the-fly and diagnose problems quickly, which helps you build better websites, faster.
+use super::*;
+
+impl<'a, TraceProtocol, Settings> TraceDocView<'a, TraceProtocol, Settings>
+where
+    TraceProtocol: IsTraceProtocol,
+    Settings: HasTraceViewDocSettings,
+{
+    pub(super) fn render_devtools(&mut self, ui: &mut egui::Ui) {
+        ui.vertical(|ui| {
+            ui.label(RichText::new("Devtools").color(Color32::WHITE));
+            ui.separator();
+            // for debug
+            ui.label(
+                RichText::new(format!(
+                    r#"self.trace_synchrotron.focused_trace_id() = {:?}
+input.pointer.interact_pos() = {:?}
+ui.available_size() = {:?}"#,
+                    self.trace_synchrotron.followed_trace_id(),
+                    ui.input(|input| input.pointer.interact_pos()),
+                    ui.available_size()
+                ))
+                .color(Color32::GRAY),
+            );
+            ui.horizontal(|ui| {
+                let available_size = ui.available_size();
+                ui.label(format!("available size for control = {available_size:?}"))
+            });
+            ui.allocate_space(ui.available_size())
+        });
+    }
+}
--- crates/gui/husky-trace-doc/src/view/pedestal.rs
+++ crates/gui/husky-trace-doc/src/view/pedestal.rs
@@ -6,9 +6,14 @@ where
     Settings: HasTraceViewDocSettings,
 {
     pub(super) fn render_pedestal(&mut self, ui: &mut egui::Ui) {
-        ui.label(format!(
-            "pedestal = {:?}",
-            self.trace_synchrotron.pedestal()
-        ));
+        egui::Frame::none().inner_margin(1.0).show(ui, |ui| {
+            ui.horizontal(|ui| {
+                ui.label(format!(
+                    "pedestal = {:?}",
+                    self.trace_synchrotron.pedestal()
+                ));
+                ui.allocate_space(ui.available_size())
+            })
+        });
     }
 }
