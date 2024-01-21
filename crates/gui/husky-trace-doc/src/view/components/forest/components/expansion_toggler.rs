f16c0ea6f53bd35b22d496fb05085f3dffa33a51
xiyu <dirac12345@gmail.com>
2024-01-21 15:17:30 -0500

    Michael Jordan is goat!

--- crates/gui/husky-trace-doc/src/view/components/forest/components/expansion_toggler.rs
+++ crates/gui/husky-trace-doc/src/view/components/forest/components/expansion_toggler.rs
@@ -2,7 +2,7 @@ use super::*;
 use egui::{Response, Ui};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize, Hash)]
-pub enum ExpansionTogglerButtonStyle {
+pub enum ExpansionTogglerSymbolStyle {
     PlusMinus,
     ConcreteArrowHead,
 }
@@ -20,9 +20,10 @@ where
         ui: &mut Ui,
     ) -> Response {
         // todo: different styles
-        let toggler_text = ExpansionTogglerButtonStyle::PlusMinus.fun_name(expanded);
-        let toggler_text = RichText::new(toggler_text).family(FontFamily::Monospace);
-        let toggler = Label::new(toggler_text).sense(Sense::click());
+        let symbol_style = ExpansionTogglerSymbolStyle::ConcreteArrowHead;
+        let toggler =
+            Label::new(RichText::new(symbol_style.text(expanded)).family(FontFamily::Monospace))
+                .sense(Sense::click());
         let toggler_response = toggler.ui(ui);
         if toggler_response.clicked() {
             self.add_action(TraceViewAction::ToggleExpansion { trace_id })
@@ -31,16 +32,16 @@ where
     }
 }
 
-impl ExpansionTogglerButtonStyle {
-    fn fun_name(&self, expanded: bool) -> &str {
+impl ExpansionTogglerSymbolStyle {
+    fn text(&self, expanded: bool) -> &str {
         match self {
-            ExpansionTogglerButtonStyle::PlusMinus => match expanded {
+            ExpansionTogglerSymbolStyle::PlusMinus => match expanded {
                 true => "-",
                 false => "+",
             },
-            ExpansionTogglerButtonStyle::ConcreteArrowHead => match expanded {
-                true => "-",
-                false => "+",
+            ExpansionTogglerSymbolStyle::ConcreteArrowHead => match expanded {
+                true => "⏷",
+                false => "⏵",
             },
         }
     }
