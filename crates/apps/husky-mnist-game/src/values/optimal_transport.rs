4c070e0a4dfa0ff82772a4a02a3c54908d818f8a
xiyu <dirac12345@gmail.com>
2024-01-27 19:53:02 -0500

    vary widths

--- crates/apps/husky-mnist-game/src/values/optimal_transport.rs
+++ crates/apps/husky-mnist-game/src/values/optimal_transport.rs
@@ -12,11 +12,10 @@ pub struct OptimalTransport {
 impl OptimalTransport {
     pub(crate) fn new_ad_hoc(input: &Input, skeleton: &MnistSkeleton) -> Self {
         Self {
-            source_points: vec![(2.0, 14.0).into(), (25.0, 14.0).into()],
-            target_points: vec![(14.0, 2.0).into(), (14.0, 25.0).into()],
-            optimal_transport_matrix: Array2::from_shape_fn((2, 2), |(i, j)| match i == j {
-                true => 0.0,
-                false => 1.0,
+            source_points: vec![(2.0, 14.0).into(), (14.0, 14.0).into(), (25.0, 14.0).into()],
+            target_points: vec![(14.0, 2.0).into(), (14.0, 14.0).into(), (14.0, 25.0).into()],
+            optimal_transport_matrix: Array2::from_shape_fn((3, 3), |(i, j)| {
+                1.0f64 / ((1 + i + j) as f64)
             }),
         }
     }
@@ -43,7 +42,12 @@ impl Visualize for OptimalTransport {
                 (weight > threshold).then(|| -> Visual {
                     let start = self.source_points[i].into();
                     let end = self.target_points[j].into();
-                    Visual::new_line_segment(start, end, (2.0, Color::Red), visual_synchrotron)
+                    Visual::new_line_segment(
+                        start,
+                        end,
+                        ((10.0 * weight / eff_weights) as f32, Color::Red),
+                        visual_synchrotron,
+                    )
                 })
             }))
         }
