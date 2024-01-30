c3159a00e66d5fcb5991334eea43ed9f46ff8f1a
xiyu <dirac12345@gmail.com>
2024-01-30 14:59:09 -0500

    add docs

--- crates/linkage/husky-javelin/src/lib.rs
+++ crates/linkage/husky-javelin/src/lib.rs
@@ -1,3 +1,10 @@
+//! a javelin is a group of linkages, the name inspired by Diablo 2. For instance, the type constructor javelin will contains the type constructor linkage and also the (pattern) field access linkages.
+//!
+//! It's easier to collect javelins. We first collect javelins and then flatten them to get linkages.
+//!
+//! Javelins are divided into two classes (as inspired by Diablo 2):
+//! - Amazon. Those javelins doesn't contain any template parameters and thus are always collected. In a sense, amazons are always there, nonvanishing.
+//! - Valkyrie. Those javelins contain nonempty template parameters and thus are collected if invoked. So we need to do some "tree walk" to completely collect valkyrie javelins. In a sense, valkyries are summoned by amazons.
 #![feature(associated_type_bounds)]
 /// the name amazon comes from diablo 2
 pub mod amazon;
