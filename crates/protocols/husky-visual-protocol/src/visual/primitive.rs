3c802c231d84f69f979a308cb90907a51a57d7e8
xiyu <dirac12345@gmail.com>
2024-02-02 05:32:47 -0500

    save

--- crates/protocols/husky-visual-protocol/src/visual/primitive.rs
+++ crates/protocols/husky-visual-protocol/src/visual/primitive.rs
@@ -3,6 +3,7 @@ use ordered_float::OrderedFloat;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize, Hash)]
 pub enum PrimitiveVisual {
+    I8(i8),
     F32(OrderedFloat<f32>),
     F64(OrderedFloat<f64>),
 }
