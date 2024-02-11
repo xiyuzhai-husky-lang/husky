458e260ee8756406b3609e6abda6a6ca09b40685
xiyu <dirac12345@gmail.com>
2024-02-11 04:28:43 -0500

    complete eliminate mods

--- crates/devtools/cargo-eliminate-mods/src/lib.rs
+++ crates/devtools/cargo-eliminate-mods/src/lib.rs
@@ -1,6 +1,25 @@
 use husky_git_utils::dirty::assert_husky_git_dir_clean;
+use husky_path_utils::{
+    rust::{collect_rust_source_paths_with, husky_cargo_workspace_manifest_dir},
+    Path, PathBuf,
+};
+use std::fs;
 
 pub fn eliminate_mods() {
     assert_husky_git_dir_clean();
-    todo!()
+    for path in collect_rust_source_paths_with(husky_cargo_workspace_manifest_dir(), |path| {
+        path.ends_with("mod.rs")
+    }) {
+        rename_mod_file(&path).unwrap()
+    }
+}
+
+fn rename_mod_file(path: &Path) -> std::io::Result<()> {
+    if path.ends_with("mod.rs") {
+        if let Some(parent_path) = path.parent() {
+            let new_path = parent_path.with_extension("rs");
+            fs::rename(path, new_path)?;
+        }
+    }
+    Ok(())
 }
