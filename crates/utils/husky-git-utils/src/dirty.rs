15ec6c11aad54ed5a65a40e57bd086c4bc092549
xiyu <dirac12345@gmail.com>
2024-02-11 03:56:08 -0500

    husky-git-utils

---
+++ crates/utils/husky-git-utils/src/dirty.rs
@@ -0,0 +1,35 @@
+use git2::{Repository, StatusOptions, StatusShow};
+use husky_path_utils::cargo::husky_cargo_workspace_manifest_dir;
+use std::path::Path;
+
+pub fn is_husky_git_dir_dirty() -> Result<bool, git2::Error> {
+    is_git_dir_dirty(husky_cargo_workspace_manifest_dir().unwrap())
+}
+
+pub fn is_git_dir_dirty<P: AsRef<Path>>(path: P) -> Result<bool, git2::Error> {
+    // Attempt to open the repository at the given path
+    let repo = Repository::open(path)?;
+
+    // Create a new vector to hold the options for status.
+    let mut opts = StatusOptions::new();
+
+    // Set the options to include untracked files and to show the index and working directory changes.
+    opts.include_untracked(true)
+        .show(StatusShow::IndexAndWorkdir);
+
+    // Check the status of files in the repository
+    let statuses = repo.statuses(Some(&mut opts))?;
+
+    // If there are no statuses, the directory is not dirty
+    Ok(!statuses.is_empty())
+}
+
+#[ignore]
+#[test]
+fn main() {
+    match is_husky_git_dir_dirty() {
+        Ok(dirty) if dirty => println!("Git directory is dirty"),
+        Ok(_) => println!("Git directory is clean"),
+        Err(e) => println!("Error checking Git directory: {}", e),
+    }
+}
--- crates/utils/husky-path-utils/src/cargo.rs
+++ crates/utils/husky-path-utils/src/cargo.rs
@@ -4,7 +4,7 @@ pub fn cargo_manifest_dir() -> Result<PathBuf, std::env::VarError> {
     std::env::var("CARGO_MANIFEST_DIR").map(|s| s.into())
 }
 
-pub fn cargo_workspace_manifest_dir() -> Result<PathBuf, std::env::VarError> {
+pub fn husky_cargo_workspace_manifest_dir() -> Result<PathBuf, std::env::VarError> {
     let base_dir = cargo_manifest_dir()?;
     let mut dir: &Path = &base_dir;
     let mut last_manifest_dir: &Path = &dir;
