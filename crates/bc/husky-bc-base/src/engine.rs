c40fa966ac5fabb76aa265721d61378a31f5aa20
xiyu <dirac12345@gmail.com>
2024-02-11 12:02:07 -0500

    save

--- crates/bc/husky-bc-base/src/engine.rs
+++ crates/bc/husky-bc-base/src/engine.rs
@@ -3,5 +3,6 @@ use crate::{db::BcDb, history::BcHistory, state::BcState};
 pub struct BcEngine<'a> {
     db: &'a BcDb,
     state: BcState,
+    goal: BcState,
     history: BcHistory,
 }
--- crates/bc/husky-bc-base/src/lib.rs
+++ crates/bc/husky-bc-base/src/lib.rs
@@ -6,6 +6,7 @@ pub mod history;
 pub mod rule;
 pub mod state;
 pub mod stmt;
+mod term;
 pub mod var;
 
 use shifted_unsigned_int::ShiftedU32;
