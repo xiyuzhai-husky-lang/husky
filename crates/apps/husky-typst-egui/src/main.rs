b9fef88777c0c872c976fbfa776ca07a4a878927
xiyu <dirac12345@gmail.com>
2024-02-11 03:43:31 -0500

    typst egui partial success

--- crates/apps/husky-typst-egui/src/main.rs
+++ crates/apps/husky-typst-egui/src/main.rs
@@ -9,10 +9,12 @@ use husky_typst::{
     layout::{Frame, FrameItem, GroupItem, Point, Size},
     visualize::TypstShape,
 };
+use render::render;
 use std::sync::mpsc::{Receiver, SyncSender};
 use std::sync::Arc;
 
-mod renderer;
+mod compile;
+mod render;
 mod sandbox;
 
 fn main() {
@@ -48,7 +50,7 @@ impl App {
                 .next()
                 .ok_or_else(|| r#"error"#.into())
         });
-        let (renderer, rendered) = renderer::spawn(cc.egui_ctx.clone());
+        let (renderer, rendered) = compile::spawn_compilation_thread(cc.egui_ctx.clone());
         println!("compiled = {:?}", compiled);
         Self {
             renderer,
@@ -126,87 +128,4 @@ fn translate_paint(paint: &TypstPaint) -> Color32 {
 
 fn linear_to_discrete(red: f32) -> u8 {
     (red * 255.0).round() as u8
-}
-
-fn render_item(painter: &egui::Painter, origin: Pos2, position: Point, item: &FrameItem) {
-    let translate_point = |point: Point| origin + vec2(to_px(point.x), to_px(point.y));
-    let translate_size = |size: Size| vec2(to_px(size.x), to_px(size.y));
-    let translate_stroke = |stroke: Option<TypstFixedStroke>| {
-        stroke.map_or(Stroke::NONE, |stroke| {
-            stroke.thickness;
-            (to_px(stroke.thickness), translate_paint(&stroke.paint)).into()
-        })
-    };
-
-    let position = translate_point(position);
-    match item {
-        FrameItem::Group(GroupItem {
-            frame,
-            transform,
-            clip_path,
-        }) => {
-            assert!(
-                transform.is_identity(),
-                "non-identity transforms not yet implemented"
-            );
-            let inner_painter = if let Some(clip_path) = clip_path {
-                painter.with_clip_rect(Rect::from_min_size(position, translate_size(frame.size())))
-            } else {
-                painter.clone()
-            };
-            render(&inner_painter, frame);
-        }
-        FrameItem::Text(text) => {
-            let font_id = TextStyle::Body.resolve(&painter.ctx().style());
-            painter.text(
-                origin,
-                Align2::CENTER_CENTER,
-                text.text.clone(),
-                font_id,
-                translate_paint(&text.fill),
-            );
-        }
-        FrameItem::Shape(
-            TypstShape {
-                geometry,
-                fill,
-                stroke,
-            },
-            _span,
-        ) => match geometry {
-            TypstGeometry::Line(to_point) => {
-                painter.line_segment(
-                    [position, translate_point(*to_point)],
-                    translate_stroke(stroke.as_ref().cloned()),
-                );
-            }
-            TypstGeometry::Rect(size) => {
-                painter.rect(
-                    Rect::from_min_size(position, translate_size(*size)),
-                    Rounding::default(),
-                    fill.as_ref().map_or(Color32::TRANSPARENT, translate_paint),
-                    translate_stroke(stroke.as_ref().cloned()),
-                );
-            }
-            TypstGeometry::Path(..) => todo!(),
-        },
-        FrameItem::Image(..) => todo!(),
-        FrameItem::Meta(..) => {}
-    }
-}
-
-fn render_inner<'a>(
-    painter: &egui::Painter,
-    origin: Pos2,
-    items: impl Iterator<Item = &'a (Point, FrameItem)>,
-) {
-    for (position, item) in items {
-        render_item(painter, origin, *position, item);
-    }
-}
-
-fn render(painter: &egui::Painter, frame: &Frame) {
-    let origin = painter.clip_rect().left_top();
-    let items = frame.items();
-    render_inner(painter, origin, items);
 }
