84e03663b35f4deffe0b61c451b6f31e278a1d7f
xiyu <dirac12345@gmail.com>
2024-02-13 07:42:27 -0500

    save

--- crates/syntax/husky-ast/src/parser/indent.rs
+++ crates/syntax/husky-ast/src/parser/indent.rs
@@ -6,10 +6,21 @@ impl<'a> AstParser<'a> {
     }
 
     /// do something with indent increased
-    pub(super) fn with_indent(&mut self, f: impl FnOnce(&mut Self) -> AstIdxRange) -> AstIdxRange {
+    pub(super) fn indented(&mut self, f: impl FnOnce(&mut Self) -> AstIdxRange) -> AstIdxRange {
         self.indent.incr();
         let result = f(self);
         self.indent.decr();
+        result
+    }
+
+    pub(super) fn with_indent(
+        &mut self,
+        indent: u32,
+        f: impl FnOnce(&mut Self) -> AstIdxRange,
+    ) -> AstIdxRange {
+        let old_indent = std::mem::replace(&mut self.indent, Indent(indent));
+        let result = f(self);
+        self.indent = old_indent;
         result
     }
 }
