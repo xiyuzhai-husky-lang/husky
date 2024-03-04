f6fc944ae425cc231633676f2749afb42d344bed
xiyu <dirac12345@gmail.com>
2024-03-03 21:24:06 -0500

    clean

--- crates/hir/husky-hir-pattern/src/build.rs
+++
@@ -1,7 +0,0 @@
-use crate::*;
-use husky_syn_expr::SynPatternExprArena;
-
-pub trait BuildHirPattern {
-    fn hir_pattern_db(&self) -> &dyn HirPatternDb;
-    fn syn_pattern_expr_arena(&self) -> &SynPatternExprArena;
-}
--- crates/hir/husky-hir-pattern/src/db.rs
+++
@@ -1,6 +0,0 @@
-use crate::*;
-
-pub trait HirPatternDb: salsa::DbWithJar<HirPatternJar> {}
-
-#[salsa::jar]
-pub struct HirPatternJar(HirPattern);
