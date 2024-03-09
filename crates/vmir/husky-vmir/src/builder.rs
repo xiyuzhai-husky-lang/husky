9c55f501a8aea64232a30f6882dcbaf3431b8797
xiyu <dirac12345@gmail.com>
2024-03-08 20:00:55 -0500

    rename

--- crates/vmir/husky-vmir/src/builder.rs
+++ crates/vmir/husky-vmir/src/builder.rs
@@ -1,11 +1,17 @@
-use crate::{expr::VmirExprArena, stmt::VmirStmtArena, vmir::VmirData};
+use crate::{
+    expr::{VmirExprArena, VmirExprData, VmirExprIdx},
+    stmt::VmirStmtArena,
+    vmir::VmirData,
+};
 use husky_coword::Ident;
 use husky_hir_eager_expr::{HirEagerExprArena, HirEagerStmtArena};
+use husky_linkage::instantiation::LinInstantiation;
 
 pub(crate) struct VmirExprBuilder<'db> {
     db: &'db ::salsa::Db,
     hir_eager_expr_arena: &'db HirEagerExprArena,
     hir_eager_stmt_arena: &'db HirEagerStmtArena,
+    instantiation: &'db LinInstantiation,
     vmir_expr_arena: VmirExprArena,
     vmir_stmt_arena: VmirStmtArena,
     buffer: Vec<VmirData>,
@@ -19,5 +25,9 @@ impl<'db> VmirExprBuilder<'db> {
 
     pub(crate) fn hir_eager_stmt_arena(&self) -> &'db HirEagerStmtArena {
         self.hir_eager_stmt_arena
+    }
+
+    pub(crate) fn alloc_expr(&mut self, expr_data: VmirExprData) -> VmirExprIdx {
+        self.vmir_expr_arena.alloc_one(expr_data)
     }
 }
