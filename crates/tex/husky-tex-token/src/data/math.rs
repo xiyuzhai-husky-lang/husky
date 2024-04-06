dd20607f95ceded4dd8afcf2df7fbde6e47e7f96
xiyu <dirac12345@gmail.com>
2024-04-05 23:03:44 -0400

    lex number in tex

--- crates/tex/husky-tex-token/src/data/math.rs
+++ crates/tex/husky-tex-token/src/data/math.rs
@@ -1,3 +1,4 @@
+use super::*;
 use husky_tex_math_letter::TexMathLetter;
 
 #[salsa::derive_debug_with_db]
@@ -7,6 +8,7 @@ pub enum TexMathTokenData {
     Command(TexMathCommand),
     Delimiter(TexMathDelimiter),
     Letter(TexMathLetter),
+    Nat32(u32),
     Other(char),
 }
 
@@ -22,14 +24,98 @@ pub enum TexMathCommand {
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum TexMathDelimiter {
-    /// '{', '}'
+    /// `{`,  `}`
     Curl,
-    /// '[', ']'
+    /// `[`, `]`
     Box,
-    /// '(', ')'
+    /// `(`, `)`
     Par,
-    /// '\{', '\}'
+    /// `\{`, `\}`
     Set,
+    /// `{`, `}`
+    Attach,
 }
 
 pub enum Script {}
+
+impl<'a> TexTokenizer<'a> {
+    pub(super) fn next_math_token_data(&mut self) -> Option<TexMathTokenData> {
+        match self.chars.peek()? {
+            '\\' => todo!(),
+            numeric if numeric.is_numeric() => {
+                let numeric_str_slice = self.chars.next_numeric_str_slice();
+                match numeric_str_slice.parse::<u32>() {
+                    Ok(number) => Some(number.into()), // ad hoc
+                    Err(_) => {
+                        use husky_print_utils::p;
+
+                        p!(numeric_str_slice);
+                        todo!()
+                    }
+                }
+            }
+            '{' => todo!(),
+            c => {
+                self.chars.next();
+                match TexMathLetter::try_from_char(c) {
+                    Some(letter) => Some(letter.into()),
+                    None => Some(c.into()),
+                }
+            }
+        }
+    }
+}
+
+#[test]
+fn next_text_token_data_works() {
+    fn t(input: &str, expected: &Expect) {
+        let db = &DB::default();
+        let tokenizer = TexTokenizer::new(db, input, TexMode::Math);
+        let tokens: Vec<_> = tokenizer.map(|(_, token_data)| token_data).collect();
+        expected.assert_debug_eq(&(tokens.debug(db)));
+    }
+    t(
+        "hello",
+        &expect![[r#"
+            [
+                TexTokenData::Math(
+                    TexMathTokenData::Letter(
+                        LowerH,
+                    ),
+                ),
+                TexTokenData::Math(
+                    TexMathTokenData::Letter(
+                        LowerE,
+                    ),
+                ),
+                TexTokenData::Math(
+                    TexMathTokenData::Letter(
+                        LowerL,
+                    ),
+                ),
+                TexTokenData::Math(
+                    TexMathTokenData::Letter(
+                        LowerL,
+                    ),
+                ),
+                TexTokenData::Math(
+                    TexMathTokenData::Letter(
+                        LowerO,
+                    ),
+                ),
+            ]
+        "#]],
+    );
+    t(
+        "0",
+        &expect![[r#"
+            [
+                TexTokenData::Math(
+                    TexMathTokenData::Nat32(
+                        0,
+                    ),
+                ),
+            ]
+        "#]],
+    )
+}
--- crates/tex/husky-tex-token/src/lib.rs
+++ crates/tex/husky-tex-token/src/lib.rs
@@ -1,10 +1,10 @@
 pub mod data;
 pub mod idx;
+pub mod lexer;
 pub mod reserved_char;
 pub mod storage;
 #[cfg(test)]
 mod tests;
-pub mod tokenizer;
 
 #[cfg(test)]
 use self::tests::*;
