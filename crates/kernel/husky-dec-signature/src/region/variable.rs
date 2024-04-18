2cc47265c898bbb9d5810de38b61b88b792ccfb3
xiyu <dirac12345@gmail.com>
2024-04-18 18:31:03 -0400

    save

--- crates/kernel/husky-dec-signature/src/region/variable.rs
+++ crates/kernel/husky-dec-signature/src/region/variable.rs
@@ -54,6 +54,44 @@ impl DecSymbolicVariableSignature {
     }
 }
 
+/// # constructors
+
+impl DecSymbolicVariableRegion {
+    /// will initialize `inherited_syn_symbol_terms`;
+    /// but will leave current_syn_symbol_terms unintialized;
+    /// `self_ty_term` is set to that of parent if parent exists, otherwise none;
+    /// `self_value_term` is set to that of parent if parent exists, otherwise none
+    pub(crate) fn new(
+        parent: Option<&DecSymbolicVariableRegion>,
+        syn_expr_region_data: &SynExprRegionData,
+        dec_term_menu: &DecTermMenu,
+    ) -> Self {
+        let registry = parent.map_or(Default::default(), |parent| parent.registry.clone());
+        let implicit_self_lifetime = syn_expr_region_data
+            .has_self_lifetime()
+            .then_some(dec_term_menu.implicit_self_lifetime());
+        let implicit_self_place = syn_expr_region_data
+            .has_self_place()
+            .then_some(dec_term_menu.implicit_self_place());
+        let symbol_name_map = parent.map_or(Default::default(), |parent| parent.names.clone());
+        Self {
+            registry,
+            signatures: SymbolOrderedMap::new(parent.map(|parent| &parent.signatures)),
+            names: symbol_name_map,
+            self_ty: parent.map(|parent| parent.self_ty).flatten(),
+            self_value: parent.map(|parent| parent.self_value).flatten(),
+            self_lifetime: implicit_self_lifetime,
+            self_place: implicit_self_place,
+            autos: implicit_self_lifetime
+                .into_iter()
+                .chain(implicit_self_place)
+                .collect(),
+        }
+    }
+}
+
+/// # getters
+
 impl DecSymbolicVariableRegion {
     pub fn self_lifetime(&self) -> Option<DecSymbolicVariable> {
         self.self_lifetime
@@ -66,9 +104,139 @@ impl DecSymbolicVariableRegion {
     pub fn auto_template_parameter_symbols(&self) -> &[DecSymbolicVariable] {
         &self.autos
     }
+    /// this only works on type definitions
+    ///
+    /// example:
+    /// ```husky
+    /// enum Animal<T> where
+    /// | Dog
+    /// | Cat
+    /// ```
+    ///
+    /// then self type term is `Animal T`
+    fn ty_defn_self_ty_term(&self, db: &::salsa::Db, ty_path: TypePath) -> DecTerm {
+        let mut self_ty: DecTerm = DecItemPath::Type(ty_path.into()).into();
+        for current_syn_symbol_signature in self.signatures.current_syn_symbol_map().iter().copied()
+        {
+            match current_syn_symbol_signature.kind {
+                VariableSignatureKind::TemplateParameter => {
+                    let argument = current_syn_symbol_signature
+                        .term()
+                        .expect("should have term");
+                    self_ty = self_ty.apply(db, argument)
+                }
+                VariableSignatureKind::ParenateParameter => unreachable!(),
+                VariableSignatureKind::FieldVariable => break,
+            }
+        }
+        self_ty
+    }
 
-    pub(crate) fn svar_registry_mut(&mut self) -> &mut DecSymbolicVariableRegistry {
+    pub fn self_ty(&self) -> Option<DecTerm> {
+        self.self_ty
+    }
+
+    pub fn symbol_name_map(&self) -> &DecSymbolicVariableNameMap {
+        &self.names
+    }
+
+    pub fn self_value(&self) -> Option<DecSymbolicVariable> {
+        self.self_value
+    }
+
+    pub fn inherited_variable_signature(
+        &self,
+        inherited_variable_idx: InheritedSymbolicVariableIdx,
+    ) -> DecSymbolicVariableSignature {
+        self.signatures[inherited_variable_idx]
+    }
+
+    /// None for variables defined in the body
+    pub fn current_parameter_variable_signature(
+        &self,
+        current_variable_idx: CurrentVariableIdx,
+    ) -> Option<DecSymbolicVariableSignature> {
+        self.signatures
+            .current_syn_symbol_map()
+            .get(current_variable_idx.index())
+            .copied()
+    }
+
+    pub(crate) fn registry_mut(&mut self) -> &mut DecSymbolicVariableRegistry {
         &mut self.registry
+    }
+}
+
+/// # actions
+
+impl DecSymbolicVariableRegion {
+    pub(crate) fn infer_self_ty_parameter_and_self_value_parameter(
+        &mut self,
+        db: &::salsa::Db,
+        toolchain: Toolchain,
+        region_path: SynNodeRegionPath,
+        symbol_region: &VariableRegionData,
+    ) {
+        if symbol_region.allow_self_ty().to_bool() && self.self_ty.is_none() {
+            self.self_ty = match region_path {
+                SynNodeRegionPath::Decl(ItemSynNodePath::MajorItem(
+                    MajorItemSynNodePath::Trait(_),
+                )) => Some(self.new_self_ty_symbol(toolchain, db).into()),
+                SynNodeRegionPath::Decl(ItemSynNodePath::MajorItem(
+                    MajorItemSynNodePath::Type(ty_node_path),
+                )) => Some(
+                    self.ty_defn_self_ty_term(
+                        db,
+                        ty_node_path
+                            .unambiguous_item_path(db)
+                            .expect("should have valid item path"),
+                    ),
+                ),
+                SynNodeRegionPath::Decl(ItemSynNodePath::ImplBlock(syn_node_path)) => {
+                    match syn_node_path {
+                        ImplBlockSynNodePath::TypeImplBlock(syn_node_path) => {
+                            None // reserved for later stage
+                        }
+                        ImplBlockSynNodePath::TraitForTypeImplBlock(impl_block_path) => {
+                            match impl_block_path.ty_sketch(db) {
+                                TypeSketch::DeriveAny => {
+                                    Some(self.new_self_ty_symbol(toolchain, db).into())
+                                }
+                                TypeSketch::Path(ty_path) => None, // reserved for later stage
+                            }
+                        }
+                        ImplBlockSynNodePath::IllFormedImplBlock(_) => None,
+                    }
+                }
+                _ => unreachable!(),
+            }
+        }
+        if symbol_region.allow_self_value().to_bool() && self.self_value.is_none() {
+            self.self_value = Some(
+                DecSymbolicVariable::new_self_value(
+                    db,
+                    toolchain,
+                    &mut self.registry,
+                    self.self_ty.expect("self type should exists"),
+                )
+                .into(),
+            )
+        }
+    }
+
+    fn new_self_ty_symbol(
+        &mut self,
+        toolchain: Toolchain,
+        db: &::salsa::Db,
+    ) -> DecSymbolicVariable {
+        let var = DecSymbolicVariable::new_self_ty(db, toolchain, &mut self.registry);
+        self.autos.push(var);
+        var
+    }
+
+    pub(crate) fn set_self_ty(&mut self, self_ty: Option<DecTerm>) {
+        debug_assert!(self.self_ty.is_none());
+        self.self_ty = self_ty
     }
 
     #[inline(always)]
@@ -152,165 +320,5 @@ impl DecSymbolicVariableRegion {
             self.names.add(symbol, name)
         }
         self.signatures.insert_next(idx, signature)
-    }
-
-    pub fn symbol_name_map(&self) -> &DecSymbolicVariableNameMap {
-        &self.names
-    }
-}
-
-impl DecSymbolicVariableRegion {
-    /// will initialize `inherited_syn_symbol_terms`;
-    /// but will leave current_syn_symbol_terms unintialized;
-    /// `self_ty_term` is set to that of parent if parent exists, otherwise none;
-    /// `self_value_term` is set to that of parent if parent exists, otherwise none
-    pub(crate) fn new(
-        parent: Option<&DecSymbolicVariableRegion>,
-        syn_expr_region_data: &SynExprRegionData,
-        dec_term_menu: &DecTermMenu,
-    ) -> Self {
-        let registry = parent.map_or(Default::default(), |parent| parent.registry.clone());
-        let implicit_self_lifetime = syn_expr_region_data
-            .has_self_lifetime()
-            .then_some(dec_term_menu.implicit_self_lifetime());
-        let implicit_self_place = syn_expr_region_data
-            .has_self_place()
-            .then_some(dec_term_menu.implicit_self_place());
-        let symbol_name_map = parent.map_or(Default::default(), |parent| parent.names.clone());
-        Self {
-            registry,
-            signatures: SymbolOrderedMap::new(parent.map(|parent| &parent.signatures)),
-            names: symbol_name_map,
-            self_ty: parent.map(|parent| parent.self_ty).flatten(),
-            self_value: parent.map(|parent| parent.self_value).flatten(),
-            self_lifetime: implicit_self_lifetime,
-            self_place: implicit_self_place,
-            autos: implicit_self_lifetime
-                .into_iter()
-                .chain(implicit_self_place)
-                .collect(),
-        }
-    }
-
-    pub(crate) fn infer_self_ty_parameter_and_self_value_parameter(
-        &mut self,
-        db: &::salsa::Db,
-        toolchain: Toolchain,
-        region_path: SynNodeRegionPath,
-        symbol_region: &VariableRegionData,
-    ) {
-        if symbol_region.allow_self_ty().to_bool() && self.self_ty.is_none() {
-            self.self_ty = match region_path {
-                SynNodeRegionPath::Decl(ItemSynNodePath::MajorItem(
-                    MajorItemSynNodePath::Trait(_),
-                )) => Some(self.new_self_ty_symbol(toolchain, db).into()),
-                SynNodeRegionPath::Decl(ItemSynNodePath::MajorItem(
-                    MajorItemSynNodePath::Type(ty_node_path),
-                )) => Some(
-                    self.ty_defn_self_ty_term(
-                        db,
-                        ty_node_path
-                            .unambiguous_item_path(db)
-                            .expect("should have valid item path"),
-                    ),
-                ),
-                SynNodeRegionPath::Decl(ItemSynNodePath::ImplBlock(syn_node_path)) => {
-                    match syn_node_path {
-                        ImplBlockSynNodePath::TypeImplBlock(syn_node_path) => {
-                            None // reserved for later stage
-                        }
-                        ImplBlockSynNodePath::TraitForTypeImplBlock(impl_block_path) => {
-                            match impl_block_path.ty_sketch(db) {
-                                TypeSketch::DeriveAny => {
-                                    Some(self.new_self_ty_symbol(toolchain, db).into())
-                                }
-                                TypeSketch::Path(ty_path) => None, // reserved for later stage
-                            }
-                        }
-                        ImplBlockSynNodePath::IllFormedImplBlock(_) => None,
-                    }
-                }
-                _ => unreachable!(),
-            }
-        }
-        if symbol_region.allow_self_value().to_bool() && self.self_value.is_none() {
-            self.self_value = Some(
-                DecSymbolicVariable::new_self_value(
-                    db,
-                    toolchain,
-                    &mut self.registry,
-                    self.self_ty.expect("self type should exists"),
-                )
-                .into(),
-            )
-        }
-    }
-    fn new_self_ty_symbol(
-        &mut self,
-        toolchain: Toolchain,
-        db: &::salsa::Db,
-    ) -> DecSymbolicVariable {
-        let var = DecSymbolicVariable::new_self_ty(db, toolchain, &mut self.registry);
-        self.autos.push(var);
-        var
-    }
-
-    /// this only works on type definitions
-    ///
-    /// example:
-    /// ```husky
-    /// enum Animal<T> where
-    /// | Dog
-    /// | Cat
-    /// ```
-    ///
-    /// then self type term is `Animal T`
-    fn ty_defn_self_ty_term(&self, db: &::salsa::Db, ty_path: TypePath) -> DecTerm {
-        let mut self_ty: DecTerm = DecItemPath::Type(ty_path.into()).into();
-        for current_syn_symbol_signature in self.signatures.current_syn_symbol_map().iter().copied()
-        {
-            match current_syn_symbol_signature.kind {
-                VariableSignatureKind::TemplateParameter => {
-                    let argument = current_syn_symbol_signature
-                        .term()
-                        .expect("should have term");
-                    self_ty = self_ty.apply(db, argument)
-                }
-                VariableSignatureKind::ParenateParameter => unreachable!(),
-                VariableSignatureKind::FieldVariable => break,
-            }
-        }
-        self_ty
-    }
-
-    pub fn self_ty(&self) -> Option<DecTerm> {
-        self.self_ty
-    }
-
-    pub(crate) fn set_self_ty(&mut self, self_ty: Option<DecTerm>) {
-        debug_assert!(self.self_ty.is_none());
-        self.self_ty = self_ty
-    }
-
-    pub fn self_value(&self) -> Option<DecSymbolicVariable> {
-        self.self_value
-    }
-
-    pub fn inherited_variable_signature(
-        &self,
-        inherited_variable_idx: InheritedSymbolicVariableIdx,
-    ) -> DecSymbolicVariableSignature {
-        self.signatures[inherited_variable_idx]
-    }
-
-    /// None for variables defined in the body
-    pub fn current_parameter_variable_signature(
-        &self,
-        current_variable_idx: CurrentVariableIdx,
-    ) -> Option<DecSymbolicVariableSignature> {
-        self.signatures
-            .current_syn_symbol_map()
-            .get(current_variable_idx.index())
-            .copied()
     }
 }
