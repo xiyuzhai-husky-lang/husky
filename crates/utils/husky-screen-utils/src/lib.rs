fc87bee33f38d46a8d15dbb649abcaeb34bdad85
xiyu <dirac12345@gmail.com>
2024-04-19 14:44:44 -0400

    guarantee screen is lightened when asking

---
+++ crates/utils/husky-screen-utils/src/lib.rs
@@ -0,0 +1,25 @@
+use std::process::Command;
+
+pub fn light_up_screen() {
+    // Define the script path and arguments.
+    let script_path = "scripts/adjust_screen_brightness.sh";
+    let args = ["1.0", "HDMI-1-0", "0.5"];
+
+    // Use Command to execute the script with arguments.
+    let output = Command::new(script_path).args(&args).output();
+
+    match output {
+        Ok(output) => {
+            if !output.status.success() {
+                // Print stderr if the command failed.
+                panic!(
+                    "Script execution failed: {}",
+                    String::from_utf8_lossy(&output.stderr)
+                );
+            }
+        }
+        Err(e) => {
+            panic!("Failed to execute script: {}", e)
+        }
+    }
+}
