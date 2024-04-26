3ddc800106b64c79243cae66b6ddc32869fb9832
xiyu <dirac12345@gmail.com>
2024-04-25 20:43:48 -0400

    save

--- crates/semantics/husky-sem-expr/src/pattern/refutability.rs
+++ crates/semantics/husky-sem-expr/src/pattern/refutability.rs
@@ -1,18 +1,109 @@
 use crate::SemExprBuilder;
-use husky_syn_expr::SynPatternIdx;
+use husky_entity_path::TypeVariantPath;
+use husky_fly_term::FlyTerm;
+use husky_syn_expr::{SynPatternData, SynPatternIdx};
 
 struct PatternProduct {
     patterns: Vec<SynPatternIdx>,
 }
 
-struct PatternChoices {
-    choices: Vec<PatternProduct>,
+struct PatternProductChoices {
+    products: Vec<PatternProduct>,
 }
 
-pub enum RefutationTarget {}
+#[derive(Debug, Clone, Copy)]
+pub enum Refutation {
+    All { ty: FlyTerm },
+    TypeVariant { ty: FlyTerm, path: TypeVariantPath },
+}
+
+pub struct RefutationProduct {
+    refutations: Vec<Refutation>,
+}
+
+pub enum SemPatternRefutabilityError {
+    Original(OriginalSemPatternRefutabilityError),
+    Derived(DerivedSemPatternRefutabilityError),
+}
+
+pub enum OriginalSemPatternRefutabilityError {}
+
+pub enum DerivedSemPatternRefutabilityError {
+    Todo,
+}
+
+pub type SemPatternRefutabilityResult<T> = Result<T, ()>;
 
 impl<'a> SemExprBuilder<'a> {
-    fn refute(choices: PatternChoices, target: RefutationTarget) -> PatternChoices {
-        todo!()
+    /// Firstly, try to see if one choice covers everything;
+    /// secondly, distribute the problem to each factor.
+    ///
+    /// If the following matches agains target `(A, B)`
+    /// ```husky
+    /// | (patt11, patt12)
+    /// | (patt21, patt22)
+    /// ```
+    ///,
+    /// we will first see if either `(patt11, patt12)` or `(patt21, patt22)` will cover everything,
+    /// then we will reduce the problem to two subproblems
+    /// - refuting `patt11 | patt21` against `A`
+    /// - refuting `patt12 | patt22` against `B`
+    ///
+    /// but this is wrong haha,
+    ///
+    /// it remains to see what's the best way to do it
+    fn refute_product(
+        &self,
+        pattern_product_choices: PatternProductChoices,
+        refutation_product: RefutationProduct,
+    ) -> SemPatternRefutabilityResult<bool> {
+        for product in &pattern_product_choices.products {
+            if self.is_product_trivial(product)? {
+                return Ok(true);
+            }
+        }
+        let n = refutation_product.refutations.len();
+        for i in 0..n {
+            let pattern_choices = pattern_product_choices
+                .products
+                .iter()
+                .map(|product| product.patterns[i])
+                .collect();
+            if !self.refute(pattern_choices, refutation_product.refutations[i])? {
+                return Ok(false);
+            }
+        }
+        Ok(true)
+    }
+
+    fn is_product_trivial(&self, product: &PatternProduct) -> SemPatternRefutabilityResult<bool> {
+        for pattern in &product.patterns {
+            match self.syn_expr_region_data().pattern_expr_arena()[pattern] {
+                SynPatternData::Ident { .. } => (),
+                _ => return Ok(false),
+            }
+        }
+        Ok(true)
+    }
+
+    fn refute(
+        &self,
+        choices: Vec<SynPatternIdx>,
+        refutation: Refutation,
+    ) -> SemPatternRefutabilityResult<bool> {
+        // ```
+        // | Animal::Cat { weight, height }
+        // | Animal::Dog { weight, height }
+        // ```
+        match refutation {
+            Refutation::All { ty } => {
+                todo!("look into what kind of type `ty` is, and then decompose it and use refute_product")
+            }
+            Refutation::TypeVariant { ty, path } => {
+                todo!(
+                    "look into what kind of type variant `path` of `ty` is, and then decompose it and use refute_product"
+                )
+            }
+        }
     }
 }
