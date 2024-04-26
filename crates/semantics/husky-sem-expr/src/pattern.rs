cd76dfaa322f04e069b47cfc512f5303643f5a7d
xiyu <dirac12345@gmail.com>
2024-04-25 20:04:40 -0400

    save

--- crates/semantics/husky-sem-expr/src/pattern.rs
+++ crates/semantics/husky-sem-expr/src/pattern.rs
@@ -7,25 +7,3 @@ use husky_regional_token::{
 };
 use husky_syn_expr::SynPatternData;
 use husky_token_data::LiteralTokenData;
-
-#[salsa::derive_debug_with_db]
-#[derive(Debug, PartialEq, Eq)]
-pub enum SemaPatternData {
-    /// example: `1`
-    Literal {
-        regional_token_idx: RegionalTokenIdx,
-        literal: LiteralTokenData,
-    },
-    /// example: `a`
-    Ident {
-        symbol_modifier_tokens: Option<EphemSymbolModifierRegionalTokens>,
-        ident_token: IdentRegionalToken,
-    },
-}
-
-#[salsa::derive_debug_with_db]
-#[derive(Debug, PartialEq, Eq)]
-pub struct SemaPatternEntry {
-    data_result: SemaExprDataResult<SemaPatternData>,
-    ty_result: SemaExprTypeResult<FlyTerm>,
-}
--- crates/semantics/husky-sem-expr/src/pattern/refutability.rs
+++ crates/semantics/husky-sem-expr/src/pattern/refutability.rs
@@ -1,1 +1,15 @@
+use husky_syn_expr::SynPatternIdx;
 
+struct PatternProduct {
+    patterns: Vec<SynPatternIdx>,
+}
+
+struct PatternChoices {
+    choices: Vec<PatternProduct>,
+}
+
+pub enum RefutationTarget {}
+
+fn refute(choices: PatternChoices, target: RefutationTarget) -> PatternChoices {
+    todo!()
+}
