7466ce90d72cd0660a478c4da879f0768b906f93
xiyu <dirac12345@gmail.com>
2024-05-17 09:58:09 -0400

    save

--- crates/linkage/husky-javelin/src/lib.rs
+++ crates/linkage/husky-javelin/src/lib.rs
@@ -6,7 +6,6 @@
 //! Javelins are divided into two classes (as inspired by Diablo 2):
 //! - Amazon. Those javelins doesn't contain any template parameters and thus are always collected. In a sense, amazons are always there, nonvanishing.
 //! - Valkyrie. Those javelins contain nonempty template parameters and thus are collected if invoked. So we need to do some "tree walk" to completely collect valkyrie javelins. In a sense, valkyries are summoned by amazons.
-#![feature(associated_type_bounds)]
 /// the name amazon comes from diablo 2
 pub mod amazon;
 pub mod instantiation;
