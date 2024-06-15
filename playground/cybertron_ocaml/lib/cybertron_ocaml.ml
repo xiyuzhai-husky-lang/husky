b35e2841eb04e59e21e8a88b80c1b505bd74487f
xiyu <dirac12345@gmail.com>
2024-06-15 06:31:00 -0400

    attention in ocaml

--- playground/cybertron_ocaml/lib/cybertron_ocaml.ml
+++ playground/cybertron_ocaml/lib/cybertron_ocaml.ml
@@ -1,2 +1,10 @@
 open Printf
 let say_hello name = printf "Hello, %s!\n" name
+
+let boolAttention f qs ks vs = List.map (fun q -> f (
+  List.combine ks vs |> List.filter (fun (k, _) -> q k) |> List.map (fun (_, v) -> v)
+)) qs
+
+let floatAttention (f: (float * 'v) list -> 'o) qs ks vs = List.map (fun q -> f (
+  List.combine ks vs |> List.map (fun (k, v) -> (q k, v))
+)) qs
