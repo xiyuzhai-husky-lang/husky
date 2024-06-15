fff080b35282ba5336c17b2ab416196462fd03a6
xiyu <dirac12345@gmail.com>
2024-06-14 23:37:40 -0400

    save

--- playground/cybertron-hs/src/Lib.hs
+++ playground/cybertron-hs/src/Lib.hs
@@ -4,11 +4,11 @@ someFunc :: IO ()
 someFunc = do
   putStrLn $ show $ map_simple [2, 3]
   putStrLn $ show $ initial_parse [LiteralToken $ Int 1, LiteralToken $ Int 2]
+  putStrLn $ show $ newExprArena [LiteralToken $ Int 1, LiteralToken $ Int 2]
   putStrLn "someFunc"
 
 map_simple :: [Int] -> [Int]
 map_simple is = map (+ 1) is
-
 data Expr =
   LiteralExpr Literal
   | BinaryExpr Expr BinaryOpr Expr
@@ -33,3 +33,18 @@ initial_parse tokens = map (
     LiteralToken lit -> Right $ LiteralExpr lit
     OprToken opr -> Left opr
   ) tokens
+
+newtype ExprArena = ExprArena [ExprArenaEntry]
+  deriving Show
+
+data ExprArenaEntry = ExprArenaEntry { normal:: Maybe Expr, extra:: Maybe Expr }
+  deriving Show
+
+data ExprIdx = Normal Int | Extra Int
+
+newExprArena:: [Token] -> [ExprArenaEntry]
+newExprArena tokens = map (\_ -> ExprArenaEntry { normal = Nothing, extra = Nothing }) tokens
+
+getExpr:: ExprArena -> ExprIdx -> Maybe Expr
+getExpr (ExprArena arena) (Normal idx) = normal $ arena!!idx
+getExpr (ExprArena arena) (Extra idx) = extra $ arena!!idx
