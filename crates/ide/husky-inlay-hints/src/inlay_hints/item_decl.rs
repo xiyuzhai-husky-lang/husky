c30a11caa236d42cd78abfceb0303907d433ed49
xiyu <dirac12345@gmail.com>
2024-06-17 01:21:36 -0400

    feat: inlay hints start to work

--- crates/ide/husky-inlay-hints/src/inlay_hints/item_decl.rs
+++ crates/ide/husky-inlay-hints/src/inlay_hints/item_decl.rs
@@ -5,7 +5,11 @@ use husky_entity_path::path::ItemPath;
 pub(crate) fn item_decl_inlay_hints(db: &::salsa::Db, item_path_id: ItemPathId) -> Vec<InlayHint> {
     let item_path = item_path_id.item_path(db);
     match item_path {
-        ItemPath::Submodule(_, _) => vec![],
+        ItemPath::Submodule(_, _) => vec![InlayHint {
+            position: InlayHintPosition::EndOfRegionSpaced,
+            label: InlayHintLabel::String("testing inlay hints".to_string()),
+            kind: InlayHintKind::AttrInferred,
+        }],
         ItemPath::MajorItem(_) => vec![],
         ItemPath::AssocItem(_) => vec![],
         ItemPath::TypeVariant(_, _) => vec![],
