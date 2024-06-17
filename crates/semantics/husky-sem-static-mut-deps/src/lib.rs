32aedbb3e83df26df02496aff6d570c46cddd47b
xiyu <dirac12345@gmail.com>
2024-06-17 10:40:42 -0400

    init sem deps

--- crates/semantics/husky-sem-static-mut-deps/src/lib.rs
+++ crates/semantics/husky-sem-static-mut-deps/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
+mod jar;
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod tests;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+use self::jar::SemStaticMutDepsJar as Jar;
+#[cfg(test)]
+use self::tests::*;
