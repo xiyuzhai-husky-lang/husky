ea8408976ef3060ac18cc3883ed1cac75dbf0a5d
xiyu <dirac12345@gmail.com>
2024-06-24 18:34:54 -0400

    fix bug!

--- crates/semantics/husky-sem-static-mut-deps/src/jar.rs
+++ crates/semantics/husky-sem-static-mut-deps/src/jar.rs
@@ -1,6 +1,6 @@
 #[salsa::jar]
 pub struct SemStaticMutDepsJar(
     crate::region::SemStaticMutDepsRegion,
-    crate::graph_dynamics::item_sem_static_mut_deps_cycle_group_initial_value,
+    crate::graph_dynamics::item_sem_static_mut_deps_initial_value,
     crate::graph_dynamics::item_sem_static_mut_deps_cycle_group_final_values,
 );
