93a2d79d163f3ed53b2d8e2277d9971e2d61bf3a
xiyu <dirac12345@gmail.com>
2024-06-29 10:02:55 -0400

    make extension better

--- extensions/husky-analyzer/src/commands.ts
+++ extensions/husky-analyzer/src/commands.ts
@@ -4,58 +4,26 @@ import type { Context } from "./context";
 
 export type Cmd = (...args: any[]) => unknown;
 function gotoLocation(ctx: Context): Cmd {
-    return async (locationLink: lc.LocationLink) => {
-        const client = ctx.client;
-        if (client) {
-            const uri = client.protocol2CodeConverter.asUri(
-                locationLink.targetUri
-            );
-            let range = client.protocol2CodeConverter.asRange(
-                locationLink.targetSelectionRange
-            );
-            // collapse the range to a cursor position
-            range = range.with({ end: range.start });
+  return async (locationLink: lc.LocationLink) => {
+    const client = ctx.client;
+    if (client) {
+      const uri = client.protocol2CodeConverter.asUri(locationLink.targetUri);
+      let range = client.protocol2CodeConverter.asRange(locationLink.targetSelectionRange);
+      // collapse the range to a cursor position
+      range = range.with({ end: range.start });
 
-            await vscode.window.showTextDocument(uri, { selection: range });
-        }
-    };
-}
-
-function analyzerStatus(ctx: Context): Cmd {
-    return async () => {
-        console.log("todo: analyzer status");
-    };
-}
-
-function memoryUsage(ctx: Context): Cmd {
-    return async () => {
-        console.log("todo: memory usage");
-    };
+      await vscode.window.showTextDocument(uri, { selection: range });
+    }
+  };
 }
 
 function notImplemented(ctx: Context): Cmd {
-    return async () => {
-        console.log("todo: not implemented");
-    };
+  return async () => {
+    console.error("todo: not implemented");
+  };
 }
 
+// note: make it coherent with those ("commands", "menus") in package.json
 export const NAMED_COMMAND_FACTORIES: [string, (ctx: Context) => Cmd][] = [
-    ["analyzerStatus", analyzerStatus],
-    ["joinLines", notImplemented],
-    ["memoryUsage", memoryUsage],
-    ["onEnter", notImplemented],
-    ["openCorgiToml", notImplemented],
-    ["openDocs", notImplemented],
-    ["parentModule", notImplemented],
-    ["reloadWorkspace", notImplemented],
-    ["reload", notImplemented],
-    ["run", notImplemented],
-    ["serverVersion", notImplemented],
-    ["ssr", notImplemented],
-    ["syntaxTree", notImplemented],
-    ["toggleInlayHints", notImplemented],
-    ["viewHir", notImplemented],
-    ["viewFileText", notImplemented],
-    // Internal commands which are invoked by the server.
-    ["gotoLocation", notImplemented],
+  ["deps", notImplemented],
 ];
