6fc5b018c96e17633371955e81d924ffbbdb37bd
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-07-12 16:13:53 -0700

    put music in assets

--- scripts/play_update_expect_failure_music.sh
+++ scripts/play_update_expect_failure_music.sh
@@ -1,2 +1,2 @@
-mpg321 ~/Music/update_expect_failure.mp3 >/dev/null 2>&1
+mpg321 assets/music/update_expect_failure.mp3 >/dev/null 2>&1
 exit 1
--- scripts/play_update_expect_success_music.sh
+++ scripts/play_update_expect_success_music.sh
@@ -1,1 +1,1 @@
-mpg321 ~/Music/update_expect_success.mp3 >/dev/null 2>&1
+mpg321 assets/music/update_expect_success.mp3 >/dev/null 2>&1
