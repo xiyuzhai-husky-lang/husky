de1087a7a51f142f012600e5b4259184d92c70e5
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-07-12 20:23:25 -0700

    adjust build

--- scripts/ubuntu_setup.sh
+++ scripts/ubuntu_setup.sh
@@ -4,20 +4,23 @@ sudo apt install curl
 if ["$(which cargo)" = ""]; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
-cargo install trunk
 rustup target add wasm32-unknown-unknown
 if ["$(which npm)" = ""]; then
     curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
     sudo apt-get install -y nodejs
 fi
+sudo apt install clang
+sudo apt install lld
+sudo apt install pkg-config
+sudo apt install libssl-dev
+sudo apt install libopenblas-dev
+sudo apt install mpg321
+. ~/.cargo/env
+cargo build
+make vscode
+sudo snap install scc
 if ["$(which lake)" = ""]; then
     echo "needs to install lake!"
     exit 1
 fi
-sudo apt install clang
-sudo apt-get install lld
-. ~/.cargo/env
-cargo build
 lake build
-make vscode
-sudo snap install scc
