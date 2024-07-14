3f9f10233d832ec0c9f5f9ec7f61ba521dee0e8d
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-07-14 14:03:58 -0700

    init hir static var deps

---
+++ crates/hir/husky-hir-lazy-expr/src/static_var_deps.rs
@@ -0,0 +1,21 @@
+use crate::*;
+use husky_entity_path::path::ItemPath;
+use husky_sem_expr::SemExprRegion;
+
+pub struct HirLazyStaticVarDeps();
+
+pub enum HirLazyStaticVarDep {
+    Item(ItemPath),
+}
+
+#[salsa::derive_debug_with_db]
+#[derive(Debug, PartialEq, Eq)]
+pub struct HirLazyExprStaticVarDepsRegion {}
+
+#[salsa::tracked(return_ref)]
+pub fn hir_lazy_expr_static_var_deps_region(
+    db: &::salsa::Db,
+    sem_expr_region: SemExprRegion,
+) -> HirLazyExprStaticVarDepsRegion {
+    todo!()
+}
