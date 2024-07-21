9facf3f22dfc1202a8cc53952a59323c8a5dc8ff
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-07-20 23:02:17 -0700

    better timing

--- scripts/play_update_expect_failure_music.sh
+++ scripts/play_update_expect_failure_music.sh
@@ -1,5 +1,5 @@
 MONITOR=HDMI-2
-DURATION=0.3
+DURATION=1
 scripts/adjust_screen_brightness.sh 1.0 $MONITOR $DURATION
 mpg321 assets/music/elden-ring-you-died-sound-effect.mp3 >/dev/null 2>&1
 exit 1
--- scripts/play_update_expect_success_music.sh
+++ scripts/play_update_expect_success_music.sh
@@ -1,4 +1,4 @@
 MONITOR=HDMI-2
-DURATION=0.3
+DURATION=1
 scripts/adjust_screen_brightness.sh 1.0 $MONITOR $DURATION
 mpg321 assets/music/elden-ring-melina-voice-we-re-almost-there.mp3 >/dev/null 2>&1
