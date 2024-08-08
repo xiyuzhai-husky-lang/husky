e794f680e16e92e45eee10efa50ade0571ad4681
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-08-07 20:02:41 -0700

    reduce wait time

--- crates/protocols/husky-trace-protocol/src/client/test_utils.rs
+++ crates/protocols/husky-trace-protocol/src/client/test_utils.rs
@@ -11,13 +11,14 @@ where
     fn test_trace_client(self) {
         let tokio_runtime = Arc::new(tokio::runtime::Runtime::new().unwrap());
         let server_thread = std::thread::spawn(|| self.serve_traces("localhost:51718"));
-        let hundred_millis = std::time::Duration::from_millis(100);
+        let wait_time = std::time::Duration::from_millis(10);
         // wait until the server is there for sure
-        std::thread::sleep(hundred_millis);
+        std::thread::sleep(wait_time);
         let mut client =
             TraceClient::<T::TraceProtocol, ()>::new(tokio_runtime, "ws://localhost:51718/ws", ());
-        std::thread::sleep(hundred_millis);
+        std::thread::sleep(wait_time);
         client.update(&mut None);
+        // std::thread::sleep(hundred_millis);
         // client.take_view_action(TraceViewAction::FollowTrace {
         //     trace_id: TraceId::from_index(0),
         // });
