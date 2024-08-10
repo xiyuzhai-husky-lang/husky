200fb63055123ede8ea3e55a5c7b42019e539816
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-08-10 14:37:08 -0700

    for static var, rename set_id to replace_id, making it unsafe, add with_id

--- crates/devsoul/husky-devsoul-interface/src/static_var.rs
+++ crates/devsoul/husky-devsoul-interface/src/static_var.rs
@@ -1,8 +1,24 @@
 use crate::*;
 
-pub trait IsStaticVar<StaticVarId> {
+pub trait IsStaticVar<StaticVarId>
+where
+    StaticVarId: std::fmt::Debug + Copy + Eq,
+{
     fn item_path_id_interface() -> ItemPathIdInterface;
     fn ids() -> impl Iterator<Item = StaticVarId>;
     fn get_id() -> StaticVarId;
-    fn set_id(id: StaticVarId);
+    /// use with_id instead
+    unsafe fn replace_id(id: StaticVarId) -> Option<StaticVarId>;
+
+    fn with_id<R>(id: StaticVarId, f: impl FnOnce() -> R) -> R {
+        unsafe {
+            let old = Self::replace_id(id);
+            let r = f();
+            if let Some(old) = old {
+                let new = Self::replace_id(old);
+                assert_eq!(new, Some(id));
+            }
+            r
+        }
+    }
 }
--- registry/ml-task-0.1.0/src/lib.rs
+++ registry/ml-task-0.1.0/src/lib.rs
@@ -1,7 +1,10 @@
 use ad_hoc_devsoul_dependency::{ugly::*, *};
 use husky_core::*;
 
-pub trait IsMlTask<StaticVarId> {
+pub trait IsMlTask<StaticVarId>
+where
+    StaticVarId: std::fmt::Debug + Copy + Eq,
+{
     type Input;
 
     type INPUT: __IsStaticVar<StaticVarId>;
