7b0576f50ac3f5c10eb464b70df2b1ca0f7d89d5
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-08-22 21:00:25 -0700

    save

--- crates/cybertron/husky-cybertron-mini-compiler/src/token/literal.rs
+++ crates/cybertron/husky-cybertron-mini-compiler/src/token/literal.rs
@@ -1,14 +1,17 @@
+use ordered_float::OrderedFloat;
 use std::fmt::Pointer;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub enum Literal {
     Int(i32),
+    Float(OrderedFloat<f32>),
 }
 
 impl std::fmt::Display for Literal {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Literal::Int(i) => i.fmt(f),
+            Literal::Float(_) => todo!(),
         }
     }
 }
@@ -17,6 +20,7 @@ impl std::fmt::Debug for Literal {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Literal::Int(i) => f.write_fmt(format_args!("`{i}`")),
+            Literal::Float(_) => todo!(),
         }
     }
 }
@@ -25,6 +29,7 @@ impl Literal {
     pub fn repr(self) -> String {
         match self {
             Literal::Int(i) => i.to_string(),
+            Literal::Float(_) => todo!(),
         }
     }
 }
