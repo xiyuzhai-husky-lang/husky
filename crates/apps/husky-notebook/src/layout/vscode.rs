d1e136c4f5b4c1e5381de1adc875adb6beef5e25
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-08-24 13:27:19 -0700

    add log view

--- crates/apps/husky-notebook/src/layout/vscode.rs
+++ crates/apps/husky-notebook/src/layout/vscode.rs
@@ -22,9 +22,17 @@ impl NotebookApp {
             .show(ctx, |ui| self.render_activity_view(ctx, ui));
         egui::SidePanel::right("figure_view")
             .resizable(false)
-            .show(ctx, |ui| self.render_assist_view(ctx, ui));
+            .show(ctx, |ui| self.render_middle_right_ground(ctx, ui));
         egui::CentralPanel::default()
             .frame(self.settings.main_panel_frame())
             .show(ctx, |ui| self.render_docs_view(ui));
+    }
+
+    fn render_middle_right_ground(&mut self, ctx: &egui::Context, ui: &mut egui::Ui) {
+        egui::TopBottomPanel::bottom("log_view")
+            .resizable(false)
+            .frame(self.settings.log_view_frame())
+            .show_inside(ui, |ui| self.render_log_view(ctx, ui));
+        self.render_assist_view(ctx, ui)
     }
 }
--- crates/apps/husky-notebook/src/settings.rs
+++ crates/apps/husky-notebook/src/settings.rs
@@ -21,6 +21,10 @@ impl NotebookSettings {
         egui::Frame::none()
     }
 
+    pub(crate) fn log_view_frame(&self) -> egui::Frame {
+        egui::Frame::none()
+    }
+
     pub(crate) fn main_panel_frame(&self) -> egui::containers::Frame {
         egui::containers::Frame::default()
             .inner_margin(0.0)
