c1cb6f3df435092f525e6acbc108caf84d20fb2d
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-08-25 02:07:07 -0700

    update deps; use forked egui

--- crates/utils/husky-cargo-utils/src/metadata.rs
+++ crates/utils/husky-cargo-utils/src/metadata.rs
@@ -3,7 +3,7 @@ use std::path::Path;
 
 pub fn workspace_package_summaries(workspace_dir: &Path) -> Vec<Summary> {
     let manifest_path = &workspace_dir.join("Cargo.toml");
-    let config = cargo::Config::default().expect("what the hell");
-    let ws = Workspace::new(manifest_path, &config).expect("what the hell");
+    let gctx = cargo::GlobalContext::default().expect("what the hell");
+    let ws = Workspace::new(manifest_path, &gctx).expect("what the hell");
     ws.members().map(|pkg| pkg.summary().clone()).collect()
 }
