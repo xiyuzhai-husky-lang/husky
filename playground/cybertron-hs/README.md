58381709383ee2b77ea2f9124e4a3f8756f794d8
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-08-25 11:07:30 -0700

    clean up

--- playground/cybertron-hs/README.md
+++
@@ -1,1 +0,0 @@
-# cybertron-hs
--- playground/namekian-lean/NamekianLean/State.lean
+++
@@ -1,7 +0,0 @@
-import NamekianLean.Registry
--- import NamekianLean.Registry
-
-structure LogicState where
-  registry:  LogicRegistry
-  intros: List LogicIntro
-  tree: LogicTree
