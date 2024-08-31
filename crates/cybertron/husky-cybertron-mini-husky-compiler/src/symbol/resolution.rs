d1d6db766cd021039de9c1a245e79baa9ac2d2a8
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-08-31 16:32:52 -0700

    cybertron

---
+++ crates/cybertron/husky-cybertron-mini-husky-compiler/src/symbol/resolution.rs
@@ -0,0 +1,60 @@
+use super::defn::calc_symbol_defns;
+use super::*;
+
+pub enum SymbolResolution {}
+
+fn calc_symbol_resolution(asts: Seq<Option<Ast>>) -> Seq<Option<Symbol>> {
+    let symbol_defns = calc_symbol_defns(asts);
+    let idents = asts.map(|ast| match ast?.data {
+        AstData::Ident(ident) => Some(ident),
+        _ => None,
+    });
+    symbol_defns
+        .map(|symbol_defn| Some(symbol_defn?.symbol))
+        .first_filtered_by_attention(idents, symbol_defns, |ident, symbol_defn| {
+            let Some(ident) = ident else { return false };
+            let Some(symbol_defn) = symbol_defn else {
+                return false;
+            };
+            // todo: consider scope
+            symbol_defn.symbol.ident == ident
+        })
+        .map(|s| s.flatten())
+}
+
+#[test]
+fn calc_symbol_resoluion_works() {
+    fn t(input: &str, expect: Expect) {
+        let asts = calc_asts_from_input(input, 10);
+        let symbol_resolutions = calc_symbol_resolution(asts);
+        expect.assert_debug_eq(&symbol_resolutions)
+    }
+    t(
+        "1",
+        expect![[r#"
+        [None]
+    "#]],
+    );
+    t(
+        "fn f() {} fn g() { f() }",
+        expect![[r#"
+            [
+                None,
+                Some(Symbol { ident: `f`, source: #1, data: Item { kind: Fn } }),
+                None,
+                None,
+                None,
+                None,
+                None,
+                Some(Symbol { ident: `g`, source: #7, data: Item { kind: Fn } }),
+                None,
+                None,
+                None,
+                Some(Symbol { ident: `f`, source: #1, data: Item { kind: Fn } }),
+                None,
+                None,
+                None,
+            ]
+        "#]],
+    );
+}
