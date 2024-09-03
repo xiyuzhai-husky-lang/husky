8d743c3445eda116b3ff77802bbb65f0556c54e9
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-09-02 20:35:23 -0700

    refactor roles

--- crates/cybertron/husky-cybertron-mini-husky-compiler/src/ty.rs
+++ crates/cybertron/husky-cybertron-mini-husky-compiler/src/ty.rs
@@ -1,8 +1,10 @@
-use crate::token::ident::Ident;
-
 pub mod checking;
+pub mod expectation;
 pub mod signature;
 
+use crate::token::ident::Ident;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum Type {
     Ident(Ident),
     Option(Ident),
