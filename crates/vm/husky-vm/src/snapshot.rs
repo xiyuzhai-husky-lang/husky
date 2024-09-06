09eba59f20cd9cb76e144d6e9474e05b05591a9e
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-09-06 13:38:53 -0700

    add snapshots with mutex to history

--- crates/vm/husky-vm/src/snapshot.rs
+++ crates/vm/husky-vm/src/snapshot.rs
@@ -1,5 +1,7 @@
 use husky_linket::linket::Linket;
 use husky_linket_impl::linket_impl::{IsLinketImpl, LinketImplFrozenValue};
+use husky_vmir::stmt::VmirStmtIdx;
+use vec_like::{ordered_vec_map::OrderedVecPairMap, SmallVecPairMap};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct VmSnapshotKey {}
@@ -16,5 +18,21 @@ impl<LinketImpl: IsLinketImpl> VmSnapshot<LinketImpl> {
 
     pub fn place_frozen_values(&self) -> &[LinketImplFrozenValue<LinketImpl>] {
         &self.place_frozen_values
+    }
+}
+
+pub(crate) type VmSnapshotsData<LinketImpl: IsLinketImpl> = SmallVecPairMap<
+    VmirStmtIdx<LinketImpl>,
+    OrderedVecPairMap<VmSnapshotKey, VmSnapshot<LinketImpl>>,
+    4,
+>;
+
+pub struct VmSnapshots<LinketImpl: IsLinketImpl> {
+    data: VmSnapshotsData<LinketImpl>,
+}
+
+impl<LinketImpl: IsLinketImpl> VmSnapshots<LinketImpl> {
+    pub fn new(data: VmSnapshotsData<LinketImpl>) -> Self {
+        Self { data }
     }
 }
