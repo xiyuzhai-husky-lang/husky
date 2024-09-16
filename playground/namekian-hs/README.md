b5d61023c204e53e199abd208cbcf0b6344b1979
xiyu <dirac12345@gmail.com>
2024-09-16 03:53:35 -0700

    clear up

--- playground/namekian-hs/README.md
+++
@@ -1,1 +0,0 @@
-# logic-monad-hs
--- playground/namekian-hs/src/Registry.hs
+++
@@ -1,7 +0,0 @@
-module Registry (LogicRegistry, defaultRegistry) where
-
-newtype LogicRegistry = LogicRegistry Int
-    deriving (Show)
-
-defaultRegistry::LogicRegistry
-defaultRegistry = LogicRegistry 0
