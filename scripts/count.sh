1aa8d4f42a5ea4b86e438026197e8afe6e30af26
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-09-18 16:02:54 -0700

    improve count

--- scripts/count.sh
+++ scripts/count.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git pull
 # cloc crates --exclude-dir=node_modules,out,target,dist,forks,package-lock.json,package.json,target-rs --exclude-ext=html,css,json,toml,md
-scc ./ --exclude-dir=expect-files,tests,target-rs,__rust_gen__,__rust_gen_cache__,node_modules,out,target,dist,forks,package-lock.json,package.json,target-rs
+scc ./ --exclude-dir=expect-files,tests,target-rs,node_modules,out,target,dist,package-lock.json,package.json,target-rs
