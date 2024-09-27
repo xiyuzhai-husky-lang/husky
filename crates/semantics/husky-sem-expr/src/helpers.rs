029c960a99d99d83e134cc62a291b01559775a8f
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-09-27 02:15:08 -0700

    rust macro sucks

--- crates/semantics/husky-sem-expr/src/helpers.rs
+++ crates/semantics/husky-sem-expr/src/helpers.rs
@@ -1,8 +1,8 @@
 pub mod aggregator;
 pub mod analysis;
 pub mod codespan;
+pub mod path;
 pub mod range;
-pub mod region;
 pub mod visitor;
 
 use crate::*;
--- crates/syntax/husky-syn-expr/src/helpers.rs
+++ crates/syntax/husky-syn-expr/src/helpers.rs
@@ -2,3 +2,4 @@ pub mod block_expr;
 pub mod codespan;
 
 use crate::*;
+use husky_entity_tree::region_path::SynNodeRegionPath;
