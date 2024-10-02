86c2cd35bab27f2abbb2e7eb997643e612f8a723
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-10-02 05:03:37 -0700

    towards removing devsoul interface

--- crates/linket/husky-linket-impl/src/ugly.rs
+++ crates/linket/husky-linket-impl/src/ugly.rs
@@ -2,3 +2,4 @@ pub use crate::{
     exception::ExceptionSource as __ExceptionSource, linket_impl::IsLinketImpl as __IsLinketImpl,
     pedestal::IsPedestal as __IsPedestal, static_var::IsStaticVar as __IsStaticVar,
 };
+pub use smallvec::SmallVec as __SmallVec;
