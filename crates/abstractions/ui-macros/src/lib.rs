1c3a3471e8c3fcce55c2bb51499a4b70f72a67fb
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-10-03 19:30:14 -0700

    refactor settings ui traits

--- crates/abstractions/ui-macros/src/lib.rs
+++ crates/abstractions/ui-macros/src/lib.rs
@@ -1,15 +1,10 @@
+mod settings;
+
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(SettingSectionUi)]
 pub fn derive_setting_section_ui(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
-    let name = &input.ident;
-
-    let expanded = quote! {
-        impl SettingSectionUi for #name {}
-    };
-
-    TokenStream::from(expanded)
+    settings::section::derive_setting_section_ui(input)
 }
--- crates/abstractions/ui/src/settings.rs
+++ crates/abstractions/ui/src/settings.rs
@@ -1,7 +1,30 @@
-pub trait SettingsUi {}
+use crate::ui::IsUi;
 
-pub trait SettingSectionUi {}
+pub trait SettingsUi<Ui: IsUi> {}
 
-pub trait SettingSubsectionUi {}
+pub trait SettingSectionUi<Ui: IsUi> {
+    fn for_each_subsection(&mut self, f: &mut dyn FnMut(&str, &mut dyn SettingSubsectionUi<Ui>));
+}
 
-pub trait SettingUi {}
+/// trivial implementation
+impl<Ui: IsUi> SettingSectionUi<Ui> for () {
+    fn for_each_subsection(&mut self, f: &mut dyn FnMut(&str, &mut dyn SettingSubsectionUi<Ui>)) {}
+}
+
+pub trait SettingSubsectionUi<Ui: IsUi> {
+    fn for_each_item(&mut self, f: &mut dyn FnMut(&str, &mut dyn SettingItemUi<Ui>));
+}
+
+/// trivial implementation
+impl<Ui: IsUi> SettingSubsectionUi<Ui> for () {
+    fn for_each_item(&mut self, f: &mut dyn FnMut(&str, &mut dyn SettingItemUi<Ui>)) {}
+}
+
+pub trait SettingItemUi<Ui: IsUi> {
+    fn setting_item_ui(&mut self, ui: &mut Ui);
+}
+
+/// trivial implementation
+impl<Ui: IsUi> SettingItemUi<Ui> for () {
+    fn setting_item_ui(&mut self, _ui: &mut Ui) {}
+}
