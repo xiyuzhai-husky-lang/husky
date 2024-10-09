75bbdab1f4be83c02fe1164f2b2a5c96b2904d3a
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-10-09 08:47:54 -0700

    output types for sha

--- crates/utils/husky-sha-utils/src/lib.rs
+++ crates/utils/husky-sha-utils/src/lib.rs
@@ -3,24 +3,42 @@ use sealed::sealed;
 use sha2::{Digest, Sha256, Sha512};
 use std::hash::Hasher;
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub struct Sha256Output(crypto_common::Output<Sha256>);
+
+impl Sha256Output {
+    pub fn hex(&self) -> String {
+        hex::encode(&self.0)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub struct Sha512Output(crypto_common::Output<Sha512>);
+
+impl Sha512Output {
+    pub fn hex(&self) -> String {
+        hex::encode(&self.0)
+    }
+}
+
 #[sealed]
 pub trait ShaHash {
-    fn sha256(&self) -> String;
-    fn sha512(&self) -> String;
+    fn sha256(&self) -> Sha256Output;
+    fn sha512(&self) -> Sha512Output;
 }
 
 #[sealed]
 impl<T: std::hash::Hash> ShaHash for T {
-    fn sha256(&self) -> String {
+    fn sha256(&self) -> Sha256Output {
         let mut hasher: Sha256Hasher = Sha256Hasher::default();
         self.hash(&mut hasher);
-        hex::encode(hasher.0.finalize())
+        Sha256Output(hasher.0.finalize())
     }
 
-    fn sha512(&self) -> String {
+    fn sha512(&self) -> Sha512Output {
         let mut hasher: Sha512Hasher = Sha512Hasher::default();
         self.hash(&mut hasher);
-        hex::encode(hasher.0.finalize())
+        Sha512Output(hasher.0.finalize())
     }
 }
 
@@ -45,11 +63,11 @@ impl Hasher for Sha256Hasher {
 fn test_sha256() {
     let input = b"hello world";
     let expected = "9fbc31964e86c79e0e3fffbcf8afafcedfbdd2a9e189cf93bdc608df5bcaa0a8";
-    assert_eq!(input.sha256(), expected);
+    assert_eq!(input.sha256().hex(), expected);
 
     let input_vec = vec![1, 2, 3, 4, 5];
     let expected_vec = "268f9f97feb4c3a10c18abadc02f6db42d6c4cc1875f532bd293c749053274e2";
-    assert_eq!(input_vec.sha256(), expected_vec);
+    assert_eq!(input_vec.sha256().hex(), expected_vec);
 }
 
 #[derive(Default)]
@@ -73,11 +91,11 @@ impl Hasher for Sha512Hasher {
 fn test_sha512() {
     let input = b"hello world";
     let expected = "3c87bc935c66a8a5baa40bdbc8ae311bfdc659e3931353b1b0bd5419484a4c86161839ed8f0f3c299bf169211b68c7e8ddd8dee6aad79e85855ae9ada93c743a";
-    assert_eq!(input.sha512(), expected);
+    assert_eq!(input.sha512().hex(), expected);
 
     let input_vec = vec![1, 2, 3, 4, 5];
     let expected_vec = "553177761401b5cfb10fb6a6444a7f9d3efd32f07892d3e2f8ea5d734b1e1612249a0d0a95062e0dc17bb89b8768bbd0f0f6d5247f56071d2770532bab8cbccc";
-    assert_eq!(input_vec.sha512(), expected_vec);
+    assert_eq!(input_vec.sha512().hex(), expected_vec);
 }
 
 #[test]
