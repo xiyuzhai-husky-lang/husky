3fec0a1afe3dbca992c315ad2788f5e3297a3328
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-10-09 12:09:09 -0700

    mayuri jobs

--- crates/mayuri/mayuri-prototype/src/config.rs
+++ crates/mayuri/mayuri-prototype/src/config.rs
@@ -1,56 +1,5 @@
+pub mod mayuri;
+pub mod nemu;
+
 use serde::Deserialize;
 use std::path::PathBuf;
-
-#[derive(Debug, Deserialize, PartialEq, Eq)]
-pub struct MayuriConfig {}
-
-#[derive(Debug, Deserialize, PartialEq, Eq)]
-pub struct SrcConfig {
-    pub path: PathBuf,
-}
-
-#[derive(Debug, Deserialize, PartialEq, Eq)]
-pub struct NemuConfig {
-    src: Vec<NemuSrcFragment>,
-}
-
-#[derive(Debug, Deserialize, PartialEq, Eq)]
-pub struct NemuSrcFragment {
-    path: String,
-}
-
-#[cfg(test)]
-use toml;
-
-#[test]
-fn mayuri_config_works() {
-    let toml_str = r#"
-    "#;
-
-    assert_eq!(MayuriConfig {}, toml::from_str(toml_str).unwrap());
-}
-
-#[test]
-fn nemu_config_works() {
-    let toml_str = r#"
-        [[src]]
-        path = "main.py"
-        [[src]]
-        path = "eval.py"
-    "#;
-
-    let config: NemuConfig = toml::from_str(toml_str).unwrap();
-
-    let expected_config = NemuConfig {
-        src: vec![
-            NemuSrcFragment {
-                path: "main.py".to_string(),
-            },
-            NemuSrcFragment {
-                path: "eval.py".to_string(),
-            },
-        ],
-    };
-
-    assert_eq!(config, expected_config);
-}
--- crates/mayuri/mayuri-prototype/src/lib.rs
+++ crates/mayuri/mayuri-prototype/src/lib.rs
@@ -1,4 +1,6 @@
 pub mod config;
 pub mod experiment;
 pub mod fs;
+pub mod job;
 pub mod src;
+pub mod test;
