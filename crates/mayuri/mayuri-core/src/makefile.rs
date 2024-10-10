f519916569a9e5306a5778f87d475965b3921965
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-10-10 01:20:53 -0700

    mayuri makefile extracted

---
+++ crates/mayuri/mayuri-core/src/makefile.rs
@@ -0,0 +1,19 @@
+use crate::*;
+use husky_make_utils::extract::extract_makefile_rules;
+use std::path::Path;
+
+#[derive(Debug, Serialize)]
+pub struct MayuriMakefileExtracted {
+    content: String,
+}
+
+impl MayuriMakefileExtracted {
+    pub fn new(
+        root: impl AsRef<Path>,
+        rules: impl IntoIterator<Item = String>,
+    ) -> Result<Self, Box<dyn std::error::Error>> {
+        let makefile_path = root.as_ref().join("makefile");
+        let content = extract_makefile_rules(makefile_path, rules)?;
+        Ok(Self { content })
+    }
+}
