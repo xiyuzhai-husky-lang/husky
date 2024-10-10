9a66d51d5c64b77deb8ecc8925ad7686ecf4743d
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-10-10 12:50:17 -0700

    better

--- scripts/update_expect_quick.sh
+++ scripts/update_expect_quick.sh
@@ -36,13 +36,10 @@ OVERALL_SUCCESS=true
 for TARGET in $MAKE_QUICK_TARGETS; do
   cargo build -p $TARGET --tests
   if ! UPDATE_EXPECT=1 cargo test -p "$TARGET" -j 1 -- --nocapture; then
-    OVERALL_SUCCESS=false
+    scripts/play_update_expect_failure_music.sh
+    exit 1  # Exit the script with an error code
   fi
 done
 
-# Play success or failure music based on the overall result
-if $OVERALL_SUCCESS; then
-  scripts/play_update_expect_success_music.sh
-else
-  scripts/play_update_expect_failure_music.sh
-fi
+# If we've made it here, all tests have passed
+scripts/play_update_expect_success_music.sh
