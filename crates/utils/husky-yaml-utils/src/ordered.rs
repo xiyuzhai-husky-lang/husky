77fef0cc624583ab5fe5bcb8f673eff861075239
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-10-11 16:35:52 -0700

    yml is too complicated

--- crates/utils/husky-yaml-utils/src/ordered.rs
+++ crates/utils/husky-yaml-utils/src/ordered.rs
@@ -4,23 +4,21 @@ use vec_like::ordered_vec_map::OrderedVecPairMap;
 use yaml_rust2::yaml::Hash;
 use yaml_rust2::Yaml;
 
-use crate::any::AnyYamlVisitor;
-
 /// `OrderedYaml` is a wrapper around `yaml_rust2::Yaml` that orders key-value pairs in YAML maps alphabetically by keys.
 #[derive(PartialEq, Eq, Clone, PartialOrd, Ord, Hash)]
 pub struct OrderedYaml {
     inner: Yaml,
 }
 
-impl<'de> Deserialize<'de> for OrderedYaml {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        let yaml = deserializer.deserialize_any(AnyYamlVisitor)?;
-        Ok(OrderedYaml::new(yaml.to_inner()))
-    }
-}
+// impl<'de> Deserialize<'de> for OrderedYaml {
+//     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+//     where
+//         D: serde::Deserializer<'de>,
+//     {
+//         let yaml = deserializer.deserialize_any(AnyYamlVisitor)?;
+//         Ok(OrderedYaml::new(yaml.to_inner()))
+//     }
+// }
 
 impl OrderedYaml {
     pub fn new(mut yaml: Yaml) -> Self {
@@ -58,29 +56,5 @@ impl fmt::Debug for OrderedYaml {
         write!(f, "OrderedYaml(\"",)?;
         yaml_rust2::YamlEmitter::new(f).dump(&self.inner).unwrap();
         write!(f, "\")",)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_ordered_yaml_deserialization() {
-        let yaml_str = r#"
-        c: 3
-        a: 1
-        b: 2
-        "#;
-
-        let ordered: OrderedYaml = serde_yml::from_str(yaml_str).unwrap();
-        // yaml_rust2::YamlLoader::load_from_str(yaml_str).unwrap();
-
-        if let Yaml::Hash(hash) = &ordered.inner {
-            let keys: Vec<_> = hash.keys().map(|k| k.as_str().unwrap()).collect();
-            assert_eq!(keys, vec!["a", "b", "c"]);
-        } else {
-            panic!("Expected a Hash");
-        }
     }
 }
