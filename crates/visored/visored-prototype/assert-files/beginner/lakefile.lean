92ea80120955bfb395f84f4b29956b20e5285d40
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-10-16 15:11:13 -0700

    rename

--- crates/visored/visored-prototype/assert-files/beginner/lakefile.lean
+++ crates/visored/visored-prototype/assert-files/beginner/lakefile.lean
@@ -12,5 +12,5 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
-lean_lib «MyProject» where
+lean_lib «Beginner» where
   -- add any library configuration options here
