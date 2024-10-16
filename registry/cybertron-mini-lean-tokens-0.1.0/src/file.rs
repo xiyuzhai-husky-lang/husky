2bc1fd3f42f4c0c0beb1d22c63dd0b4a17c860f8
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-10-15 19:34:52 -0700

    save

--- registry/cybertron-mini-lean-tokens-0.1.0/src/file.rs
+++ registry/cybertron-mini-lean-tokens-0.1.0/src/file.rs
@@ -1,8 +1,16 @@
 use shifted_unsigned_int::ShiftedU32;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct FileId(ShiftedU32);
+
 impl FileId {
     pub(crate) fn new(file: u32) -> Self {
         Self(file.into())
+    }
+}
+
+impl Into<u32> for FileId {
+    fn into(self) -> u32 {
+        self.0.into()
     }
 }
--- registry/cybertron-mini-lean-tokens-0.1.0/src/position.rs
+++ registry/cybertron-mini-lean-tokens-0.1.0/src/position.rs
@@ -1,8 +1,16 @@
 use shifted_unsigned_int::ShiftedU32;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct PositionIdx(ShiftedU32);
+
 impl PositionIdx {
     pub(crate) fn new(position: u32) -> Self {
         Self(todo!())
+    }
+}
+
+impl Into<u32> for PositionIdx {
+    fn into(self) -> u32 {
+        self.0.into()
     }
 }
