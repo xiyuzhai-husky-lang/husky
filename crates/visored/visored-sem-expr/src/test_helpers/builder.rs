7915cd2ca7f4c7ffea2ef58348e82391b8c6ccf2
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-10-20 16:30:44 -0700

    save

--- crates/visored/visored-sem-expr/src/test_helpers/builder.rs
+++ crates/visored/visored-sem-expr/src/test_helpers/builder.rs
@@ -1,12 +1,24 @@
 use crate::{
-    clause::VdSemClauseArena, expr::VdSemExprArena, phrase::VdSemPhraseArena,
-    sentence::VdSemSentenceArena,
+    clause::VdSemClauseArena, expr::VdSemExprArena, helpers::latex_fmt::VdSemExprLaTeXFormatter,
+    phrase::VdSemPhraseArena, sentence::VdSemSentenceArena,
 };
 
 pub struct VdSemExprTestBuilder<'db> {
     db: &'db ::salsa::Db,
-    arena: VdSemExprArena,
+    expr_arena: VdSemExprArena,
     phrase_arena: VdSemPhraseArena,
     clause_arena: VdSemClauseArena,
     sentence_arena: VdSemSentenceArena,
+}
+
+impl<'db> VdSemExprTestBuilder<'db> {
+    pub fn latex_formatter<'a>(&'a self) -> VdSemExprLaTeXFormatter<'a> {
+        VdSemExprLaTeXFormatter::new(
+            self.db,
+            self.expr_arena.as_arena_ref(),
+            self.phrase_arena.as_arena_ref(),
+            self.clause_arena.as_arena_ref(),
+            self.sentence_arena.as_arena_ref(),
+        )
+    }
 }
