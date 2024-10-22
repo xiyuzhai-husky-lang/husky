884fa3f533b147f77b753247b1090ab80cce436e
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-10-21 21:04:12 -0700

    save

--- crates/visored/visored-hir-expr/src/builder.rs
+++ crates/visored/visored-hir-expr/src/builder.rs
@@ -1,3 +1,29 @@
+use crate::{expr::VdHirExprArena, stmt::VdHirStmtArena};
+use visored_sem_expr::{
+    clause::VdSemClauseArenaRef, expr::VdSemExprArenaRef, phrase::VdSemPhraseArenaRef,
+    region::VdSemExprRegionData, sentence::VdSemSentenceArenaRef,
+};
+
 pub struct VdHirExprBuilder<'db> {
     db: &'db ::salsa::Db,
+    vd_sem_expr_arena: VdSemExprArenaRef<'db>,
+    vd_sem_phrase_arena: VdSemPhraseArenaRef<'db>,
+    vd_sem_clause_arena: VdSemClauseArenaRef<'db>,
+    vd_sem_sentence_arena: VdSemSentenceArenaRef<'db>,
+    expr_arena: VdHirExprArena,
+    stmt_arena: VdHirStmtArena,
+}
+
+impl<'db> VdHirExprBuilder<'db> {
+    pub fn new(db: &'db ::salsa::Db, vd_sem_expr_region_data: &'db VdSemExprRegionData) -> Self {
+        Self {
+            db,
+            vd_sem_expr_arena: vd_sem_expr_region_data.expr_arena(),
+            vd_sem_phrase_arena: vd_sem_expr_region_data.phrase_arena(),
+            vd_sem_clause_arena: vd_sem_expr_region_data.clause_arena(),
+            vd_sem_sentence_arena: vd_sem_expr_region_data.sentence_arena(),
+            expr_arena: VdHirExprArena::default(),
+            stmt_arena: VdHirStmtArena::default(),
+        }
+    }
 }
