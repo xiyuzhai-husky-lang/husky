0016f012b5df486b6d6a57f949b81ed79d9a7a58
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-10-23 02:14:24 -0700

    remove unnecessary

--- crates/latex/latex-token/src/data.rs
+++ crates/latex/latex-token/src/data.rs
@@ -1,9 +1,7 @@
-pub mod code;
 pub mod math;
 pub mod rose;
-pub mod tree;
 
-use self::{code::LxCodeTokenData, math::LxMathTokenData, rose::LxRoseTokenData};
+use self::{math::LxMathTokenData, rose::LxRoseTokenData};
 use crate::lexer::LxLexer;
 #[cfg(test)]
 use crate::*;
@@ -13,7 +11,6 @@ use latex_prelude::mode::LxMode;
 #[enum_class::from_variants]
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum LxTokenData {
-    Code(LxCodeTokenData),
     Math(LxMathTokenData),
     Rose(LxRoseTokenData),
 }
@@ -21,7 +18,6 @@ pub enum LxTokenData {
 impl<'a> LxLexer<'a> {
     pub(crate) fn next_token_data(&mut self) -> Option<LxTokenData> {
         match self.mode {
-            LxMode::Code => todo!(),
             LxMode::Rose => self.next_text_token_data().map(Into::into),
             LxMode::Math => self.next_math_token_data().map(Into::into),
         }
