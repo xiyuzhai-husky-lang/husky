682ee84f1c84ffccd7fa1c5d87b10be09785b0b0
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-10-24 11:13:26 -0700

    save

--- crates/latex/latex-annotation/src/annotation/space.rs
+++ crates/latex/latex-annotation/src/annotation/space.rs
@@ -1,4 +1,20 @@
-#[derive(Debug, PartialEq, Eq)]
+#[enum_class::from_variants]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum LxSpaceAnnotation {
     Void,
+    Apply(LxApplyAnnotation),
+    Sever(LxSeverAnnotation),
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum LxApplyAnnotation {
+    ScalarMul,
+    ScalarDifferentialFormMul,
+    DifferentialOperatorFunctionMul,
+}
+
+#[enum_class::from_variants]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum LxSeverAnnotation {
+    Index,
 }
