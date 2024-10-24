5e2befe054e02d8403e9ef0b920b939eaea0e580
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-10-23 23:13:24 -0700

    save

--- crates/latex/latex-annotation/src/annotation/token.rs
+++ crates/latex/latex-annotation/src/annotation/token.rs
@@ -1,4 +1,12 @@
 #[derive(Debug, PartialEq, Eq)]
 pub enum LxTokenAnnotation {
     Void,
+    Integral(LxIntegralAnnotation),
+    Variable(LxVariableAnnotation),
 }
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum LxIntegralAnnotation {}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum LxVariableAnnotation {}
