69de780a652f8875952cc876f90f04dcb95a9bec
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-10-26 11:50:00 -0700

    fix annotations setup test data; new pc alienware aurora-r16

--- scripts/play_update_expect_failure_music.sh
+++ scripts/play_update_expect_failure_music.sh
@@ -1,4 +1,4 @@
-MONITOR=HDMI-2
+MONITOR=HDMI-0
 DURATION=1
 # mpg321 assets/music/elden-ring-you-died-sound-effect.mp3 --frames 8 >/dev/null 2>&1 &
 spd-say "failed"
--- scripts/update_expect.sh
+++ scripts/update_expect.sh
@@ -4,13 +4,13 @@ set -e
 
 # Duration over which to change the brightness, in seconds
 DURATION=0.5
-MONITOR=HDMI-2
+MONITOR=HDMI-0
 
 # Define a cleanup function
 cleanup() {
   scripts/clean_rich_test_locks.py
   # Reset screen brightness
-  scripts/adjust_screen_brightness.sh 1.0 $MONITOR $DURATION &
+  # scripts/adjust_screen_brightness.sh 1.0 $MONITOR $DURATION &
 }
 
 # Trap the EXIT signal to call the cleanup function
@@ -18,7 +18,7 @@ cleanup() {
 trap cleanup EXIT
 
 # Set the screen brightness to very low
-scripts/adjust_screen_brightness.sh 0.0 $MONITOR $DURATION &
+# scripts/adjust_screen_brightness.sh 0.1 $MONITOR $DURATION &
 
 # Format the Rust project
 cargo fmt
