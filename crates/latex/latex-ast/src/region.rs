e7d6284487f9f411830f2c6e8d870685e9941192
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-10-29 17:31:00 -0700

    save

--- crates/latex/latex-ast/src/region.rs
+++ crates/latex/latex-ast/src/region.rs
@@ -1,2 +1,20 @@
+use crate::ast::LxAstArena;
+
 #[salsa::tracked]
 pub struct LxAstRegion {}
+
+pub struct LxAstRegionData {
+    arena: LxAstArena,
+}
+
+impl LxAstRegionData {
+    pub(crate) fn new(arena: LxAstArena) -> Self {
+        Self { arena }
+    }
+}
+
+impl LxAstRegionData {
+    pub fn arena(&self) -> &LxAstArena {
+        &self.arena
+    }
+}
