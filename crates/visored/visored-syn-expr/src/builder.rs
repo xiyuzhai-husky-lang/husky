6d662eee263ed05186e0ab8948e4f448d0a47b41
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-10-30 21:42:28 -0700

    more

--- crates/visored/visored-syn-expr/src/builder.rs
+++ crates/visored/visored-syn-expr/src/builder.rs
@@ -1,1 +1,26 @@
+use crate::{
+    clause::VdSynClauseArena,
+    expr::{VdSynExprArena, VdSynExprData, VdSynExprIdx},
+    phrase::{VdSynPhraseArena, VdSynPhraseData, VdSynPhraseIdx},
+    region::VdSynExprRegionData,
+    sentence::{VdSynSentenceArena, VdSynSentenceData, VdSynSentenceIdx},
+};
 
+pub(crate) struct VdSemExprBuilder<'db> {
+    db: &'db ::salsa::Db,
+    expr_arena: VdSynExprArena,
+    phrase_arena: VdSynPhraseArena,
+    clause_arena: VdSynClauseArena,
+    sentence_arena: VdSynSentenceArena,
+}
+
+impl<'db> VdSemExprBuilder<'db> {
+    pub fn finish(self) -> VdSynExprRegionData {
+        VdSynExprRegionData::new(
+            self.expr_arena,
+            self.phrase_arena,
+            self.clause_arena,
+            self.sentence_arena,
+        )
+    }
+}
