5b108777343b28c73ac1c7b7f298fd48118517bd
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-10-31 11:22:08 -0700

    save

--- crates/visored/visored-syn-expr/src/tests.rs
+++ crates/visored/visored-syn-expr/src/tests.rs
@@ -1,2 +1,8 @@
-#[salsa::db(husky_coword::jar::CowordJar, visored_zfs_ty::jar::VdZfsTypeJar)]
+#[salsa::db(
+    husky_coword::jar::CowordJar,
+    visored_zfs_ty::jar::VdZfsTypeJar,
+    latex_ast::jar::LxAstJar,
+    visored_opr::jar::VdOprJar,
+    crate::jar::VdSynExprJar
+)]
 pub(crate) struct DB {}
