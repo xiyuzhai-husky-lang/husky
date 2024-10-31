e278e69ae41bc1b6f528cc75e541c41b002bf971
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-10-31 12:49:51 -0700

    save

--- crates/visored/visored-syn-expr/src/clause.rs
+++ crates/visored/visored-syn-expr/src/clause.rs
@@ -1,5 +1,7 @@
 use super::*;
-use idx_arena::{Arena, ArenaIdx, ArenaIdxRange, ArenaRef};
+use idx_arena::{
+    map::ArenaMap, ordered_map::ArenaOrderedMap, Arena, ArenaIdx, ArenaIdxRange, ArenaRef,
+};
 
 pub enum VdSynClauseData {
     Verb,
@@ -9,3 +11,5 @@ pub type VdSynClauseArena = Arena<VdSynClauseData>;
 pub type VdSynClauseArenaRef<'a> = ArenaRef<'a, VdSynClauseData>;
 pub type VdSynClauseIdx = ArenaIdx<VdSynClauseData>;
 pub type VdSynClauseIdxRange = ArenaIdxRange<VdSynClauseData>;
+pub type VdSynClauseMap<T> = ArenaMap<VdSynClauseData, T>;
+pub type VdSynClauseOrderedMap<T> = ArenaOrderedMap<VdSynClauseData, T>;
--- crates/visored/visored-syn-expr/src/sentence.rs
+++ crates/visored/visored-syn-expr/src/sentence.rs
@@ -1,5 +1,7 @@
 use crate::clause::VdSynClauseIdxRange;
-use idx_arena::{Arena, ArenaIdx, ArenaIdxRange, ArenaRef};
+use idx_arena::{
+    map::ArenaMap, ordered_map::ArenaOrderedMap, Arena, ArenaIdx, ArenaIdxRange, ArenaRef,
+};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub enum VdSynSentenceData {
@@ -10,3 +12,5 @@ pub type VdSynSentenceArena = Arena<VdSynSentenceData>;
 pub type VdSynSentenceArenaRef<'a> = ArenaRef<'a, VdSynSentenceData>;
 pub type VdSynSentenceIdx = ArenaIdx<VdSynSentenceData>;
 pub type VdSynSentenceIdxRange = ArenaIdxRange<VdSynSentenceData>;
+pub type VdSynSentenceMap<T> = ArenaMap<VdSynSentenceData, T>;
+pub type VdSynSentenceOrderedMap<T> = ArenaOrderedMap<VdSynSentenceData, T>;
