90d7b45ab06193ee6e340d9ec93bd3e3600efddc
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-10-31 17:18:50 -0700

    add token storage as an argument to VdAnnotations

--- crates/latex/latex-ast/src/tests.rs
+++ crates/latex/latex-ast/src/tests.rs
@@ -3,5 +3,5 @@ pub(crate) use salsa::DebugWithDb;
 
 use husky_coword::jar::CowordJar;
 
-#[salsa::db(CowordJar)]
+#[salsa::db(CowordJar, crate::jar::LxAstJar, latex_command::jar::LxCommandJar)]
 pub struct DB {}
