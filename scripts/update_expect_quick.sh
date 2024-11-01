0f6f9c10c2ed762e16be1c45a216386a253c4e94
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-11-01 03:50:57 -0700

    save

--- scripts/update_expect_quick.sh
+++ scripts/update_expect_quick.sh
@@ -29,9 +29,6 @@ UPDATE_EXPECT=1 cargo test -p husky-jar-utils
 # Check the Rust project, including tests
 cargo check --tests
 
-# Initialize a variable to track overall success
-OVERALL_SUCCESS=true
-
 # Loop through each target and run tests
 for TARGET in $MAKE_QUICK_TARGETS; do
   cargo build -p $TARGET --tests
