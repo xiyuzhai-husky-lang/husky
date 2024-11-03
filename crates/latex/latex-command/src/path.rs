fa7f6725603ccbc807375ba7d93f11bd9d0174a3
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-11-03 13:26:13 -0800

    init latex command

--- crates/latex/latex-command/src/path.rs
+++ crates/latex/latex-command/src/path.rs
@@ -1,7 +1,104 @@
+pub mod menu;
+
 use husky_coword::Coword;
+use thiserror::Error;
 
 #[salsa::derive_debug_with_db]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum LxCommandPath {
-    Coword(Coword),
+pub struct LxCommandPath {
+    package: LxPackagePath,
+    name: LxCommandName,
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum LxPackagePath {
+    Prelude,
+}
+
+#[salsa::derive_debug_with_db]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum LxCommandName {
+    LettersOnly(LettersOnlyLxCommandName),
+    Escape(OneDigitNonLetterLxCommandName),
+}
+
+#[salsa::derive_debug_with_db]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct LettersOnlyLxCommandName(Coword);
+
+#[salsa::derive_debug_with_db]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct OneDigitNonLetterLxCommandName(char);
+
+impl LxCommandPath {
+    #[deprecated(note = "ad hoc")]
+    pub fn new_prelude(ident: Coword, db: &salsa::Db) -> Self {
+        Self {
+            package: LxPackagePath::Prelude,
+            name: LxCommandName::new(ident, db).unwrap(),
+        }
+    }
+}
+
+impl LxCommandPath {
+    pub fn package(&self) -> LxPackagePath {
+        self.package
+    }
+
+    pub fn name(&self) -> LxCommandName {
+        self.name
+    }
+}
+
+impl LxCommandName {
+    pub fn new(ident: Coword, db: &salsa::Db) -> LxCommandIdentResult<Self> {
+        let data = ident.data(db);
+        if data.len() == 0 {
+            Err(LxCommandIdentError::Empty)?
+        } else if data.len() == 1 {
+            let c = data.chars().next().unwrap();
+            if !c.is_alphabetic() {
+                return Ok(Self::Escape(OneDigitNonLetterLxCommandName(c)));
+            }
+        } else {
+            for c in data.chars() {
+                if !c.is_alphabetic() {
+                    Err(LxCommandIdentError::NonAlphabeticCharater(c))?;
+                }
+            }
+        }
+        Ok(Self::LettersOnly(LettersOnlyLxCommandName(ident)))
+    }
+
+    pub fn new2(data: &str, db: &salsa::Db) -> LxCommandIdentResult<Self> {
+        if data.len() == 0 {
+            Err(LxCommandIdentError::Empty)?
+        } else if data.len() == 1 {
+            let c = data.chars().next().unwrap();
+            if !c.is_alphabetic() {
+                return Ok(Self::Escape(OneDigitNonLetterLxCommandName(c)));
+            }
+        } else {
+            for c in data.chars() {
+                if !c.is_alphabetic() {
+                    Err(LxCommandIdentError::NonAlphabeticCharater(c))?;
+                }
+            }
+        }
+        Ok(Self::LettersOnly(LettersOnlyLxCommandName(
+            Coword::from_ref(db, data),
+        )))
+    }
+}
+
+#[derive(Debug, Error, PartialEq, Eq)]
+pub enum LxCommandIdentError {
+    /// command identifier cannot be empty
+    #[error("empty identifier")]
+    Empty,
+    /// for an identifier with len > 1, all characters must be alphabetic
+    #[error("non alphabetic character: `{0}`")]
+    NonAlphabeticCharater(char),
+}
+
+pub type LxCommandIdentResult<T> = Result<T, LxCommandIdentError>;
