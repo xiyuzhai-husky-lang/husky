c8099235d0b75d6bbbd07e75e1b009e879192102
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-11-04 20:01:25 -0800

    parse latex rose ast

---
+++ crates/latex/latex-rose-punctuation/src/lib.rs
@@ -0,0 +1,30 @@
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[repr(u8)]
+pub enum LxRosePunctuation {
+    /// `,`
+    Comma,
+    /// `.`
+    Period,
+    /// `:`
+    Colon,
+    /// `;`
+    Semicolon,
+    /// `!`
+    Exclamation,
+    /// `?`
+    Question,
+}
+
+impl LxRosePunctuation {
+    pub fn try_from_char(c: char) -> Option<Self> {
+        match c {
+            ',' => Some(Self::Comma),
+            '.' => Some(Self::Period),
+            ':' => Some(Self::Colon),
+            ';' => Some(Self::Semicolon),
+            '!' => Some(Self::Exclamation),
+            '?' => Some(Self::Question),
+            _ => None,
+        }
+    }
+}
