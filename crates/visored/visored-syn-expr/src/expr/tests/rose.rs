c8a491fbba3ad20684a9118bf9a2dc7c962ab35a
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-11-04 22:51:51 -0800

    save

---
+++ crates/visored/visored-syn-expr/src/expr/tests/rose.rs
@@ -0,0 +1,29 @@
+use super::*;
+use crate::test_helpers::example::VdSynExprExample;
+use expect_test::{expect, Expect};
+use latex_prelude::mode::LxMode;
+use visored_annotation::annotation::{space::VdSpaceAnnotation, token::VdTokenAnnotation};
+
+fn t(
+    input: &str,
+    token_annotations: &[((&str, &str), VdTokenAnnotation)],
+    space_annotations: &[((&str, &str), VdSpaceAnnotation)],
+    expected: &Expect,
+) {
+    use crate::helpers::show::display_tree::VdSynExprDisplayTreeBuilder;
+
+    let db = &DB::default();
+    let example = VdSynExprExample::new(
+        input,
+        LxMode::Rose,
+        token_annotations,
+        space_annotations,
+        db,
+    );
+    expected.assert_eq(&example.show_display_tree(db));
+}
+
+#[test]
+fn let_sentence_vd_syn_expr_parsing_works() {
+    t("let $x = 1$", &[], &[], &expect![[r#""let x = 1""#]]);
+}
