43b93104942e7a3b2f8c309bc47f6300c26b701e
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-11-08 14:12:13 -0800

    "1+1" to lean works!

--- crates/lean/lean-hir-expr/src/lib.rs
+++ crates/lean/lean-hir-expr/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod builder;
 pub mod expr;
 pub mod fmt;
+pub mod helpers;
 pub mod jar;
 pub mod region;
 pub mod stmt;
--- crates/visored/visored-lean-transpilation/src/tests/math.rs
+++ crates/visored/visored-lean-transpilation/src/tests/math.rs
@@ -4,10 +4,14 @@ use latex_prelude::mode::LxMode;
 fn t(input: &str, expect: &Expect) {
     let db = &DB::default();
     let example = VdLeanTranspilationExample::new(input, LxMode::Math, &[], &[], db);
-    // expect.assert_eq(&example.show_display_tree(db));
+    expect.assert_eq(&example.show_display_tree(db));
 }
 
 #[test]
 fn basic_visored_to_lean_works() {
-    t("1 + 1", &expect![""]);
+    t("1 + 1", &expect![[r#"
+        binary: `+`
+        ├─ literal: `1`
+        └─ literal: `1`
+    "#]]);
 }
