e2f60a06954ac83ef6084807748558ad778c6a9e
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-11-09 13:04:46 -0800

    save

--- crates/visored/visored-resolution/src/table.rs
+++ crates/visored/visored-resolution/src/table.rs
@@ -1,5 +1,5 @@
 use crate::resolution::{
-    command::{VdCommandResolution, VdCommandResolutionMap},
+    command::{VdCompleteCommandResolution, VdCompleteCommandResolutionMap},
     punctuation::VdPunctuationResolution,
 };
 use latex_command::path::LxCommandPath;
@@ -7,18 +7,18 @@ use latex_math_punctuation::{LxMathPunctationMap, LxMathPunctuation};
 
 pub struct VdDefaultResolutionTable {
     punctuation_resolution_map: LxMathPunctationMap<Option<VdPunctuationResolution>>,
-    command_resolution_map: VdCommandResolutionMap,
+    complete_command_resolution_map: VdCompleteCommandResolutionMap,
 }
 
 impl VdDefaultResolutionTable {
     pub fn new(
         punctuation_resolution_map: LxMathPunctationMap<Option<VdPunctuationResolution>>,
-        command_resolution_map: VdCommandResolutionMap,
+        command_resolution_map: VdCompleteCommandResolutionMap,
         db: &salsa::Db,
     ) -> Self {
         Self {
             punctuation_resolution_map,
-            command_resolution_map,
+            complete_command_resolution_map: command_resolution_map,
         }
     }
 }
@@ -32,9 +32,9 @@ impl std::ops::Index<LxMathPunctuation> for VdDefaultResolutionTable {
 }
 
 impl std::ops::Index<LxCommandPath> for VdDefaultResolutionTable {
-    type Output = VdCommandResolution;
+    type Output = VdCompleteCommandResolution;
 
     fn index(&self, index: LxCommandPath) -> &Self::Output {
-        &self.command_resolution_map[&index]
+        &self.complete_command_resolution_map[&index]
     }
 }
