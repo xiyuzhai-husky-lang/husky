783d211497968dedfb81be97bd9d86b57361b18a
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-11-14 00:49:58 -0800

    init visored signature

---
+++ crates/visored/visored-signature/src/jar.rs
@@ -0,0 +1,2 @@
+#[salsa::jar]
+pub struct VdSignatureJar(crate::menu::vd_signature_menu);
---
+++ crates/visored/visored-signature/src/signature.rs
@@ -0,0 +1,7 @@
+pub mod binary_opr;
+pub mod function;
+pub mod prefix_opr;
+pub mod separator;
+pub mod suffix_opr;
+
+use crate::*;
