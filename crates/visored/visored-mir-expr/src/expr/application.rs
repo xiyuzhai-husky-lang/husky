d0cc3a456a283a46798a5c7dd3d65e14a2dd3826
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-11-14 18:38:32 -0800

    save

--- crates/visored/visored-mir-expr/src/expr/application.rs
+++ crates/visored/visored-mir-expr/src/expr/application.rs
@@ -1,5 +1,29 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
-pub enum VdMirApplicationFunction {
-    NormalSeparator,
+use super::*;
+use either::*;
+use visored_signature::signature::separator::base::VdBaseSeparatorSignature;
+use visored_zfc_ty::instantiation::VdInstantiation;
+
+pub mod menu;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum VdMirFunc {
+    NormalBaseSeparator(VdBaseSeparatorSignature),
     InSet,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub enum VdMirFuncKey {
+    NormalBaseSeparator(VdInstantiation),
+    InSet,
+}
+
+impl VdMirFunc {
+    pub fn key_or_expr(self, db: &::salsa::Db) -> Either<VdMirFuncKey, VdMirExprIdx> {
+        match self {
+            VdMirFunc::NormalBaseSeparator(signature) => Left(VdMirFuncKey::NormalBaseSeparator(
+                signature.instantiation(db),
+            )),
+            VdMirFunc::InSet => Left(VdMirFuncKey::InSet),
+        }
+    }
 }
--- crates/visored/visored-signature/src/signature/separator/base.rs
+++ crates/visored/visored-signature/src/signature/separator/base.rs
@@ -1,32 +1,8 @@
 use super::*;
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[salsa::interned]
 pub struct VdBaseSeparatorSignature {
-    instantiation: VdInstantiation,
-    item_ty: VdZfcType,
-    expr_ty: VdZfcType,
-}
-
-impl VdBaseSeparatorSignature {
-    pub(crate) fn new(
-        instantiation: VdInstantiation,
-        item_ty: VdZfcType,
-        expr_ty: VdZfcType,
-    ) -> Self {
-        Self {
-            instantiation,
-            item_ty,
-            expr_ty,
-        }
-    }
-}
-
-impl VdBaseSeparatorSignature {
-    pub fn item_ty(&self) -> VdZfcType {
-        self.item_ty
-    }
-
-    pub fn expr_ty(&self) -> VdZfcType {
-        self.expr_ty
-    }
+    pub instantiation: VdInstantiation,
+    pub item_ty: VdZfcType,
+    pub expr_ty: VdZfcType,
 }
