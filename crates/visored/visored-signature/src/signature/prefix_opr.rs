6c94ccf8d3d9781c86e4eff6903c7ab1f3f38884
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-11-15 22:27:32 -0800

    fix

--- crates/visored/visored-signature/src/signature/prefix_opr.rs
+++ crates/visored/visored-signature/src/signature/prefix_opr.rs
@@ -3,22 +3,15 @@ use super::*;
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub struct VdBasePrefixOprSignature {
     pub instantiation: VdInstantiation,
-    pub lopr_ty: VdType,
-    pub ropr_ty: VdType,
+    pub opd_ty: VdType,
     pub expr_ty: VdType,
 }
 
 impl VdBasePrefixOprSignature {
-    pub fn new(
-        instantiation: VdInstantiation,
-        lopr_ty: VdType,
-        ropr_ty: VdType,
-        expr_ty: VdType,
-    ) -> Self {
+    pub fn new(instantiation: VdInstantiation, opd_ty: VdType, expr_ty: VdType) -> Self {
         Self {
             instantiation,
-            lopr_ty,
-            ropr_ty,
+            opd_ty,
             expr_ty,
         }
     }
@@ -29,8 +22,8 @@ impl VdBasePrefixOprSignature {
         self.instantiation
     }
 
-    pub fn opr_ty(self) -> VdType {
-        self.ropr_ty
+    pub fn opd_ty(self) -> VdType {
+        self.opd_ty
     }
 
     pub fn expr_ty(self) -> VdType {
