fd4d4224effac1ea6a7e775601581e1ec8aaae0f
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-11-18 21:33:38 -0800

    parse full document to visored lean transpilation

--- crates/visored/visored-lean-transpilation/src/tests/document.rs
+++ crates/visored/visored-lean-transpilation/src/tests/document.rs
@@ -15,3 +15,75 @@ fn t(content: &str, expected_display_tree: &Expect, expected_fmt: &Expect) {
     expected_display_tree.assert_eq(&tracker.show_display_tree(db));
     expected_fmt.assert_eq(&tracker.show_fmt(db));
 }
+
+#[test]
+fn basic_document_to_vd_mir_works() {
+    t(
+        r#"\documentclass{article}
+\usepackage{amsmath}
+\begin{document}
+Let $x\in\mathbb{R}$.
+\end{document}"#,
+        &expect![[r#"
+            └─ group: `division`
+              └─ group: `paragraph`
+                └─ group: `sentence`
+                  └─ variable: `x`
+        "#]],
+        &expect!["variable x : ℝ"],
+    );
+    t(
+        r#"\documentclass{article}
+\usepackage{amsmath}
+\begin{document}
+\section{Introduction}
+Let $x\in\mathbb{R}$.
+\end{document}"#,
+        &expect![[r#"
+            └─ group: `division`
+              └─ group: `division`
+                └─ group: `paragraph`
+                  └─ group: `sentence`
+                    └─ variable: `x`
+        "#]],
+        &expect!["variable x : ℝ"],
+    );
+    t(
+        r#"\documentclass{article}
+\usepackage{amsmath}
+\begin{document}
+\section{Introduction}
+Let $x\in\mathbb{R}$.
+\subsection{Hello}
+Let $y\in\mathbb{R}$.
+\subsection{World}
+\subsection{This}
+\subsubsection{Is}
+\subsubsection{Bad}
+\end{document}"#,
+        &expect![[r#"
+            └─ group: `division`
+              ├─ group: `division`
+              │ └─ group: `paragraph`
+              │   └─ group: `sentence`
+              │     └─ variable: `x`
+              ├─ group: `division`
+              │ └─ group: `division`
+              │   └─ group: `paragraph`
+              │     └─ group: `sentence`
+              │       └─ variable: `y`
+              ├─ group: `division`
+              └─ group: `division`
+                ├─ group: `division`
+                └─ group: `division`
+        "#]],
+        &expect![[r#"
+            variable x : ℝ
+
+            variable y : ℝ
+
+
+
+        "#]],
+    );
+}
