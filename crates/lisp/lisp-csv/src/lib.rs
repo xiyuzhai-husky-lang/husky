c3df29fe01ac9e5c84e3da8a35c819f2454d7df9
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-11-21 13:49:32 -0800

    init lisp-csv

---
+++ crates/lisp/lisp-csv/src/lib.rs
@@ -0,0 +1,13 @@
+pub mod error;
+pub mod expr;
+pub mod file;
+pub mod helpers;
+mod parser;
+pub mod row;
+
+use self::{error::*, expr::*, file::*, parser::*, row::*};
+use ordered_float::OrderedFloat;
+
+pub fn parse_lp_csv(s: &str) -> Result<LpCsvFile, LpCsvError> {
+    LpCsvParser::new(s).parse_file()
+}
