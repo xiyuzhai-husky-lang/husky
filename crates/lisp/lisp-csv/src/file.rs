7131c2e7af126874c4c6e2b61dccea7040cf1552
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-11-21 16:02:58 -0800

    feat: lisp csv file

--- crates/lisp/lisp-csv/src/file.rs
+++ crates/lisp/lisp-csv/src/file.rs
@@ -8,9 +8,85 @@ pub enum LpCsvFile {
 impl<'a> LpCsvParser<'a> {
     pub(crate) fn parse_file(mut self) -> LpCsvResult<LpCsvFile> {
         let mut rows = Vec::new();
-        while let Some(row) = self.parse_row()? {
+        while let Some(row) = self.parse_row().into_result_option()? {
             rows.push(row);
         }
         Ok(LpCsvFile::Rows(rows))
     }
+}
+
+#[test]
+fn parse_lp_csv_file_works() {
+    fn t(input: &str, expected: Expect) {
+        let mut parser = LpCsvParser::new(input);
+        let file = parser.parse_file().unwrap();
+        expected.assert_debug_eq(&file);
+    }
+    t(
+        "1,2",
+        expect![[r#"
+        Rows(
+            [
+                SeparatedExprs(
+                    [
+                        Literal(
+                            Integer(
+                                1,
+                            ),
+                        ),
+                        Literal(
+                            Integer(
+                                2,
+                            ),
+                        ),
+                    ],
+                ),
+            ],
+        )
+    "#]],
+    );
+    t(
+        r#"mathbb,1,2
+mathcal,1,2"#,
+        expect![[r#"
+            Rows(
+                [
+                    SeparatedExprs(
+                        [
+                            Ident(
+                                "mathbb",
+                            ),
+                            Literal(
+                                Integer(
+                                    1,
+                                ),
+                            ),
+                            Literal(
+                                Integer(
+                                    2,
+                                ),
+                            ),
+                        ],
+                    ),
+                    SeparatedExprs(
+                        [
+                            Ident(
+                                "mathcal",
+                            ),
+                            Literal(
+                                Integer(
+                                    1,
+                                ),
+                            ),
+                            Literal(
+                                Integer(
+                                    2,
+                                ),
+                            ),
+                        ],
+                    ),
+                ],
+            )
+        "#]],
+    );
 }
--- crates/lisp/lisp-csv/src/row.rs
+++ crates/lisp/lisp-csv/src/row.rs
@@ -1,13 +1,133 @@
 use crate::*;
+use maybe_result::*;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum LpCsvRow {
-    Value(LpCsvExpr),
-    SeparatedValues(Vec<LpCsvExpr>),
+    Expr(LpCsvExpr),
+    SeparatedExprs(Vec<LpCsvExpr>),
 }
 
 impl<'a> LpCsvParser<'a> {
-    pub(crate) fn parse_row(&mut self) -> LpCsvResult<Option<LpCsvRow>> {
-        todo!()
+    pub(crate) fn parse_row(&mut self) -> LpCsvMaybeResult<LpCsvRow> {
+        self.ignore_whitespaces_and_tabs_and_blank_lines_and_comments();
+        let expr = self.parse_expr()?;
+        self.ignore_whitespaces_and_tabs_and_comments();
+        match self.chars.peek() {
+            Some(',') => {
+                let mut exprs: Vec<LpCsvExpr> = vec![expr];
+                loop {
+                    self.chars.eat_char();
+                    let Some(expr) = self.parse_expr() else {
+                        todo!()
+                    };
+                    exprs.push(expr);
+                    self.ignore_whitespaces_and_tabs_and_comments();
+                    match self.chars.peek() {
+                        Some(',') => (),
+                        Some('\n') | None => break,
+                        Some(_) => todo!(),
+                    }
+                }
+                JustOk(LpCsvRow::SeparatedExprs(exprs))
+            }
+            Some('\n') | None => JustOk(LpCsvRow::Expr(expr)),
+            Some(_) => todo!(),
+        }
     }
+}
+
+#[test]
+fn parse_lp_csv_row_works() {
+    fn t(input: &str, expect: Expect) {
+        let mut parser = LpCsvParser::new(input);
+        let row = parser.parse_row();
+        expect.assert_debug_eq(&row);
+    }
+    t(
+        "",
+        expect![[r#"
+        Nothing
+    "#]],
+    );
+    t(
+        "1",
+        expect![[r#"
+        JustOk(
+            Expr(
+                Literal(
+                    Integer(
+                        1,
+                    ),
+                ),
+            ),
+        )
+    "#]],
+    );
+    t(
+        "1,2",
+        expect![[r#"
+            JustOk(
+                SeparatedExprs(
+                    [
+                        Literal(
+                            Integer(
+                                1,
+                            ),
+                        ),
+                        Literal(
+                            Integer(
+                                2,
+                            ),
+                        ),
+                    ],
+                ),
+            )
+        "#]],
+    );
+    t(
+        "mathbb,1,2",
+        expect![[r#"
+            JustOk(
+                SeparatedExprs(
+                    [
+                        Ident(
+                            "mathbb",
+                        ),
+                        Literal(
+                            Integer(
+                                1,
+                            ),
+                        ),
+                        Literal(
+                            Integer(
+                                2,
+                            ),
+                        ),
+                    ],
+                ),
+            )
+        "#]],
+    );
+    t(
+        r#"1,2
+3,4"#,
+        expect![[r#"
+            JustOk(
+                SeparatedExprs(
+                    [
+                        Literal(
+                            Integer(
+                                1,
+                            ),
+                        ),
+                        Literal(
+                            Integer(
+                                2,
+                            ),
+                        ),
+                    ],
+                ),
+            )
+        "#]],
+    );
 }
