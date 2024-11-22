f572c9fb6c9fd4639c468ce4d0d3bb4cf6fdab90
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-11-21 16:02:58 -0800

    feat: lisp csv file

--- crates/lisp/lisp-csv/src/error.rs
+++ crates/lisp/lisp-csv/src/error.rs
@@ -1,6 +1,9 @@
+use maybe_result::MaybeResult;
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum LpCsvError {
     InvalidValue(String),
 }
 
 pub type LpCsvResult<T> = Result<T, LpCsvError>;
+pub type LpCsvMaybeResult<T> = MaybeResult<T, LpCsvError>;
