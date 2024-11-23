62556197cef8bea00cb69fca526acb8fb24fa736
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-11-23 03:21:55 -0800

    now dispatch tables are loaded from lisp csv files

--- crates/visored/visored-sem-expr/src/helpers/tracker.rs
+++ crates/visored/visored-sem-expr/src/helpers/tracker.rs
@@ -131,7 +131,8 @@ impl<'a, Input: IsVdSemExprInput<'a>> VdSemExprTracker<'a, Input> {
             output: syn_output,
         } = VdSynExprTracker::new(input, token_annotations, space_annotations, db);
         let item_path_zfc_ty_table = VdItemPathZfcTypeTable::new_standard(db);
-        let default_global_dispatch_table = VdDefaultGlobalDispatchTable::new_standard(db);
+        let default_global_dispatch_table =
+            VdDefaultGlobalDispatchTable::from_standard_lisp_csv_file_dir(db);
         let mut builder = VdSemExprBuilder::new(
             db,
             &token_storage,
