9427db34814d9a5aa954c97805c61261a26f9f77
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-11-26 18:53:07 -0800

    translate assume stmt

--- projects/ai-math-autoformalization/lean/central-46/Central46/Shorts/batch1.lean
+++ projects/ai-math-autoformalization/lean/central-46/Central46/Shorts/batch1.lean
@@ -30,15 +30,33 @@ end Example4
 namespace Example5
 variable (x2 : ℝ)
 
-def h4 : x2 + 1 / x2 - 2 = (x2 ^ 2 + 1 - 2 * x2) / x2 := sorry
+variable (h4 : x2 > 0)
 
-def h5 : (x2 ^ 2 + 1 - 2 * x2) / x2 = (x2 - 1) ^ 2 / x2 := sorry
+def h5 : x2 + 1 / x2 - 2 = (x2 ^ 2 + 1 - 2 * x2) / x2 := sorry
 
-def h6 : x2 + 1 / x2 - 2 = (x2 - 1) ^ 2 / x2 := sorry
+def h6 : (x2 ^ 2 + 1 - 2 * x2) / x2 = (x2 - 1) ^ 2 / x2 := sorry
 
-def h7 : (x2 - 1) ^ 2 / x2 ≥ 0 := sorry
+def h7 : x2 + 1 / x2 - 2 = (x2 - 1) ^ 2 / x2 := sorry
 
-def h8 : x2 + 1 / x2 - 2 ≥ 0 := sorry
+def h8 : (x2 - 1) ^ 2 / x2 ≥ 0 := sorry
 
-def h9 : x2 + 1 / x2 ≥ 2 := sorry
+def h9 : x2 + 1 / x2 - 2 ≥ 0 := sorry
+
+def h10 : x2 + 1 / x2 ≥ 2 := sorry
 end Example5
+
+namespace Example6
+variable (x3 : ℝ)
+
+def h11 : x3 + 1 / x3 - 2 = (x3 ^ 2 + 1 - 2 * x3) / x3 := sorry
+
+def h12 : (x3 ^ 2 + 1 - 2 * x3) / x3 = (x3 - 1) ^ 2 / x3 := sorry
+
+def h13 : x3 + 1 / x3 - 2 = (x3 - 1) ^ 2 / x3 := sorry
+
+def h14 : (x3 - 1) ^ 2 / x3 ≥ 0 := sorry
+
+def h15 : x3 + 1 / x3 - 2 ≥ 0 := sorry
+
+def h16 : x3 + 1 / x3 ≥ 2 := sorry
+end Example6
