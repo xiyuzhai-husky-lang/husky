ec5756819b3501fb498efa6a2df23f4b382f146c
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-11-28 03:11:29 -0800

    impl `latex-ast-hsy`

--- registry/latex-ast-hsy-0.1.0/src/lib.rs
+++ registry/latex-ast-hsy-0.1.0/src/lib.rs
@@ -1,10 +1,112 @@
+mod file;
+mod tracker;
+
+use self::{file::*, tracker::*};
 use husky_core::*;
 use husky_standard_linket_impl::ugly::*;
+use idx_arena::ArenaIdx;
 use latex_ast::ast::LxAstIdx;
 
 pub struct LxAstId {
-    // TODO: file id
+    pub file: LxFileIdx,
     pub idx: LxAstIdx,
+}
+
+impl From<__VarId> for LxAstId {
+    fn from(id: __VarId) -> Self {
+        let __VarId::Pair([fst, snd]) = id else {
+            todo!()
+        };
+        Self {
+            file: fst.into(),
+            idx: unsafe { lx_ast_idx_from_u32(snd) },
+        }
+    }
+}
+
+unsafe fn lx_ast_idx_from_u32(idx: u32) -> LxAstIdx {
+    match idx >> 30 {
+        0 => LxAstIdx::Math(ArenaIdx::new_ext(idx as usize & 0x3FFFFFFF)),
+        1 => LxAstIdx::Rose(ArenaIdx::new_ext(idx as usize & 0x3FFFFFFF)),
+        2 => LxAstIdx::Lisp(ArenaIdx::new_ext(idx as usize & 0x3FFFFFFF)),
+        3 => LxAstIdx::Root(ArenaIdx::new_ext(idx as usize & 0x3FFFFFFF)),
+        _ => unreachable!(),
+    }
+}
+
+unsafe fn lx_ast_idx_to_u32(idx: LxAstIdx) -> u32 {
+    match idx {
+        LxAstIdx::Math(idx) => {
+            assert!(idx.index() <= 0x3FFFFFFF);
+            (0b00 << 30) | idx.index() as u32
+        }
+        LxAstIdx::Rose(idx) => {
+            assert!(idx.index() <= 0x3FFFFFFF);
+            (0b01 << 30) | idx.index() as u32
+        }
+        LxAstIdx::Lisp(idx) => {
+            assert!(idx.index() <= 0x3FFFFFFF);
+            (0b10 << 30) | idx.index() as u32
+        }
+        LxAstIdx::Root(idx) => {
+            assert!(idx.index() <= 0x3FFFFFFF);
+            (0b11 << 30) | idx.index() as u32
+        }
+    }
+}
+
+#[test]
+fn conversion_between_u32_and_ast_idx_works() {
+    // Test Math variant (00)
+    let idx = LxAstIdx::Math(unsafe { ArenaIdx::new_ext(0) });
+    assert_eq!(
+        unsafe { lx_ast_idx_to_u32(idx) },
+        0b0000_0000_0000_0000_0000_0000_0000_0000
+    );
+    assert_eq!(
+        unsafe { lx_ast_idx_from_u32(unsafe { lx_ast_idx_to_u32(idx) }) },
+        idx
+    );
+
+    // Test Rose variant (01)
+    let idx = LxAstIdx::Rose(unsafe { ArenaIdx::new_ext(42) });
+    assert_eq!(
+        unsafe { lx_ast_idx_to_u32(idx) },
+        0b0100_0000_0000_0000_0000_0000_0010_1010
+    );
+    assert_eq!(
+        unsafe { lx_ast_idx_from_u32(unsafe { lx_ast_idx_to_u32(idx) }) },
+        idx
+    );
+
+    // Test Lisp variant (10)
+    let idx = LxAstIdx::Lisp(unsafe { ArenaIdx::new_ext(0x3FFFFFFE) });
+    assert_eq!(
+        unsafe { lx_ast_idx_to_u32(idx) },
+        0b1011_1111_1111_1111_1111_1111_1111_1110
+    );
+    assert_eq!(
+        unsafe { lx_ast_idx_from_u32(unsafe { lx_ast_idx_to_u32(idx) }) },
+        idx
+    );
+
+    // Test Root variant (11)
+    let idx = LxAstIdx::Root(unsafe { ArenaIdx::new_ext(0x3FFFFFFF) });
+    assert_eq!(
+        unsafe { lx_ast_idx_to_u32(idx) },
+        0b1111_1111_1111_1111_1111_1111_1111_1111
+    );
+    assert_eq!(
+        unsafe { lx_ast_idx_from_u32(unsafe { lx_ast_idx_to_u32(idx) }) },
+        idx
+    );
+}
+
+#[test]
+#[should_panic(expected = "assertion failed")]
+fn test_idx_overflow() {
+    let idx = LxAstIdx::Math(unsafe { ArenaIdx::new_ext(0x40000000) });
+    unsafe { lx_ast_idx_to_u32(idx) }; // Should panic due to index being too large
 }
 
 #[allow(non_upper_case_globals)]
@@ -53,7 +155,7 @@ impl __IsStaticVar<__VarId> for AST {
     ) -> __StaticVarResult<(__VarId, impl FnOnce() + 'static)> {
         // TODO: is this correct?
         todo!();
-        let default = 0usize.into();
+        let default = [0, 0].into();
         Ok((default, Self::try_set_var_id(default, locked)?))
     }
 
