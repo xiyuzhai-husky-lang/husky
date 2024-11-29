48e94c02cdd898a364b7ae83280e0e6ea0aefb5c
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-11-28 17:26:35 -0800

    save

--- crates/value/husky-standard-value/src/thawed.rs
+++ crates/value/husky-standard-value/src/thawed.rs
@@ -1216,6 +1216,7 @@ impl From<Value> for ThawedValue {
             Value::OptionBox(immortal_dyn) => todo!(),
             Value::OptionLeash(_) => todo!(),
             Value::EnumUnit { index, presenter } => ThawedValue::EnumUnit { index, presenter },
+            Value::U32U32Pair(_, _) => todo!(),
         }
     }
 }
