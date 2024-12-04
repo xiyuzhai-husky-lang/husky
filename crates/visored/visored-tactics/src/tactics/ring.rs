e7cd52801388bda60d588387f96ad756f82fbda3
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-04 14:05:23 -0800

    init ring

--- crates/visored/visored-tactics/src/tactics/ring.rs
+++ crates/visored/visored-tactics/src/tactics/ring.rs
@@ -1,87 +1,50 @@
+mod engine;
+
 use berserk::*;
 use db::BerserkerDb;
-
-// #[berserk::berserk]
-// pub struct RingTerm<'db> {
-//     #[return_ref]
-//     data: RingTermData<'db>,
-// }
+use husky_sha_utils::Sha256Output;
+use idx_arena::{Arena, ArenaIdx};
+use vec_like::ordered_small_vec_map::OrderedSmallVecPairMap;
+use visored_term::term::literal::VdLiteral;
 
 #[derive(Debug, Clone, Hash, Eq, PartialEq, PartialOrd, Ord)]
-pub struct __RingTermData<'db> {
-    data: RingTermData<'db>,
+pub enum Term {
+    Literal(LiteralTerm),
+    NonLiteral(NonLiteralTerm),
 }
 
-impl<'db> AsStatic for __RingTermData<'db> {
-    type Static = RingTermData<'static>;
-
-    fn as_static(self) -> Self::Static {
-        unsafe { std::mem::transmute(self.data) }
-    }
-}
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct RingTerm<'db>(::berserk::Berserk<'db, __RingTermData<'db>>);
-
-impl<'db> ::berserk::as_id::AsBerserkId<'db> for RingTerm<'db> {
-    fn as_id(self) -> u32 {
-        self.0.as_id()
-    }
-    fn from_id(id: u32, db: &'db ::berserk::db::BerserkerDb) -> Self {
-        Self(berserk::Berserk::from_id(id, db))
-    }
-}
-impl<'db> RingTerm<'db> {
-    pub fn new(data: RingTermData<'db>, db: &'db ::berserk::db::BerserkerDb) -> Self {
-        use berserk::Berserk;
-        let data = __RingTermData { data };
-        RingTerm(db.berserk(data))
-    }
-    pub fn data(self) -> &'db RingTermData<'db> {
-        &self.0 .0.value.data
-    }
-}
-impl<'db, Q: ?Sized> std::borrow::Borrow<Q> for __RingTermData<'db>
-where
-    RingTermData<'db>: std::borrow::Borrow<Q>,
-{
-    fn borrow(&self) -> &Q {
-        self.data.borrow()
-    }
-}
-impl<'db, 'a, Q: ?Sized> From<&'a Q> for __RingTermData<'db>
-where
-    RingTermData<'db>: From<&'a Q>,
-{
-    fn from(q: &'a Q) -> Self {
-        Self { data: q.into() }
-    }
-}
-impl<'db> RingTerm<'db> {
-    pub fn from_ref<Q: Eq + std::hash::Hash + ?Sized>(
-        q: &Q,
-        db: &'db ::berserk::db::BerserkerDb,
-    ) -> Self
-    where
-        RingTermData<'db>: std::borrow::Borrow<Q> + for<'a> From<&'a Q>,
-    {
-        RingTerm(db.berserk_ref::<__RingTermData<'db>, Q>(q))
-    }
+#[derive(Debug, Clone, Copy, Hash, Eq, PartialEq, PartialOrd, Ord)]
+pub enum LiteralTerm {
+    Nat128(i128),
+    BigNat(/* TODO */),
+    Rat128(i128, u128),
 }
 
-impl<'db> std::fmt::Debug for RingTerm<'db> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
-    }
+impl LiteralTerm {
+    pub const ZERO: Self = Self::Nat128(0);
+    pub const ONE: Self = Self::Nat128(1);
 }
+
+type NonLiteralAtomExponentials = NonLiteralTermMap<NonLiteralTerm>;
 
 #[derive(Debug, Clone, Hash, Eq, PartialEq, PartialOrd, Ord)]
-pub enum RingTermData<'db> {
+pub enum NonLiteralTermData {
     Atom,
-    Product(Vec<RingTerm<'db>>),
+    Product {
+        literal: LiteralTerm,
+        nonliteral_atom_exponentials: NonLiteralAtomExponentials,
+    },
+    Sum {
+        nonliteral_monomial_coefficients: Vec<NonLiteralTerm>,
+        constant_term: LiteralTerm,
+    },
 }
 
-impl<'db> RingTerm<'db> {
-    pub fn product(factors: impl IntoIterator<Item = Self>, db: &'db BerserkerDb) -> Self {
-        Self::new(RingTermData::Product(factors.into_iter().collect()), db)
-    }
+pub struct NonLiteralTermEntry {
+    data: NonLiteralTermData,
+    sha256: Sha256Output,
 }
+
+pub type NonLiteralTerm = ArenaIdx<NonLiteralTermEntry>;
+pub type NonLiteralTermArena = Arena<NonLiteralTermEntry>;
+pub type NonLiteralTermMap<T> = OrderedSmallVecPairMap<NonLiteralTerm, T, 4>;
