6085acbbcbef17808a87eb48e8539ba9be4db632
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-06 00:14:10 -0800

    rename llm-lock to llm-cap and add more features

--- crates/llms/openai/src/lib.rs
+++ crates/llms/openai/src/lib.rs
@@ -1,7 +1,28 @@
-pub struct OaiClient {
-    cache: LlmCache<String, String>,
+pub mod error;
+pub mod request;
+pub mod response;
+
+use self::{error::*, request::*, response::*};
+use lazy_static::lazy_static;
+use llm_cache::LlmCache;
+use llm_cap::LlmCap;
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+
+struct OaiClient {
+    lock: LlmCap,
+    cache: LlmCache<OaiRequest, OaiResponse>,
 }
 
-pub enum OaiRequest {}
+impl OaiClient {
+    fn new(file_path: PathBuf) -> OaiResult<Self> {
+        Ok(Self {
+            lock: LlmCap::new("OpenAI", "ENABLE_OPENAI_API_CALLING"),
+            cache: LlmCache::new(file_path)?,
+        })
+    }
+}
 
-pub enum OaiResponse {}
+lazy_static! {
+    static ref CLIENT: OaiClient = OaiClient::new(PathBuf::from("openai_cache.json")).unwrap();
+}
