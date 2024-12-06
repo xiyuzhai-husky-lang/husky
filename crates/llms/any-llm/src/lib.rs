2dea9051c89bc03cb6b18fdeebe7c7c8e0c3d260
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-06 11:41:59 -0800

    towards a working pipeline

--- crates/llms/any-llm/src/lib.rs
+++ crates/llms/any-llm/src/lib.rs
@@ -10,8 +10,10 @@ pub struct AnyLlmClient {
 
 impl AnyLlmClient {
     pub fn new(cache_dir: PathBuf) -> AnyLlmResult<Self> {
+        let oai_cache_path = cache_dir.join("openai.json");
+        assert!(oai_cache_path.parent().unwrap().exists());
         Ok(Self {
-            openai: OaiClient::new(todo!())?,
+            openai: OaiClient::new(oai_cache_path)?,
         })
     }
 }
--- crates/visored/visored-pipeline/src/instance.rs
+++ crates/visored/visored-pipeline/src/instance.rs
@@ -1,15 +1,19 @@
-use crate::{error::VdPipelineResult, tracker::VdPipelineTracker, VdPipelineConfig};
+use crate::{
+    error::VdPipelineResult, input::VdPipelineInput, tracker::VdPipelineTracker, VdPipelineConfig,
+};
 use std::sync::Arc;
 
 pub struct VdPipelineInstance {
     config: VdPipelineConfig,
+    input: Arc<VdPipelineInput>,
     tracker: Option<VdPipelineTracker>,
 }
 
 impl VdPipelineInstance {
-    pub fn new(config: VdPipelineConfig, src_file: Arc<String>) -> Self {
+    pub fn new(config: VdPipelineConfig, src_file: Arc<VdPipelineInput>) -> Self {
         Self {
             config,
+            input: src_file,
             tracker: None,
         }
     }
@@ -18,7 +22,7 @@ impl VdPipelineInstance {
 impl VdPipelineInstance {
     pub fn run(&mut self) -> VdPipelineResult<()> {
         assert!(self.tracker.is_none());
-        self.tracker = Some(VdPipelineTracker::new(&self.config));
+        self.tracker = Some(VdPipelineTracker::new(&self.config, self.input.clone()));
         Ok(())
     }
 }
