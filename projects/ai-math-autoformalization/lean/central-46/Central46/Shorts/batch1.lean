73686aabe66b80909ebef18bcdcfda62d7b69d37
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-08 10:54:19 -0800

    fix missing assumptions

--- projects/ai-math-autoformalization/lean/central-46/Central46/Shorts/batch1.lean
+++ projects/ai-math-autoformalization/lean/central-46/Central46/Shorts/batch1.lean
@@ -28,50 +28,50 @@ def h8(x : ℝ)(h9 : x > 0) := by
   have h15 : x + 1 / x ≥ 2 := by obvious
   exact ()
 
-def h16(x : ℝ) := by
-  have h17 : x + 1 / x - 2 = (x ^ 2 + 1 - 2 * x) / x := by obvious
-  have h18 : (x ^ 2 + 1 - 2 * x) / x = (x - 1) ^ 2 / x := by obvious
-  have h19 : x + 1 / x - 2 = (x - 1) ^ 2 / x := by obvious
-  have h20 : (x - 1) ^ 2 / x ≥ 0 := by obvious
-  have h21 : x + 1 / x - 2 ≥ 0 := by obvious
-  have h22 : x + 1 / x ≥ 2 := by obvious
+def h16(x : ℝ)(h17 : x > 0) := by
+  have h18 : x + 1 / x - 2 = (x ^ 2 + 1 - 2 * x) / x := by obvious
+  have h19 : (x ^ 2 + 1 - 2 * x) / x = (x - 1) ^ 2 / x := by obvious
+  have h20 : x + 1 / x - 2 = (x - 1) ^ 2 / x := by obvious
+  have h21 : (x - 1) ^ 2 / x ≥ 0 := by obvious
+  have h22 : x + 1 / x - 2 ≥ 0 := by obvious
+  have h23 : x + 1 / x ≥ 2 := by obvious
   exact ()
 
-def h23(x : ℝ) := by
-  have h24 : x ^ 2 + 1 - 2 * x = (x - 1) ^ 2 := by obvious
-  have h25 : (x - 1) ^ 2 ≥ 0 := by obvious
-  have h26 : x ^ 2 + 1 - 2 * x ≥ 0 := by obvious
-  have h27 : x ^ 2 + 1 ≥ 2 * x := by obvious
+def h24(x : ℝ) := by
+  have h25 : x ^ 2 + 1 - 2 * x = (x - 1) ^ 2 := by obvious
+  have h26 : (x - 1) ^ 2 ≥ 0 := by obvious
+  have h27 : x ^ 2 + 1 - 2 * x ≥ 0 := by obvious
+  have h28 : x ^ 2 + 1 ≥ 2 * x := by obvious
   exact ()
 
-def h28(x : ℝ)(h29 : x > 0) := by
-  have h30 : x + 1 - 2 * (√ x) = ((√ x) - 1) ^ 2 := by obvious
-  have h31 : ((√ x) - 1) ^ 2 ≥ 0 := by obvious
-  have h32 : x + 1 - 2 * (√ x) ≥ 0 := by obvious
-  have h33 : x + 1 ≥ 2 * (√ x) := by obvious
+def h29(x : ℝ)(h30 : x > 0) := by
+  have h31 : x + 1 - 2 * (√ x) = ((√ x) - 1) ^ 2 := by obvious
+  have h32 : ((√ x) - 1) ^ 2 ≥ 0 := by obvious
+  have h33 : x + 1 - 2 * (√ x) ≥ 0 := by obvious
+  have h34 : x + 1 ≥ 2 * (√ x) := by obvious
   exact ()
 
-def h34(x : ℝ)(h35 : x > 0)(y : ℝ)(h36 : y > 0) := by
-  have h37 : 1 / x + 1 / y - 4 / (x + y) = (y * (x + y) + x * (x + y) - 4 * x * y) / (x * y * (x + y)) := by obvious
-  have h38 : (y * (x + y) + x * (x + y) - 4 * x * y) / (x * y * (x + y)) = (y * x + x ^ 2 + x ^ 2 + y * x - 4 * x * y) / (x * y * (x + y)) := by obvious
-  have h39 : (y * x + x ^ 2 + x ^ 2 + y * x - 4 * x * y) / (x * y * (x + y)) = (x ^ 2 + x ^ 2 - 2 * x * y) / (x * y * (x + y)) := by obvious
-  have h40 : (x ^ 2 + x ^ 2 - 2 * x * y) / (x * y * (x + y)) = (x - y) ^ 2 / (x * y * (x + y)) := by obvious
-  have h41 : (x - y) ^ 2 / (x * y * (x + y)) ≥ 0 := by obvious
-  have h42 : 1 / x + 1 / y - 4 / (x + y) ≥ 0 := by obvious
-  have h43 : 1 / x + 1 / y ≥ 4 / (x + y) := by obvious
+def h35(x : ℝ)(h36 : x > 0)(y : ℝ)(h37 : y > 0) := by
+  have h38 : 1 / x + 1 / y - 4 / (x + y) = (y * (x + y) + x * (x + y) - 4 * x * y) / (x * y * (x + y)) := by obvious
+  have h39 : (y * (x + y) + x * (x + y) - 4 * x * y) / (x * y * (x + y)) = (y * x + x ^ 2 + x ^ 2 + y * x - 4 * x * y) / (x * y * (x + y)) := by obvious
+  have h40 : (y * x + x ^ 2 + x ^ 2 + y * x - 4 * x * y) / (x * y * (x + y)) = (x ^ 2 + x ^ 2 - 2 * x * y) / (x * y * (x + y)) := by obvious
+  have h41 : (x ^ 2 + x ^ 2 - 2 * x * y) / (x * y * (x + y)) = (x - y) ^ 2 / (x * y * (x + y)) := by obvious
+  have h42 : (x - y) ^ 2 / (x * y * (x + y)) ≥ 0 := by obvious
+  have h43 : 1 / x + 1 / y - 4 / (x + y) ≥ 0 := by obvious
+  have h44 : 1 / x + 1 / y ≥ 4 / (x + y) := by obvious
   exact ()
 
-def h44(a : ℝ)(h45 : a > 0)(b : ℝ)(h46 : b > 0) := by
-  have h47 : a / b + b / a - 2 = (a ^ 2 + b ^ 2 - 2 * a * b) / (a * b) := by obvious
-  have h48 : (a ^ 2 + b ^ 2 - 2 * a * b) / (a * b) = (a - b) ^ 2 / (a * b) := by obvious
-  have h49 : (a - b) ^ 2 / (a * b) ≥ 0 := by obvious
-  have h50 : a / b + b / a - 2 ≥ 0 := by obvious
-  have h51 : a / b + b / a ≥ 2 := by obvious
+def h45(a : ℝ)(h46 : a > 0)(b : ℝ)(h47 : b > 0) := by
+  have h48 : a / b + b / a - 2 = (a ^ 2 + b ^ 2 - 2 * a * b) / (a * b) := by obvious
+  have h49 : (a ^ 2 + b ^ 2 - 2 * a * b) / (a * b) = (a - b) ^ 2 / (a * b) := by obvious
+  have h50 : (a - b) ^ 2 / (a * b) ≥ 0 := by obvious
+  have h51 : a / b + b / a - 2 ≥ 0 := by obvious
+  have h52 : a / b + b / a ≥ 2 := by obvious
   exact ()
 
-def h52(x : ℝ)(y : ℝ) := by
-  have h53 : x ^ 2 + y ^ 2 - 2 * x * y = (x - y) ^ 2 := by obvious
-  have h54 : (x - y) ^ 2 ≥ 0 := by obvious
-  have h55 : x ^ 2 + y ^ 2 - 2 * x * y ≥ 0 := by obvious
-  have h56 : x ^ 2 + y ^ 2 ≥ 2 * x * y := by obvious
+def h53(x : ℝ)(y : ℝ) := by
+  have h54 : x ^ 2 + y ^ 2 - 2 * x * y = (x - y) ^ 2 := by obvious
+  have h55 : (x - y) ^ 2 ≥ 0 := by obvious
+  have h56 : x ^ 2 + y ^ 2 - 2 * x * y ≥ 0 := by obvious
+  have h57 : x ^ 2 + y ^ 2 ≥ 2 * x * y := by obvious
   exact ()
