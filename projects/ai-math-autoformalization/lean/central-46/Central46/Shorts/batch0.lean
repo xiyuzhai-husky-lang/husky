0b94fd96375a9132ef3fb706912bc9ccffe3e8f5
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-08 00:12:08 -0800

    save

--- projects/ai-math-autoformalization/lean/central-46/Central46/Shorts/batch0.lean
+++ projects/ai-math-autoformalization/lean/central-46/Central46/Shorts/batch0.lean
@@ -5,24 +5,21 @@ import Mathlib.Data.Real.Sqrt
 import Mathlib.Tactic.Explode
 
 def h := by
-  have h1 : 1 + 1 = 2 := by 
-  obvious
+  have h1 : 1 + 1 = 2 := by obvious
 
 def h2 := by
-  have h3 : x ^ 2 ≥ 0 := by 
-  obvious
+  have h3 : x ^ 2 ≥ 0 := by obvious
 
 def h4 := by
-  have h5 : x ^ 2 + 1 ≥ 2 * x := by 
-  obvious
+  have h5 : x ^ 2 + 1 ≥ 2 * x := by obvious
 
 def h6 := by
   calc
-    1 = 1 := sorry
+      1 = 1 := sorry
     _ = 1 := sorry
   calc
-    1 = 1 := sorry
+      1 = 1 := sorry
     _ < 2 := sorry
   calc
-    1.0 ≤ 1 := sorry
+      1.0 ≤ 1 := sorry
     _ ≤ 1 := sorry
