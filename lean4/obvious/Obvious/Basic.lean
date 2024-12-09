837a6ed7f0973f13d2fb1462ac605697f07b4338
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-09 12:25:36 -0800

    improve obvious

--- lean4/obvious/Obvious/Basic.lean
+++ lean4/obvious/Obvious/Basic.lean
@@ -4,27 +4,29 @@ namespace Obvious
 
 def hello := "world"
 
-syntax "obvious" : tactic
+syntax "attack" : tactic
 
 macro_rules
-  | `(tactic| obvious) => `(tactic|
-      first
-      | ring; done
-      | (
-        ring_nf
-        rw [Real.sq_sqrt]
-        ring
-        repeat obvious
-        done
-      )
-      | (congr;(try (
-        first
-        | simp; done
-        | (nlinarith; done)
-        | (apply sq_nonneg; repeat obvious; done)
-        | (apply div_nonneg; repeat obvious; done)
-        | ((try field_simp); ring; done)
-        | linarith; done))
-    ))
+| `(tactic| attack) => `(tactic|
+  first
+  | simp; done
+  | ring; done
+  | ring_nf; rw [Real.sq_sqrt]; ring; all_goals attack; done
+  | nlinarith; done
+  | apply sq_nonneg; all_goals attack; done
+  | apply div_nonneg; all_goals attack; done
+  | field_simp; ring; done
+  | linarith; done
+)
+
+macro "obvious": tactic =>`(tactic|
+  first
+  | attack; done
+  | congr; all_goals attack; done
+  | gcongr; all_goals attack; done
+  | fail "Could not prove this goal automatically. It might not be as obvious as you think!"
+)
 
 end Obvious
+
+declare_syntax_cat index
