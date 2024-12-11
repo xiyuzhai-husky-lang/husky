ad619bd474e130afaeee0c687c12ad1c73f04ff6
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-10 23:52:45 -0800

    try pyo3

---
+++ crates/llms/transformers-llm/python/lib.py
@@ -0,0 +1,8 @@
+def example_function():
+    """
+    Example function that will be called from Rust.
+    Returns a value that can be safely passed back to Rust.
+    """
+    return "Hello from Python!"
+
+# You can add more functions here as needed
