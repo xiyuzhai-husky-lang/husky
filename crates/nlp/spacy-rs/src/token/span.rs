0681facb83caba05620f0faeab126479d9c8fd95
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-14 20:18:25 -0800

    improve

--- crates/nlp/spacy-rs/src/token/span.rs
+++ crates/nlp/spacy-rs/src/token/span.rs
@@ -1,8 +1,17 @@
 use super::*;
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, FromPyObject)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Span {
     start: usize,
     end: usize,
-    root: Token,
+    root: usize,
+}
+
+impl<'py> FromPyObject<'py> for Span {
+    fn extract_bound(ob: &Bound<'py, PyAny>) -> PyResult<Self> {
+        let start = ob.getattr("start")?.extract()?;
+        let end = ob.getattr("end")?.extract()?;
+        let root = ob.getattr("root")?.getattr("i")?.extract()?;
+        Ok(Self { start, end, root })
+    }
 }
