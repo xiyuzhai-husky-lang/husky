0fa1d184d608045cb646f0dd14d0f95a8e39d9db
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-14 18:27:27 -0800

    add i to Token

--- crates/nlp/spacy-rs/src/token.rs
+++ crates/nlp/spacy-rs/src/token.rs
@@ -6,9 +6,5 @@ use pyo3::prelude::*;
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, FromPyObject)]
 pub struct Token {
     pub text: String,
-    // pub lemma: String,
-    // pub pos: String,
-    // pub tag: String,
-    // pub dep: String,
-    // pub head: usize,
+    pub i: usize,
 }
