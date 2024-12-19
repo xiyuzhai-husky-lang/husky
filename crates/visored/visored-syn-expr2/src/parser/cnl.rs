99627731a999922d0b3d8fc41dae8ab0508409b6
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-19 13:41:37 -0800

    save

---
+++ crates/visored/visored-syn-expr2/src/parser/cnl.rs
@@ -0,0 +1,2 @@
+//! `cnl` stands for `controlled natural language`.
+//! It's derived from `snl`, super natural language.
---
+++ crates/visored/visored-syn-expr2/src/parser/expr.rs
@@ -0,0 +1,68 @@
+// TODO: maybe move this under expr
+mod accept;
+mod debug;
+mod disambiguate;
+mod env;
+mod expr_stack;
+pub(crate) mod incomplete_expr;
+mod utils;
+
+use eterned::db::EternerDb;
+use expr::VdSynExprIdx;
+use latex_ast::ast::math::{LxMathAstIdx, LxMathAstIdxRange};
+use range::VdSynExprTokenIdxRange;
+use visored_opr::precedence::{VdPrecedence, VdPrecedenceRange};
+
+use self::expr_stack::VdSynExprStack;
+use crate::builder::VdSynExprBuilder;
+use crate::*;
+
+pub struct VdSynExprParser<'a, 'db> {
+    builder: &'a mut VdSynExprBuilder<'db>,
+    stack: VdSynExprStack,
+}
+
+impl<'a, 'db> VdSynExprParser<'a, 'db> {
+    fn new(builder: &'a mut VdSynExprBuilder<'db>) -> Self {
+        Self {
+            builder,
+            stack: Default::default(),
+        }
+    }
+}
+
+impl<'a, 'db> VdSynExprParser<'a, 'db> {
+    pub fn db(&self) -> &'db EternerDb {
+        self.builder.db()
+    }
+}
+
+impl<'db> VdSynExprBuilder<'db> {
+    pub fn expr_parser(&mut self) -> VdSynExprParser<'_, 'db> {
+        VdSynExprParser::new(self)
+    }
+}
+
+impl<'a, 'db> VdSynExprParser<'a, 'db> {
+    pub fn parse_expr_from_math_asts(mut self, asts: LxMathAstIdxRange) -> VdSynExprIdx {
+        let mut next = asts.start();
+        let end = asts.end();
+        while next < end {
+            self.parse_next_expr_since_nath_ast(&mut next, end);
+        }
+        self.reduce(VdPrecedenceRange::Any, None);
+        let Self { builder, stack } = self;
+        builder.alloc_expr(stack.finish())
+    }
+
+    fn parse_next_expr_since_nath_ast(&mut self, next: &mut LxMathAstIdx, end: LxMathAstIdx) {
+        let range = self.builder.ast_token_idx_range_map()[*next];
+        let preceding_space_annotation = self
+            .builder
+            .annotations()
+            .preceding_space_annotation(range.start());
+        let token = self.resolve_token(next, end);
+        let range = range.join(self.builder.ast_token_idx_range_map()[*next - 1]);
+        self.accept_ast(preceding_space_annotation, range, token);
+    }
+}
