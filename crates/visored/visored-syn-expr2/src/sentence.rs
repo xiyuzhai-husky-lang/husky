e0c826945eb23431bbb5911f9183255fc44aa51e
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-19 16:49:14 -0800

    organise

--- crates/visored/visored-syn-expr2/src/sentence.rs
+++ crates/visored/visored-syn-expr2/src/sentence.rs
@@ -1,4 +1,6 @@
+mod cnl;
 pub mod helpers;
+mod unl;
 
 use crate::{
     builder::{ToVdSyn, VdSynExprBuilder},
@@ -77,73 +79,5 @@ impl<'db> VdSynExprBuilder<'db> {
             SnlMode::Unl => todo!(),
             SnlMode::Cnl => self.parse_cnl_sentence(token_idx, word, asts, vibe),
         }
-    }
-
-    fn parse_cnl_sentence(
-        &mut self,
-        token_idx: LxRoseTokenIdx,
-        word: BaseCoword,
-        asts: &mut Peekable<impl Iterator<Item = LxRoseAstIdx>>,
-        vibe: VdSynExprVibe,
-    ) -> VdSynSentenceEntry {
-        let clauses = vec![self.parse_clause(token_idx, word, asts, vibe)];
-        let end = loop {
-            if self.peek_new_division(asts).is_some() {
-                break VdSynSentenceEnd::Void;
-            }
-            if let Some(ast_idx) = asts.next() {
-                match self.ast_arena()[ast_idx] {
-                    LxRoseAstData::TextEdit { .. } => todo!(),
-                    LxRoseAstData::Word(token_idx, coword) => {
-                        self.emit_message_over_token_to_stdout(
-                            *token_idx,
-                            format!("coword: {}", coword),
-                        );
-                        todo!("coword: {}", coword)
-                    }
-                    LxRoseAstData::Punctuation(pucntuation_token_idx, punctuation) => {
-                        match punctuation {
-                            LxRosePunctuation::Comma => todo!(),
-                            LxRosePunctuation::Period => {
-                                break VdSynSentenceEnd::Period(pucntuation_token_idx)
-                            }
-                            LxRosePunctuation::Colon => todo!(),
-                            LxRosePunctuation::Semicolon => todo!(),
-                            LxRosePunctuation::Exclamation => todo!(),
-                            LxRosePunctuation::Question => todo!(),
-                            LxRosePunctuation::LeftCurl => todo!(),
-                            LxRosePunctuation::RightCurl => todo!(),
-                            LxRosePunctuation::LeftBox => todo!(),
-                            LxRosePunctuation::RightBox => todo!(),
-                            LxRosePunctuation::EscapedBackslash => todo!(),
-                            LxRosePunctuation::EscapedLcurl => todo!(),
-                            LxRosePunctuation::EscapedRcurl => todo!(),
-                        }
-                    }
-                    LxRoseAstData::Math { .. } => todo!(),
-                    LxRoseAstData::Delimited {
-                        left_delimiter_token_idx,
-                        left_delimiter,
-                        asts,
-                        right_delimiter_token_idx,
-                        right_delimiter,
-                    } => todo!(),
-                    LxRoseAstData::CompleteCommand {
-                        command_token_idx,
-                        command_path,
-                        options,
-                        ref arguments,
-                    } => todo!(),
-                    LxRoseAstData::Environment { .. } => todo!(),
-                    LxRoseAstData::NewParagraph(_) => todo!(),
-                }
-            } else {
-                break VdSynSentenceEnd::Void;
-            }
-        };
-        let clauses = self.alloc_clauses(clauses);
-        let data = VdSynSentenceData::Clauses { clauses, end };
-        // TODO: match snl mode
-        VdSynSentenceEntry::Cnl { data }
     }
 }
