33dbf1cc83fed8e04567dfb25adabb1cdb73fc13
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-19 16:44:38 -0800

    add snl mode to visored syn expr vibe

--- crates/visored/visored-syn-expr2/src/environment.rs
+++ crates/visored/visored-syn-expr2/src/environment.rs
@@ -1,6 +1,22 @@
+use snl_prelude::mode::SnlMode;
+
 #[derive(Debug, Clone, Copy)]
-pub struct VdSynExprVibe {}
+pub struct VdSynExprVibe {
+    snl_mode: SnlMode,
+}
 
 impl VdSynExprVibe {
-    pub const ROOT_CNL: Self = Self {};
+    pub const ROOT_CNL: Self = Self {
+        snl_mode: SnlMode::Cnl,
+    };
+
+    pub const ROOT_UNL: Self = Self {
+        snl_mode: SnlMode::Unl,
+    };
+}
+
+impl VdSynExprVibe {
+    pub fn snl_mode(&self) -> SnlMode {
+        self.snl_mode
+    }
 }
