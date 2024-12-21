c8fa7687c5df0cfe60583e62d79dfd5375d20375
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-20 23:59:56 -0800

    save

--- crates/llms/sglang-llm/internal/sglang-llm-docker-ipc/src/main.rs
+++ crates/llms/sglang-llm/internal/sglang-llm-docker-ipc/src/main.rs
@@ -1,26 +1,38 @@
+use sglang_llm_ffi::chat::generate_text_batch;
 use sglang_llm_prelude::greeting::IPC_GREETING;
 use std::io::{self, BufRead};
 
 fn main() {
-    let stdin = io::stdin();
-    let mut reader = stdin.lock();
-
-    let mut input = String::new();
-
     println!("{}", IPC_GREETING);
 
+    for_each_stdin_line(|line| {
+        let prompts: Vec<String> = serde_json::from_str(line).unwrap();
+        eprintln!("prompts = {:#?}", prompts);
+        let answers = generate_text_batch(prompts);
+        eprintln!("answers = {:#?}", answers);
+        todo!()
+        // let numbers: Vec<i32> = line
+        //     .split_whitespace()
+        //     .filter_map(|s| s.parse().ok())
+        //     .collect();
+
+        // if numbers.len() >= 2 {
+        //     let sum: i32 = numbers.iter().sum();
+        //     println!("{}", sum);
+        // }
+    });
+}
+
+fn for_each_stdin_line<F, R>(mut f: F)
+where
+    F: FnMut(&str) -> R,
+{
+    let stdin = io::stdin();
+    let mut reader = stdin.lock();
+    let mut input = String::new();
+
     while reader.read_line(&mut input).unwrap() > 0 {
-        let numbers: Vec<i32> = input
-            .trim()
-            .split_whitespace()
-            .filter_map(|s| s.parse().ok())
-            .collect();
-
-        if numbers.len() >= 2 {
-            let sum: i32 = numbers.iter().sum();
-            println!("{}", sum);
-        }
-
+        let result = f(input.trim());
         input.clear();
     }
 }
