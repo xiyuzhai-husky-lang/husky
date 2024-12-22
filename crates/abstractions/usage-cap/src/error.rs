142f62badc200065873ba9508079bb9e239246e5
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-22 03:07:22 -0800

    use tempfile::TempDir;

--- crates/abstractions/usage-cap/src/error.rs
+++ crates/abstractions/usage-cap/src/error.rs
@@ -1,7 +1,10 @@
 #[derive(Debug, thiserror::Error)]
 pub enum UsageCapError {
-    #[error("LLM API calling is disabled for {0}")]
-    LlmApiCallingDisabled(&'static str),
+    #[error("LLM API calling is disabled for `{entity_name}` because the environment variable `{var_name}` is not set.")]
+    LlmApiCallingDisabled {
+        entity_name: &'static str,
+        var_name: &'static str,
+    },
     #[error("This is the final count down!")]
     FinalCountDown,
 }
