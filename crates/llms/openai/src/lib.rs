b8cb855178c82c075f543815b86eb91138a86000
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-22 13:32:23 -0800

    turn cache into caches for openai

--- crates/llms/openai/src/lib.rs
+++ crates/llms/openai/src/lib.rs
@@ -8,23 +8,27 @@ pub mod response;
 use self::{error::*, request::*, response::*};
 use cap::try_call_openai;
 use disk_cache::DiskCache;
+use enum_index::full_map::EnumFullVecMap;
 use eterned::db::EternerDb;
 use lazy_static::lazy_static;
+use model::OpenaiModel;
 use serde::{Deserialize, Serialize};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use usage_cap::UsageCap;
 
 pub struct OpenaiClient<'db> {
-    cache: DiskCache<&'db EternerDb, OpenaiRequest, OpenaiResponse>,
+    caches: EnumFullVecMap<OpenaiModel, DiskCache<&'db EternerDb, OpenaiRequest, OpenaiResponse>>,
     /// None if the environment variable `OPENAI_API_KEY` is not set.
     client_ext: Option<ext::OpenAIClient>,
 }
 
 impl<'db> OpenaiClient<'db> {
-    pub fn new(db: &'db EternerDb, file_path: PathBuf) -> OpenaiResult<Self> {
+    pub fn new(db: &'db EternerDb, cache_dir: &Path) -> OpenaiResult<Self> {
         let api_key = std::env::var("OPENAI_API_KEY").ok();
         Ok(Self {
-            cache: DiskCache::new(db, file_path)?,
+            caches: EnumFullVecMap::try_new(|model: OpenaiModel| {
+                DiskCache::new(db, cache_dir.join(format!("{}.json", model.as_str())))
+            })?,
             client_ext: match api_key {
                 Some(api_key) => Some(ext::OpenAIClient::builder().with_api_key(api_key).build()?),
                 None => None,
@@ -34,27 +38,30 @@ impl<'db> OpenaiClient<'db> {
 }
 
 impl<'db> OpenaiClient<'db> {
-    pub fn complete_chat(&self, prompt: String) -> OpenaiResult<String> {
-        let min_usage = prompt.len();
-        let request = OpenaiRequest::ChatCompletion(prompt);
-        let OpenaiResponse::ChatCompletion(response) =
-            self.cache
-                .get_or_call(request, |request| -> OpenaiResult<OpenaiResponse> {
-                    match try_call_openai::<OpenaiResult<String>>(min_usage, || {
-                        self.complete_chat_aux(request)
-                    })? {
-                        Ok(result) => match result {
-                            Ok(s) => Ok(OpenaiResponse::ChatCompletion(s)),
-                            Err(e) => Err(todo!()),
-                        },
-                        Err(e) => todo!(),
-                    }
-                })?;
+    pub fn generate_text(&self, model: OpenaiModel, input: String) -> OpenaiResult<String> {
+        let min_usage = input.len();
+        let request = OpenaiRequest::TextGeneration { input };
+        let OpenaiResponse::TextGeneration(response) =
+            self.caches[model].get_or_call(request, |request| -> OpenaiResult<OpenaiResponse> {
+                match try_call_openai::<OpenaiResult<String>>(min_usage, || {
+                    self.complete_chat_aux(model, request)
+                })? {
+                    Ok(result) => match result {
+                        Ok(s) => Ok(OpenaiResponse::TextGeneration(s)),
+                        Err(e) => Err(todo!()),
+                    },
+                    Err(e) => todo!(),
+                }
+            })?;
         Ok(response)
     }
 
-    fn complete_chat_aux(&self, request: &OpenaiRequest) -> (usize, OpenaiResult<String>) {
-        let OaiRequestExt::ChatCompletion(request_ext) = request.ext() else {
+    fn complete_chat_aux(
+        &self,
+        model: OpenaiModel,
+        request: &OpenaiRequest,
+    ) -> (usize, OpenaiResult<String>) {
+        let OaiRequestExt::ChatCompletion(request_ext) = request.ext(model) else {
             unreachable!()
         };
         match self.complete_chat_ext(request_ext) {
@@ -73,10 +80,20 @@ fn openai_client_works() {
         .canonicalize()
         .unwrap()
         .ends_with("crates/llms/openai"));
-    let cache_path = cargo_manifest_dir.join("cache/openai_client_works.json");
-    assert!(cache_path.exists());
+    let cache_dir = &cargo_manifest_dir.join("caches/openai_client_works");
+    assert!(
+        cache_dir.exists(),
+        "Cache directory does not exist at: `{}`",
+        cache_dir.display()
+    );
+    assert!(
+        cache_dir.is_dir(),
+        "Path exists but is not a directory: `{}`",
+        cache_dir.display()
+    );
 
-    let client = OpenaiClient::new(db, cache_path).unwrap();
-    let result = client.complete_chat("Hello, world!".to_string());
+    let client = OpenaiClient::new(db, &cache_dir).unwrap();
+    let model = OpenaiModel::Gpt4o;
+    let result = client.generate_text(model, "Hello, world!".to_string());
     assert!(result.is_ok(), "{}", result.unwrap_err());
 }
