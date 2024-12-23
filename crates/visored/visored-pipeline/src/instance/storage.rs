ee26ac7072506353d18fbd523a2a91659064156e
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-22 20:48:42 -0800

    add `instance storage` and `instance per file`

---
+++ crates/visored/visored-pipeline/src/instance/storage.rs
@@ -0,0 +1,31 @@
+use super::*;
+use idx_arena::Arena;
+
+pub struct VdPipelineInstanceStorage {
+    arena: Arena<VdPipelineInstance>,
+}
+
+impl VdPipelineInstanceStorage {
+    pub(crate) fn new_empty() -> Self {
+        Self {
+            arena: Arena::default(),
+        }
+    }
+}
+
+impl VdPipelineInstanceStorage {
+    pub fn alloc_instances(
+        &mut self,
+        instances: impl IntoIterator<Item = VdPipelineInstance>,
+    ) -> VdPipelineInstanceIdxRange {
+        self.arena.alloc_batch(instances)
+    }
+
+    pub fn all_instances(&self) -> &[VdPipelineInstance] {
+        &*self.arena.data()
+    }
+
+    pub(crate) fn all_instances_mut(&mut self) -> &mut [VdPipelineInstance] {
+        unsafe { self.arena.data_mut() }
+    }
+}
--- crates/visored/visored-pipeline/src/runner.rs
+++ crates/visored/visored-pipeline/src/runner.rs
@@ -1,5 +1,10 @@
 use crate::{
-    input::VdPipelineInput, instance::VdPipelineInstance, VdPipelineConfig, VdPipelineResult,
+    input::VdPipelineInput,
+    instance::{
+        storage::VdPipelineInstanceStorage, VdPipelineInstance, VdPipelineInstanceIdx,
+        VdPipelineInstanceIdxRange,
+    },
+    VdPipelineConfig, VdPipelineConfigData, VdPipelineResult,
 };
 use eterned::db::EternerDb;
 use std::path::Path;
@@ -8,9 +13,12 @@ use std::{fs, path::PathBuf};
 
 pub struct VdPipelineRunner<'db> {
     db: &'db EternerDb,
-    instances: Vec<VdPipelineInstance>,
-    configs: Vec<VdPipelineConfig>,
-    src_inputs: Vec<Arc<VdPipelineInput>>,
+    instance_storage: VdPipelineInstanceStorage,
+    instances_per_file: Vec<(
+        PathBuf,
+        Vec<(Arc<VdPipelineInput>, VdPipelineInstanceIdxRange)>,
+    )>,
+    configs: Vec<Arc<VdPipelineConfig>>,
 }
 
 impl<'db> VdPipelineRunner<'db> {
@@ -20,33 +28,46 @@ impl<'db> VdPipelineRunner<'db> {
         src_file_paths: impl IntoIterator<Item = PathBuf>,
     ) -> VdPipelineResult<Self> {
         let configs = VdPipelineConfig::from_yaml_file(config_path)?;
-
-        let mut src_inputs = vec![];
-        for path in src_file_paths {
-            let examples = VdPipelineInput::read_examples_from_file(path)?;
-            src_inputs.extend(examples);
-        }
-        let instances = configs
-            .iter()
-            .flat_map(|config| {
-                src_inputs
-                    .iter()
-                    .map(move |src| VdPipelineInstance::new(config.clone(), Arc::clone(src)))
-            })
-            .collect();
+        let mut instance_storage = VdPipelineInstanceStorage::new_empty();
+        let instances_per_file: Vec<(
+            PathBuf,
+            Vec<(Arc<VdPipelineInput>, VdPipelineInstanceIdxRange)>,
+        )> = src_file_paths
+            .into_iter()
+            .map(
+                |path| -> VdPipelineResult<(
+                    PathBuf,
+                    Vec<(Arc<VdPipelineInput>, VdPipelineInstanceIdxRange)>,
+                )> {
+                    let examples = VdPipelineInput::read_examples_from_file(&path)?;
+                    let instances = examples
+                        .iter()
+                        .map(|input| {
+                            (
+                                input.clone(),
+                                instance_storage.alloc_instances(configs.iter().map(|config| {
+                                    VdPipelineInstance::new(config.clone(), input.clone())
+                                })),
+                            )
+                        })
+                        .collect::<Vec<_>>();
+                    Ok((path, instances))
+                },
+            )
+            .collect::<VdPipelineResult<Vec<_>>>()?;
 
         Ok(Self {
             db,
-            instances,
+            instances_per_file,
             configs,
-            src_inputs,
+            instance_storage,
         })
     }
 }
 
 impl<'db> VdPipelineRunner<'db> {
     pub fn run_all_single_threaded(&mut self) -> VdPipelineResult<()> {
-        for instance in &mut self.instances {
+        for instance in self.instance_storage.all_instances_mut() {
             instance.run(self.db)?;
         }
         Ok(())
@@ -55,7 +76,8 @@ impl<'db> VdPipelineRunner<'db> {
     pub fn run_all_multi_threaded(&mut self) -> VdPipelineResult<()> {
         use rayon::prelude::*;
 
-        self.instances
+        self.instance_storage
+            .all_instances_mut()
             .par_iter_mut()
             .try_for_each(|instance| instance.run(self.db))?;
         Ok(())
