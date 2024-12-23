830793ac7fb50c12e341909857aeef0081b2768a
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-22 16:02:29 -0800

    save

--- crates/visored/visored-pipeline/src/lib.rs
+++ crates/visored/visored-pipeline/src/lib.rs
@@ -1,9 +1,15 @@
+/// builder builds one instance of the pipeline, returns a tracker
 mod builder;
 pub mod config;
 pub mod error;
 pub mod input;
+/// smallest unit of the pipeline
 mod instance;
+/// runner orchestrates all instances
 pub mod runner;
+#[cfg(test)]
+mod tests;
+/// tracker keeps the values of instance execution
 mod tracker;
 
 use self::{config::*, error::*};
