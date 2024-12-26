580f065218cf3729252833264c9bae265a16328f
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-25 21:08:29 -0800

    generate lean code for all tests

--- crates/latex/latex-ast/src/ast/math/left_right.rs
+++ crates/latex/latex-ast/src/ast/math/left_right.rs
@@ -19,6 +19,10 @@ impl<'a> LxAstParser<'a> {
         let Some(data) = self.parse_math_ast() else {
             todo!()
         };
+        match data {
+            LxMathAstData::Punctuation(..) => (),
+            _ => todo!("this could be caused by sloppy latex code like $\\left(a+b\\right)^2$"),
+        }
         let argument = self.alloc_math_ast(data);
         LxMathAstData::Righted {
             right_command_token_idx,
