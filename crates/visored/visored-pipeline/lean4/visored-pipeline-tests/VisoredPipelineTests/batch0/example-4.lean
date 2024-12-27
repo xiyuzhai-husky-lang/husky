0949eee0cb5ca49aa32b287075aa40cc00a77cec
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-26 19:41:08 -0800

    lean is lame

---
+++ crates/visored/visored-pipeline/lean4/visored-pipeline-tests/VisoredPipelineTests/batch0/example-4.lean
@@ -0,0 +1,14 @@
+import Mathlib
+import Obvious
+open Obvious
+
+def h(x : ℝ)(h1 : x > 0) := by
+  have h2 : x > 0 := by obvious
+  have h3 : (x - 1) ^ 2 ≥ 0 := by obvious
+  have h4 : x ^ 2 - 2 * x + 1 ≥ 0 := by obvious
+  have h5 : (x ^ 2 - 2 * x + 1) / x ≥ 0 := by calc
+      (x ^ 2 - 2 * x + 1) / x = x ^ 2 / x - 2 * x / x + 1 / x := by obvious
+    _ = x - 2 + 1 / x := by obvious
+    _ ≥ 0 := by obvious
+  have h6 : x + 1 / x ≥ 2 := by obvious
+  exact ()
