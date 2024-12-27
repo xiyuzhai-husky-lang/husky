38f6ec5a3ccb03506519d5612b157309e5a4b15d
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-26 21:03:08 -0800

    save

--- crates/visored/visored-pipeline/lean4/visored-pipeline-tests/VisoredPipelineTests/batch0/example-3.lean
+++ crates/visored/visored-pipeline/lean4/visored-pipeline-tests/VisoredPipelineTests/batch0/example-3.lean
@@ -3,11 +3,16 @@ import Obvious
 open Obvious
 
 def h(a : ℝ)(b : ℝ)(c : ℝ) := by
-  have h1 : (a - b) ^ 2 + (b - c) ^ 2 + (c - a) ^ 2 ≥ 0 := by calc
+  first
+  | have h1 : (a - b) ^ 2 + (b - c) ^ 2 + (c - a) ^ 2 ≥ 0 := by calc
       (a - b) ^ 2 + (b - c) ^ 2 + (c - a) ^ 2 = a ^ 2 - 2 * a * b + b ^ 2 + (b ^ 2 - 2 * b * c + c ^ 2) + (c ^ 2 - 2 * c * a + a ^ 2) := by obvious
     _ = 2 * (a ^ 2) + 2 * (b ^ 2) + 2 * (c ^ 2) - 2 * a * b - 2 * b * c - 2 * c * a := by obvious
-    _ ≥ 0 := by sorry
+    _ ≥ 0 := by obvious
+  | have h1 : 2 * (a ^ 2) + 2 * (b ^ 2) + 2 * (c ^ 2) - 2 * a * b - 2 * b * c - 2 * c * a ≥ 0 := by calc
+    2 * (a ^ 2) + 2 * (b ^ 2) + 2 * (c ^ 2) - 2 * a * b - 2 * b * c - 2 * c * a = a ^ 2 - 2 * a * b + b ^ 2 + (b ^ 2 - 2 * b * c + c ^ 2) + (c ^ 2 - 2 * c * a + a ^ 2) := by obvious
+    _ = (a - b) ^ 2 + (b - c) ^ 2 + (c - a) ^ 2 := by obvious
+    _ ≥ 0 := by obvious
   have h2 : a ^ 2 + b ^ 2 + c ^ 2 - a * b - b * c - c * a ≥ 0 := by obvious
-  exact show (a ^ 2 + b ^ 2 + c ^ 2 ≥ a * b + b * c + c * a) by obvious
+  show a ^ 2 + b ^ 2 + c ^ 2 ≥ a * b + b * c + c * a by obvious
   have h3 : a ^ 2 + b ^ 2 + c ^ 2 ≥ a * b + b * c + c * a := by obvious
   exact ()
