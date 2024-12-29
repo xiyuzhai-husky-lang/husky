3b64b0525d158bf758bb92129e9d979489fd6acf
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-29 03:23:26 -0800

    add `DashNote` to `VdTacticsEvaluationSession`

--- crates/visored/visored-tactics-evaluation/src/session.rs
+++ crates/visored/visored-tactics-evaluation/src/session.rs
@@ -1,11 +1,19 @@
 use crate::*;
+use visored_mir_expr::expr::VdMirExprIdx;
+
+type SuperVarsContext = ();
+type Term = ();
 
 pub struct VdTacticsEvaluationSession<'db> {
     db: &'db EternerDb,
+    expr_terms: DashNote<(VdMirExprIdx, SuperVarsContext), Term>,
 }
 
 impl<'db> VdTacticsEvaluationSession<'db> {
     pub fn new(db: &'db EternerDb) -> Self {
-        Self { db }
+        Self {
+            db,
+            expr_terms: DashNote::default(),
+        }
     }
 }
