7d100d22918eb4eb06cb56ada2da3340c16887a8
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-29 08:14:17 -0800

    more

--- crates/visored/visored-mir-expr/src/tactic.rs
+++ crates/visored/visored-mir-expr/src/tactic.rs
@@ -1,1 +1,18 @@
 pub mod elaboration;
+
+use elaboration::VdMirTacticElaboration;
+use idx_arena::{Arena, ArenaIdx, ArenaIdxRange};
+use once_place::OncePlace;
+
+pub enum VdMirTacticData {
+    Obvious,
+}
+
+pub struct VdMirTacticEntry {
+    data: VdMirTacticData,
+    elaboration: OncePlace<VdMirTacticElaboration>,
+}
+
+pub type VdMirTacticIdx = ArenaIdx<VdMirTacticEntry>;
+pub type VdMirTacticIdxRange = ArenaIdxRange<VdMirTacticEntry>;
+pub type VdMirTacticArena = Arena<VdMirTacticEntry>;
