80d39ae9751caa246f1754014e33713585197f43
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-29 18:32:51 -0800

    save

--- crates/visored/visored-mir-expr/src/tactic.rs
+++ crates/visored/visored-mir-expr/src/tactic.rs
@@ -1,7 +1,7 @@
 pub mod elaboration;
 
 use elaboration::VdMirTacticElaboration;
-use idx_arena::{Arena, ArenaIdx, ArenaIdxRange};
+use idx_arena::{Arena, ArenaIdx, ArenaIdxRange, ArenaRef};
 use once_place::OncePlace;
 
 pub enum VdMirTacticData {
@@ -13,6 +13,11 @@ pub struct VdMirTacticEntry {
     elaboration: OncePlace<VdMirTacticElaboration>,
 }
 
+pub type VdMirTacticIdx = ArenaIdx<VdMirTacticEntry>;
+pub type VdMirTacticIdxRange = ArenaIdxRange<VdMirTacticEntry>;
+pub type VdMirTacticArena = Arena<VdMirTacticEntry>;
+pub type VdMirTacticArenaRef<'a> = ArenaRef<'a, VdMirTacticEntry>;
+
 impl VdMirTacticEntry {
     pub fn new(data: VdMirTacticData) -> Self {
         Self {
@@ -22,6 +27,13 @@ impl VdMirTacticEntry {
     }
 }
 
-pub type VdMirTacticIdx = ArenaIdx<VdMirTacticEntry>;
-pub type VdMirTacticIdxRange = ArenaIdxRange<VdMirTacticEntry>;
-pub type VdMirTacticArena = Arena<VdMirTacticEntry>;
+impl VdMirTacticEntry {
+    pub fn data(&self) -> &VdMirTacticData {
+        &self.data
+    }
+
+    #[track_caller]
+    pub fn elaboration(&self) -> &VdMirTacticElaboration {
+        &*self.elaboration
+    }
+}
