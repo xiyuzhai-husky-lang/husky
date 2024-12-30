dfe96942831f92f38a27c048335253a025e70196
Xiyu Zhai <xiyuzhai95@gmail.com>
2024-12-29 17:38:22 -0800

    save

--- crates/visored/visored-lean-transpilation/src/stmt/sparse/show.rs
+++ crates/visored/visored-lean-transpilation/src/stmt/sparse/show.rs
@@ -4,12 +4,17 @@ use either::*;
 use lean_mir_expr::{expr::application::LnMirFunc, tactic::LnMirTacticData};
 use lean_opr::opr::binary::LnBinaryOpr;
 use lean_term::instantiation::LnInstantiation;
-use visored_mir_expr::expr::application::VdMirFunc;
+use visored_mir_expr::{expr::application::VdMirFunc, tactic::VdMirTacticIdxRange};
 use visored_opr::{opr::binary::VdBaseBinaryOpr, separator::VdBaseSeparator};
 use visored_signature::signature::separator::base::VdBaseSeparatorSignature;
 
 impl<'a> VdLeanTranspilationBuilder<'a, Sparse> {
-    pub(super) fn build_show_stmt(&mut self, formula: VdMirExprIdx) -> LnItemDefnData {
+    pub(super) fn build_show_stmt(
+        &mut self,
+        prop: VdMirExprIdx,
+        tactics: VdMirTacticIdxRange,
+    ) -> LnItemDefnData {
+        todo!("show tactics");
         todo!()
     }
 }
