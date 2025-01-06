976fb51037fb831d43fe74fcbf2acb07de0ec902
Xiyu Zhai <xiyuzhai95@gmail.com>
2025-01-06 07:22:53 -0800

    save

--- crates/visored/visored-baseq-elaborator/src/tactic/comm_ring/sum.rs
+++ crates/visored/visored-baseq-elaborator/src/tactic/comm_ring/sum.rs
@@ -1,7 +1,7 @@
-use crate::term::inum::{sum::VdBsqSumInumTerm, VdBsqNonSumInumTerm};
-
 use super::*;
-use monadic_fold::engine::IsMonadicFoldEngineScheme;
+use crate::term::inum::{sum::VdBsqSumInumTerm, VdBsqNonSumInumTerm};
+use crate::term::{builder::sum::VdBsqSumBuilder, rnum::VdBsqRnumTerm};
+use monadic_fold::engine::{IsMonadicFoldEngineScheme, IsMonadicFoldEngineSchemeFull as _};
 use std::marker::PhantomData;
 
 struct Scheme<'db, 'sess>(PhantomData<&'db ()>, PhantomData<&'sess ()>);
@@ -12,11 +12,11 @@ where
 {
     type Engine = VdBsqElaboratorInner<'db, 'sess>;
 
-    type State = VdBsqSumInumTerm<'sess>;
+    type State = VdBsqSumBuilder<'sess>;
 
-    type Item = VdBsqNonSumInumTerm<'sess>;
+    type Item = (VdBsqRnumTerm, VdBsqNonSumInumTerm<'sess>);
 
-    type Output = VdBsqSumInumTerm<'sess>;
+    type Output = AltOption<VdBsqHypothesisIdx<'sess>>;
 
     fn fold_step(
         engine: &mut Self::Engine,
@@ -25,5 +25,19 @@ where
         f: impl FnMut(&mut Self::Engine, Self::State) -> Self::Output,
     ) -> Self::Output {
         todo!()
+    }
+}
+
+impl<'db, 'sess> VdBsqElaboratorInner<'db, 'sess> {
+    pub(super) fn comm_ring_fold_sum(
+        &mut self,
+        terms: &[(VdBsqRnumTerm, VdBsqNonSumInumTerm<'sess>)],
+    ) -> AltOption<VdBsqHypothesisIdx<'sess>> {
+        Scheme::fold(
+            self,
+            VdBsqSumBuilder::new(self.floater_db()),
+            terms.iter().copied(),
+            |elaborator, builder| todo!(),
+        )
     }
 }
